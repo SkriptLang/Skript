00958cc6721b199446eb821c807551029cc69ec9
TPGamesNL <pet.teun.03@gmail.com>
2021-08-12 14:59:50 +0200

    Review response part 2

--- src/main/java/ch/njol/util/StringUtils.java
+++ src/main/java/ch/njol/util/StringUtils.java
@@ -41,13 +41,15 @@ public abstract class StringUtils {
 	 * @param i the number
 	 * @return 1st, 2nd, 3rd, 4th, etc.
 	 */
-	public static String fancyOrderNumber(final int i) {
-		if (i == 1)
-			return "1st";
-		if (i == 2)
-			return "2nd";
-		if (i == 3)
-			return "3rd";
+	public static String fancyOrderNumber(int i) {
+		int iModTen = i % 10;
+		int iModHundred = i % 100;
+		if (iModTen == 1 && iModHundred != 11)
+			return i + "st";
+		if (iModTen == 2 && iModHundred != 12)
+			return i + "nd";
+		if (iModTen == 3 && iModHundred != 13)
+			return i + "rd";
 		return i + "th";
 	}
 	
