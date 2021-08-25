f247ce2aa83c19498f94d35f072032c3ff34702c
APickledWalrus <apickledwalrus@gmail.com>
2021-08-25 17:02:44 -0400

    Add Inventory-InventoryType comparator

--- src/main/java/ch/njol/skript/classes/data/DefaultComparators.java
+++ src/main/java/ch/njol/skript/classes/data/DefaultComparators.java
@@ -46,6 +46,8 @@ import org.bukkit.entity.Item;
 import org.bukkit.entity.Projectile;
 import org.bukkit.entity.Wither;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -579,6 +581,18 @@ public class DefaultComparators {
 				}
 			});
 		}
+
+		Comparators.registerComparator(Inventory.class, InventoryType.class, new Comparator<Inventory, InventoryType>() {
+			@Override
+			public Relation compare(Inventory inventory, InventoryType inventoryType) {
+				return Relation.get(inventory.getType() == inventoryType);
+			}
+
+			@Override
+			public boolean supportsOrdering() {
+				return false;
+			}
+		});
 	}
 	
 }
