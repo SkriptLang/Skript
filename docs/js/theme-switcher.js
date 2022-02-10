e0645777344224e02b858a450702bfcdbeb30272
Ayham Al-Ali <alali_ayham@yahoo.com>
2022-02-10 16:18:30 +0200

    Fix white theme flickering (again)

--- docs/js/theme-switcher.js
+++ docs/js/theme-switcher.js
@@ -2,7 +2,7 @@
 // This file is made to fix theme flicker at load due to 'defer' in main.js loading
 // 
 
-// A quick fix to not use modules due to CORS
+// A quick fix to not use modules due to CORS on localhost
 function setCookie(cname, cvalue, exdays) {
   const d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -25,37 +25,25 @@ function getCookie(cname) {
   return "";
 }
 
-if (getCookie("darkMode") == "false") {
-  document.body.setAttribute('data-theme', 'white')
-}
-
-
-var content = document.getElementById("content");
-if (!content) {
-  content = document.getElementById("content-no-docs")
-}
-
 // Auto load DarkMode from cookies
 if (getCookie("darkMode") == "false") {
-  content.insertAdjacentHTML('beforeend', `<img style="z-index: 99;" src="./assets/light-on.svg" id="theme-switch">`);
   document.body.setAttribute('data-theme', 'white')
+  document.body.insertAdjacentHTML('beforeend', `<img style="z-index: 99;" src="./assets/light-on.svg" id="theme-switch">`);
 } else {
-  content.insertAdjacentHTML('beforeend', `<img style="z-index: 99;" src="./assets/light-off.svg" id="theme-switch">`);
-
+  document.body.insertAdjacentHTML('beforeend', `<img style="z-index: 99;" src="./assets/light-off.svg" id="theme-switch">`);
+  
   // Auto load from system theme
   // const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
   // if (darkThemeMq.matches) {
-  //   document.body.removeAttribute('data-theme');
-  // } else {
-  //   document.body.setAttribute('data-theme', 'white')
-  // }
-}
+    //   document.body.removeAttribute('data-theme');
+    // } else {
+      //   document.body.setAttribute('data-theme', 'white')
+      // }
+    }
 
 setTimeout(() => {
   var themeSwitcher = document.getElementById('theme-switch');
-  // console.log(themeSwitcher);
   themeSwitcher.addEventListener('click', (event) => {
-    // console.log("1");
     if (document.body.getAttribute("data-theme") == null) {
       document.body.setAttribute('data-theme', 'white');
       event.target.src = "./assets/light-on.svg";
