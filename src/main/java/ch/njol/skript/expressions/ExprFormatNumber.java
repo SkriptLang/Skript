f1a8f7ffd2963bd4044c77732696ca6dac19b7e9
Ayham Al-Ali <alali_ayham@yahoo.com>
2022-03-15 15:56:31 +0300

    🚀 Add number format expression

---
+++ src/main/java/ch/njol/skript/expressions/ExprFormatNumber.java
@@ -0,0 +1,109 @@
+/**
+ *   This file is part of Skript.
+ *
+ *  Skript is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Skript is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright Peter Güttinger, SkriptLang team and contributors
+ */
+package ch.njol.skript.expressions;
+
+import ch.njol.skript.Metrics;
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.expressions.base.PropertyExpression;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.Literal;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.VariableString;
+import ch.njol.skript.util.Getter;
+import ch.njol.util.Kleenean;
+import org.bukkit.event.Event;
+import org.eclipse.jdt.annotation.Nullable;
+
+import java.text.DecimalFormat;
+
+@Name("Formatted Number")
+@Description("Converts numbers to human-readable format. By default, '###,###' (e.g. '123,456,789') will be used. For reference, see this "
+		+ "<a href=\"https://docs.oracle.com/javase/7/docs/api/java/text/DecimalFormat.html\" target=\"_blank\">article</a>.")
+@Examples({
+	"command /formatnumber <number>:",
+		"\taliases: fn",
+		"\ttrigger:",
+			"\t\tsend \"Formatted: %formatted arg-1%\" to sender"
+})
+@Since("INSERT VERSION")
+public class ExprFormatNumber extends PropertyExpression<Number, String> {
+	
+	private static final String defaultFormat = "###,###";
+	
+	static {
+		Skript.registerExpression(ExprFormatNumber.class, String.class, ExpressionType.PROPERTY,
+			"%numbers% formatted [human-readable] [(with|as) %-string%]",
+			"[human-readable] formatted %numbers% [(with|as) %-string%]");
+	}
+
+	@SuppressWarnings("null")
+	private DecimalFormat format;
+
+	@Override
+	@SuppressWarnings("null")
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+		setExpr((Expression<? extends Number>) exprs[0]);
+		if (exprs[1] != null) {
+			if (!(exprs[1] instanceof Literal)) {
+				VariableString str = (VariableString) exprs[1];
+				if (!str.isSimple()) {
+					Skript.error("Number format must not contain variables!");
+					return false;
+				}
+			}
+			String customFormat = (String) exprs[1].getSingle(null);
+			try {
+				format = new DecimalFormat(customFormat);
+			} catch (Exception e) {
+				Skript.error("Incorrect number format used: " + e.getMessage());
+				return false;
+			}
+		} else {
+			format = new DecimalFormat(defaultFormat);
+		}
+		
+		return true;
+	}
+
+	@Override
+	protected String[] get(Event e, Number[] source) {
+		return get(source, new Getter<String, Number>() {
+			@Override
+			public String get(Number num) {
+				return format.format(num);
+			}
+		});
+	}
+
+	@Override
+	public Class<? extends String> getReturnType() {
+		return String.class;
+	}
+
+	@Override
+	public String toString(@Nullable Event e, boolean debug) {
+		return getExpr().toString(e, debug) + " formatted as " + format.toPattern();
+	}
+
+}
