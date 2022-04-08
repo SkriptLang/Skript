4b66bb3dcff0dbb12269023d5475e3b5cde859f4
oskarkk <16339637+oskarkk@users.noreply.github.com>
2022-04-08 23:56:14 +0200

    Improve code formatting, logic and variable names in EffSendTitle

--- src/main/java/ch/njol/skript/effects/EffSendTitle.java
+++ src/main/java/ch/njol/skript/effects/EffSendTitle.java
@@ -34,17 +34,21 @@ import ch.njol.skript.util.Timespan;
 import ch.njol.util.Kleenean;
 
 @Name("Title - Send")
-@Description({"Sends a title/subtitle to the given player(s) with optional fadein/stay/fadeout times for Minecraft versions 1.11 and above. " +
-		"",
-		"If you're sending only the subtitle, it will be shown only if there's a title displayed at the moment, otherwise it will " +
-		"be sent with the next title. To show only the subtitle, use: <code>send title \" \" with subtitle \"yourtexthere\" to player</code>.",
-		"",
-		"Note: if no input is given for the times, it will keep the ones from the last title sent, " +
-		"use the <a href='effects.html#EffResetTitle'>reset title</a> effect to restore the default values."})
-@Examples({"send title \"Competition Started\" with subtitle \"Have fun, Stay safe!\" to player for 5 seconds",
-		"send title \"Hi %player%\" to player", "send title \"Loot Drop\" with subtitle \"starts in 3 minutes\" to all players",
-		"send title \"Hello %player%!\" with subtitle \"Welcome to our server\" to player for 5 seconds with fadein 1 second and fade out 1 second",
-		"send subtitle \"Party!\" to all players"})
+@Description({
+	"Sends a title/subtitle to the given player(s) with optional fadein/stay/fadeout times for Minecraft versions 1.11 and above. ",
+	"",
+	"If you're sending only the subtitle, it will be shown only if there's a title displayed at the moment, otherwise it will " +
+	"be sent with the next title. To show only the subtitle, use: <code>send title \" \" with subtitle \"yourtexthere\" to player</code>.",
+	"",
+	"Note: if no input is given for the times, it will keep the ones from the last title sent, " +
+	"use the <a href='effects.html#EffResetTitle'>reset title</a> effect to restore the default values."
+})
+@Examples({
+	"send title \"Competition Started\" with subtitle \"Have fun, Stay safe!\" to player for 5 seconds",
+	"send title \"Hi %player%\" to player", "send title \"Loot Drop\" with subtitle \"starts in 3 minutes\" to all players",
+	"send title \"Hello %player%!\" with subtitle \"Welcome to our server\" to player for 5 seconds with fadein 1 second and fade out 1 second",
+	"send subtitle \"Party!\" to all players"
+})
 @Since("2.3")
 public class EffSendTitle extends Effect {
 	
@@ -87,38 +91,33 @@ public class EffSendTitle extends Effect {
 	@SuppressWarnings("null")
 	@Override
 	protected void execute(final Event e) {
-		String tit = title != null ? title.getSingle(e) : null;
-		String sub = subtitle != null ? subtitle.getSingle(e) : null;
+		String title = this.title != null ? this.title.getSingle(e) : null;
+		String subtitle = this.subtitle != null ? this.subtitle.getSingle(e) : null;
 		
 		if (TIME_SUPPORTED) {
-			int in, st, out;
+			int fadeIn, stay, fadeOut;
+			fadeIn = stay = fadeOut = -1;
 
-			if (fadeIn != null) {
-				Timespan t = fadeIn.getSingle(e);
-				in = t != null ? (int) t.getTicks_i() : -1;
-			} else {
-				in = -1;
+			if (this.fadeIn != null) {
+				Timespan t = this.fadeIn.getSingle(e);
+				fadeIn = t != null ? (int) t.getTicks_i() : -1;
 			}
 
-			if (stay != null) {
-				Timespan t = stay.getSingle(e);
-				st = t != null ? (int) t.getTicks_i() : -1;
-			} else {
-				st = -1;
+			if (this.stay != null) {
+				Timespan t = this.stay.getSingle(e);
+				stay = t != null ? (int) t.getTicks_i() : -1;
 			}
 
-			if (fadeOut != null) {
-				Timespan t = fadeOut.getSingle(e);
-				out = t != null ? (int) t.getTicks_i() : -1;
-			} else {
-				out = -1;
+			if (this.fadeOut != null) {
+				Timespan t = this.fadeOut.getSingle(e);
+				fadeOut = t != null ? (int) t.getTicks_i() : -1;
 			}
 			
 			for (Player p : recipients.getArray(e))
-				p.sendTitle(tit, sub, in, st, out);
+				p.sendTitle(title, subtitle, fadeIn, stay, fadeOut);
 		} else {
 			for (Player p : recipients.getArray(e))
-				p.sendTitle(tit, sub);
+				p.sendTitle(title, subtitle);
 		}
 	}
 	
