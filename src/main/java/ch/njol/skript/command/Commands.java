d49a375095887ac7ed12077b706aa902aaf53883
APickledWalrus <apickledwalrus@gmail.com>
2022-06-22 00:54:10 -0400

    Add missing imports
    They were accidentally excluded during conflict resolving

--- src/main/java/ch/njol/skript/command/Commands.java
+++ src/main/java/ch/njol/skript/command/Commands.java
@@ -57,6 +57,7 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Callable;
