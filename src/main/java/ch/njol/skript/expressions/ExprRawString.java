298ff8c6a7ac8153a15f2382e2bb0725abd18eb6
UnderscoreTud <mmbakkar06@gmail.com>
2022-07-09 01:16:24 +0300

    Made 'Raw String' expression accept multiple strings

--- src/main/java/ch/njol/skript/expressions/ExprRawString.java
+++ src/main/java/ch/njol/skript/expressions/ExprRawString.java
@@ -21,41 +21,56 @@ package ch.njol.skript.expressions;
 import ch.njol.skript.Skript;
 import ch.njol.skript.classes.Converter;
 import ch.njol.skript.expressions.base.PropertyExpression;
-import ch.njol.skript.lang.Expression;
-import ch.njol.skript.lang.ExpressionType;
-import ch.njol.skript.lang.SkriptParser;
-import ch.njol.skript.lang.VariableString;
+import ch.njol.skript.lang.*;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.skript.log.ErrorQuality;
+import ch.njol.skript.util.LiteralUtils;
+import ch.njol.skript.util.chat.ChatMessages;
 import ch.njol.util.Kleenean;
+import org.bukkit.Bukkit;
 import org.bukkit.event.Event;
 import org.eclipse.jdt.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExprRawString extends SimpleExpression<String> {
 
 	static {
-		Skript.registerExpression(ExprRawString.class, String.class, ExpressionType.SIMPLE, "raw %string%");
+		Skript.registerExpression(ExprRawString.class, String.class, ExpressionType.SIMPLE, "raw %strings%");
 	}
 
-	@SuppressWarnings("null")
-	private Expression<String> expr;
+	@SuppressWarnings("NotNullFieldNotInitialized")
+	private Expression<?> expr;
+	@SuppressWarnings("NotNullFieldNotInitialized")
+	private Expression<? extends String>[] messages;
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
-		expr = (Expression<String>) exprs[0];
-		if (expr instanceof ExprColoured) {
-			Skript.error("the 'colored' expression may not be used with a 'raw string' expression", ErrorQuality.SEMANTIC_ERROR);
-			return false;
+		expr = LiteralUtils.defendExpression(exprs[0]);
+		messages = expr instanceof ExpressionList<?> ?
+			((ExpressionList<String>) expr).getExpressions() : new Expression[]{expr};
+		for (Expression<? extends String> message : messages) {
+			if (message instanceof ExprColoured) {
+				Skript.error("the 'colored' expression may not be used with a 'raw string' expression", ErrorQuality.SEMANTIC_ERROR);
+				return false;
+			}
 		}
-		return true;
+		return LiteralUtils.canInitSafely(expr);
 	}
 
 	@Override
 	protected @Nullable String[] get(Event e) {
-		if (expr instanceof VariableString)
-			return new String[]{((VariableString) expr).toUnformattedString(e)};
-		return new String[]{expr.getSingle(e)};
+		List<String> strings = new ArrayList<>();
+		for (Expression<? extends String> message : messages) {
+			if (message instanceof VariableString) {
+				strings.add(((VariableString) message).toUnformattedString(e));
+				continue;
+			}
+			strings.addAll(List.of(message.getArray(e)));
+		}
+		return strings.toArray(new String[0]);
 	}
 
 	@Override
