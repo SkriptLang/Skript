eed6c4825a184d0060efc26f9d44a2a097772530
Ayham Al-Ali <alali_ayham@yahoo.com>
2022-07-17 14:05:58 +0300

    Return null of isMax on entities

--- src/main/java/ch/njol/skript/expressions/ExprAge.java
+++ src/main/java/ch/njol/skript/expressions/ExprAge.java
@@ -83,7 +83,7 @@ public class ExprAge extends SimplePropertyExpression<Object, Integer> {
 			Ageable ageable = (Ageable) bd;
 			return isMax ? ageable.getMaximumAge() : ageable.getAge();
 		} else if (obj instanceof org.bukkit.entity.Ageable) {
-			return ((org.bukkit.entity.Ageable) obj).getAge();
+			return isMax ? null : ((org.bukkit.entity.Ageable) obj).getAge();
 		}
 		return null;
 	}
