740c653c7b437c68e45c582b6b5fb4dd851da6a4
Ayham Al-Ali <alali_ayham@yahoo.com>
2022-08-05 17:36:19 +0300

    Remove Unused imports

--- src/main/java/ch/njol/skript/conditions/CondIsStatistic.java
+++ src/main/java/ch/njol/skript/conditions/CondIsStatistic.java
@@ -18,19 +18,12 @@
  */
 package ch.njol.skript.conditions;
 
-import ch.njol.skript.Skript;
 import ch.njol.skript.conditions.base.PropertyCondition;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
-import ch.njol.skript.lang.Condition;
-import ch.njol.skript.lang.Expression;
-import ch.njol.skript.lang.SkriptParser.ParseResult;
-import ch.njol.util.Kleenean;
 import org.bukkit.Statistic;
-import org.bukkit.event.Event;
-import org.eclipse.jdt.annotation.Nullable;
 
 @Name("Is Statistic")
 @Description("Tests whether a given string is a valid statistic name.")
