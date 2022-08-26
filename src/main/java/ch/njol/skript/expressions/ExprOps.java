45fe35b20448ec639223054202d5a74f563565e8
nylhus <matthew.a.maloneyy@gmail.com>
2022-08-26 13:05:28 -0300

    Fixed issue 4910.

--- src/main/java/ch/njol/skript/expressions/ExprOps.java
+++ src/main/java/ch/njol/skript/expressions/ExprOps.java
@@ -23,70 +23,70 @@ import org.eclipse.jdt.annotation.Nullable;
 @Since("INSERT VERSION")
 
 public class ExprOps extends SimpleExpression<OfflinePlayer> {
-    static {
-        Skript.registerExpression(ExprOps.class, OfflinePlayer.class, ExpressionType.SIMPLE, "[all [[of] the]|the] [server] op[erator]s");
-    }
+	static {
+		Skript.registerExpression(ExprOps.class, OfflinePlayer.class, ExpressionType.SIMPLE, "[all [[of] the]|the] [server] op[erator]s");
+	}
 
-    @Override
-    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
-        return true;
-    }
+	@Override
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+		return true;
+	}
 
-    @Override
-    protected OfflinePlayer[] get(Event e) {
-        return Bukkit.getOperators().toArray(new OfflinePlayer[0]);
-    }
+	@Override
+	protected OfflinePlayer[] get(Event e) {
+		return Bukkit.getOperators().toArray(new OfflinePlayer[0]);
+	}
 
-    @Nullable
-    @Override
-    public Class<?>[] acceptChange(ChangeMode mode) {
-        switch (mode) {
-            case ADD:
-            case SET:
-            case REMOVE:
-            case RESET:
-            case DELETE:
-                return CollectionUtils.array(OfflinePlayer[].class);
-        }
-        return null;
-    }
+	@Nullable
+	@Override
+	public Class<?>[] acceptChange(ChangeMode mode) {
+		switch (mode) {
+			case ADD:
+			case SET:
+			case REMOVE:
+			case RESET:
+			case DELETE:
+				return CollectionUtils.array(OfflinePlayer[].class);
+		}
+		return null;
+	}
 
-    @Override
-    public void change(Event e, @Nullable Object[] delta, ChangeMode mode) {
-        switch (mode) {
-            case SET:
-                for (OfflinePlayer p : Bukkit.getOperators())
-                    p.setOp(false);
-            case ADD:
-                for (Object p : delta)
-                    ((OfflinePlayer) p).setOp(true);
-                break;
-            case REMOVE:
-                for (Object p : delta)
-                    ((OfflinePlayer) p).setOp(false);
-                break;
-            case DELETE:
-            case RESET:
-                for (OfflinePlayer p : Bukkit.getOperators())
-                    p.setOp(false);
-                break;
-            default:
-                assert false;
-        }
-    }
+	@Override
+	public void change(Event e, @Nullable Object[] delta, ChangeMode mode) {
+		switch (mode) {
+			case SET:
+				for (OfflinePlayer p : Bukkit.getOperators())
+					p.setOp(false);
+			case ADD:
+				for (Object p : delta)
+					((OfflinePlayer) p).setOp(true);
+				break;
+			case REMOVE:
+				for (Object p : delta)
+					((OfflinePlayer) p).setOp(false);
+				break;
+			case DELETE:
+			case RESET:
+				for (OfflinePlayer p : Bukkit.getOperators())
+					p.setOp(false);
+				break;
+			default:
+				assert false;
+		}
+	}
 
-    @Override
-    public boolean isSingle() {
-        return false;
-    }
+	@Override
+	public boolean isSingle() {
+		return false;
+	}
 
-    @Override
-    public Class<? extends OfflinePlayer> getReturnType() {
-        return OfflinePlayer.class;
-    }
+	@Override
+	public Class<? extends OfflinePlayer> getReturnType() {
+		return OfflinePlayer.class;
+	}
 
-    @Override
-    public String toString(@Nullable Event e, boolean debug) {
-        return "all ops";
-    }
+	@Override
+	public String toString(@Nullable Event e, boolean debug) {
+		return "all ops";
+	}
 }
