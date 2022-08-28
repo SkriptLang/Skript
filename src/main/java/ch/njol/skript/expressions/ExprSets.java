c6ef2b9e15c5bb74a27f0c0d0308f9d89ce94acd
_tud <mmbakkar06@gmail.com>
2022-08-29 01:07:05 +0300

    fix pattern

--- src/main/java/ch/njol/skript/expressions/ExprSets.java
+++ src/main/java/ch/njol/skript/expressions/ExprSets.java
@@ -62,7 +62,7 @@ public class ExprSets extends SimpleExpression<Object> {
 		Skript.registerExpression(ExprSets.class, Object.class, ExpressionType.COMBINED,
 				"[(all [[of] the]|the|every)] item(s|[ ]types)", "[(all [[of] the]|the)] items of type[s] %itemtypes%",
 				"[(all [[of] the]|the|every)] block(s|[ ]types)", "[(all [[of] the]|the)] blocks of type[s] %itemtypes%",
-				"[(all [[of] the]|the|every)] colo[u]rs)");
+				"[(all [[of] the]|the|every)] colo[u]rs");
 	}
 
 	@Nullable
