9c693b316649667c0d748681067766efe9a1c2a0
_tud <mmbakkar06@gmail.com>
2022-09-16 06:25:26 +0300

    requested changes

--- src/main/java/ch/njol/skript/expressions/ExprFireTicks.java
+++ src/main/java/ch/njol/skript/expressions/ExprFireTicks.java
@@ -30,7 +30,7 @@ import org.bukkit.entity.Entity;
 import org.bukkit.event.Event;
 import org.eclipse.jdt.annotation.Nullable;
 
-@Name("Fire Time")
+@Name("Entity Fire Burn Duration")
 @Description("How much time an entity will be burning for.")
 @Examples({"send \"You will stop burning in %fire time of player%\""})
 @Since("INSERT VERSION")
