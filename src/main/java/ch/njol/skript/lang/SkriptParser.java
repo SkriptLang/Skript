c47367ba2c30c6c51e66670bdf60f1764a6662a8
APickledWalrus <apickledwalrus@gmail.com>
2022-10-03 21:55:28 -0400

    Maybe fix it?

--- src/main/java/ch/njol/skript/lang/SkriptParser.java
+++ src/main/java/ch/njol/skript/lang/SkriptParser.java
@@ -60,6 +60,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.MatchResult;
