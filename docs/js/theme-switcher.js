8a4bac123a7225cc305b602dd53309bda0422e3b
Ayham Al-Ali <alali_ayham@yahoo.com>
2022-10-28 15:35:29 +0200

    Add Cookies Accept Bottom Bar
    - Cleaned up functions to be in their own file to remove duplicated code in theme-switcher.js and main.js

--- docs/js/theme-switcher.js
+++ docs/js/theme-switcher.js
@@ -2,57 +2,6 @@
 // This file is made to fix theme flicker at load due to 'defer' in main.js loading
 // 
 
-// A quick fix to not use modules due to CORS on localhost
-function setStorageItem(item, value, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  localStorage.setItem(item, value + "; " + d.toUTCString());
-}
-
-function removeStorageItem(item) {
-  localStorage.removeItem(item)
-}
-
-function getStorageItem(item, noExpireationCheck = false) {
-  let result = localStorage.getItem(item);
-  if (!noExpireationCheck) {
-    let expires;
-    expires, result = result.split("; ")[1], result.split("; ")[0];
-    if (expires) { // item with no expiration date
-      if (new Date(expires) < new Date()) {
-        removeStorageItem(item);
-        return null;
-      }
-    }
-  }
-  return result;
-}
-
-
-// <> Cookies
-function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  let expires = "expires=" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/; SameSite=None; Secure";
-}
-
-function getCookie(cname) {
-  let name = cname + "=";
-  let ca = document.cookie.split(';');
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
-// Cookies </>
-
 // Auto load DarkMode from cookies
 if (getCookie("darkMode") == "false") {
   document.body.setAttribute('data-theme', 'white')
@@ -63,10 +12,10 @@ if (getCookie("darkMode") == "false") {
   // Auto load from system theme
   // const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
   // if (darkThemeMq.matches) {
-    //   document.body.removeAttribute('data-theme');
-    // } else {
-      //   document.body.setAttribute('data-theme', 'white')
-      // }
+  //     document.body.removeAttribute('data-theme');
+  //   } else {
+  //       document.body.setAttribute('data-theme', 'white')
+  //     }
     }
 
 setTimeout(() => {
@@ -82,4 +31,4 @@ setTimeout(() => {
       setCookie("darkMode", "true", 99);
     }
   });
-}, 500); // For some reason this wouldn't work in index.html (only) unless I add some delay o.O
+}, 500); // For some reason this wouldn't work in index.html (only) unless some delay is added o.O
