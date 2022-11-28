0e2cf86eaa410ea6331659fb15c71b96f49001c7
kiip1 <25848425+kiip1@users.noreply.github.com>
2022-11-28 22:36:28 +0100

    Allow modifications of real max players

--- src/main/java/ch/njol/skript/expressions/ExprMaxPlayers.java
+++ src/main/java/ch/njol/skript/expressions/ExprMaxPlayers.java
@@ -18,17 +18,12 @@
  */
 package ch.njol.skript.expressions;
 
-import org.bukkit.Bukkit;
-import org.bukkit.event.Event;
-import org.bukkit.event.server.ServerListPingEvent;
-import org.eclipse.jdt.annotation.Nullable;
-
-import com.destroystokyo.paper.event.server.PaperServerListPingEvent;
 import ch.njol.skript.Skript;
 import ch.njol.skript.classes.Changer.ChangeMode;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.RequiredPlugins;
 import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
@@ -36,13 +31,20 @@ import ch.njol.skript.lang.SkriptParser;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
 import ch.njol.util.coll.CollectionUtils;
+import com.destroystokyo.paper.event.server.PaperServerListPingEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.event.Event;
+import org.bukkit.event.server.ServerListPingEvent;
+import org.eclipse.jdt.annotation.Nullable;
 
 @Name("Max Players")
 @Description({"The count of max players. This can be changed in a <a href='events.html#server_list_ping'>server list ping</a> event only.",
-		"'real max players' returns the real count of max players of the server always and can't be changed."})
+		"'real max players' returns the real count of max players of the server and can be modified on Paper 1.16 or later."})
 @Examples({"on server list ping:",
 		"	set the max players count to (online players count + 1)"})
-@Since("2.3")
+@RequiredPlugins("Paper 1.16+ (modify max real players)")
+@Since("2.3, INSERT VERSION (modify max real players)")
 public class ExprMaxPlayers extends SimpleExpression<Long> {
 
 	static {
@@ -52,6 +54,7 @@ public class ExprMaxPlayers extends SimpleExpression<Long> {
 	}
 
 	private static final boolean PAPER_EVENT_EXISTS = Skript.classExists("com.destroystokyo.paper.event.server.PaperServerListPingEvent");
+	private static final boolean SET_MAX_PLAYERS_EXISTS = Skript.methodExists(Server.class, "setMaxPlayers", int.class);
 
 	private boolean isReal;
 
@@ -59,10 +62,12 @@ public class ExprMaxPlayers extends SimpleExpression<Long> {
 	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
 		boolean isServerPingEvent = getParser().isCurrentEvent(ServerListPingEvent.class) ||
 				(PAPER_EVENT_EXISTS && getParser().isCurrentEvent(PaperServerListPingEvent.class));
+		
 		if (parseResult.mark == 2 && !isServerPingEvent) {
 			Skript.error("The 'shown' max players count expression can't be used outside of a server list ping event");
 			return false;
 		}
+		
 		isReal = (parseResult.mark == 0 && !isServerPingEvent) || parseResult.mark == 1;
 		return true;
 	}
@@ -73,52 +78,72 @@ public class ExprMaxPlayers extends SimpleExpression<Long> {
 		if (!isReal && !(e instanceof ServerListPingEvent))
 			return null;
 
-		if (isReal)
-			return CollectionUtils.array((long) Bukkit.getMaxPlayers());
-		else
-			return CollectionUtils.array((long) ((ServerListPingEvent) e).getMaxPlayers());
+		if (isReal) return CollectionUtils.array((long) Bukkit.getMaxPlayers());
+		else return CollectionUtils.array((long) ((ServerListPingEvent) e).getMaxPlayers());
 	}
 
 	@Override
 	@Nullable
 	public Class<?>[] acceptChange(ChangeMode mode) {
-		if (!isReal) {
-			if (getParser().getHasDelayBefore().isTrue()) {
-				Skript.error("Can't change the fake max players count anymore after the server list ping event has already passed");
-				return null;
-			}
-			switch (mode) {
-				case SET:
-				case ADD:
-				case REMOVE:
-				case DELETE:
-				case RESET:
-					return CollectionUtils.array(Number.class);
-			}
+		if (!isReal && getParser().getHasDelayBefore().isTrue()) {
+			Skript.error("Can't change the fake max players count anymore after the server list ping event has already passed");
+			return null;
 		}
+		
+		if (isReal && !SET_MAX_PLAYERS_EXISTS) {
+			Skript.error("Modifying real max player count is not supported on this server version");
+			return null;
+		}
+		
+		switch (mode) {
+			case SET:
+			case ADD:
+			case REMOVE:
+				return CollectionUtils.array(Number.class);
+			case RESET:
+			case DELETE:
+				if (!isReal) return CollectionUtils.array(Number.class);
+		}
+		
 		return null;
 	}
 
 	@SuppressWarnings("null")
 	@Override
 	public void change(Event e, @Nullable Object[] delta, ChangeMode mode) {
-		if (!(e instanceof ServerListPingEvent))
+		int amount = ((Number) delta[0]).intValue();
+		
+		if (!isReal && !(e instanceof ServerListPingEvent))
 			return;
 
-		ServerListPingEvent event = (ServerListPingEvent) e;
-		switch (mode) {
-			case SET:
-				event.setMaxPlayers(((Number) delta[0]).intValue());
-				break;
-			case ADD:
-				event.setMaxPlayers(event.getMaxPlayers() + ((Number) delta[0]).intValue());
-				break;
-			case REMOVE:
-				event.setMaxPlayers(event.getMaxPlayers() - ((Number) delta[0]).intValue());
-				break;
-			case DELETE:
-			case RESET:
-				event.setMaxPlayers(Bukkit.getMaxPlayers());
+		if (isReal) {
+			switch (mode) {
+				case SET:
+					Bukkit.setMaxPlayers(amount);
+					break;
+				case ADD:
+					Bukkit.setMaxPlayers(Bukkit.getMaxPlayers() + amount);
+					break;
+				case REMOVE:
+					Bukkit.setMaxPlayers(Bukkit.getMaxPlayers() - amount);
+					break;
+			}
+		} else {
+			ServerListPingEvent event = (ServerListPingEvent) e;
+			switch (mode) {
+				case SET:
+					event.setMaxPlayers(amount);
+					break;
+				case ADD:
+					event.setMaxPlayers(event.getMaxPlayers() + amount);
+					break;
+				case REMOVE:
+					event.setMaxPlayers(event.getMaxPlayers() - amount);
+					break;
+				case DELETE:
+				case RESET:
+					event.setMaxPlayers(Bukkit.getMaxPlayers());
+			}
 		}
 	}
 
