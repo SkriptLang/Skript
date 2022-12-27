1928dac3dc1bdbef4cd1e01747b53645b76fb85e
APickledWalrus <apickledwalrus@gmail.com>
2022-12-27 04:54:14 -0500

    Improve event values

--- src/main/java/ch/njol/skript/classes/data/BukkitEventValues.java
+++ src/main/java/ch/njol/skript/classes/data/BukkitEventValues.java
@@ -1210,12 +1210,20 @@ public final class BukkitEventValues {
 		//PlayerEditBookEvent
 		EventValues.registerEventValue(PlayerEditBookEvent.class, ItemStack.class, new Getter<ItemStack, PlayerEditBookEvent>() {
 			@Override
-			public ItemStack get(PlayerEditBookEvent e) {
+			public ItemStack get(PlayerEditBookEvent event) {
 				ItemStack book = new ItemStack(Material.WRITABLE_BOOK);
-				book.setItemMeta(e.getNewBookMeta());
+				book.setItemMeta(event.getPreviousBookMeta());
 				return book;
 			}
-		}, 0);
+		}, EventValues.TIME_PAST);
+		EventValues.registerEventValue(PlayerEditBookEvent.class, ItemStack.class, new Getter<ItemStack, PlayerEditBookEvent>() {
+			@Override
+			public ItemStack get(PlayerEditBookEvent event) {
+				ItemStack book = new ItemStack(Material.WRITABLE_BOOK);
+				book.setItemMeta(event.getNewBookMeta());
+				return book;
+			}
+		}, EventValues.TIME_FUTURE);
 		//ItemDespawnEvent
 		EventValues.registerEventValue(ItemDespawnEvent.class, Item.class, new Getter<Item, ItemDespawnEvent>() {
 			@Override
