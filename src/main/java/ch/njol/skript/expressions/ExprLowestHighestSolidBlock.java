45044a08a2127ef02bd346668e863cca1103e683
Patrick Miller <apickledwalrus@gmail.com>
2022-12-27 10:10:55 -0500

    Remove unused import

--- src/main/java/ch/njol/skript/expressions/ExprLowestHighestSolidBlock.java
+++ src/main/java/ch/njol/skript/expressions/ExprLowestHighestSolidBlock.java
@@ -19,7 +19,6 @@
 package ch.njol.skript.expressions;
 
 import ch.njol.skript.Skript;
-import ch.njol.skript.bukkitutil.ItemUtils;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
