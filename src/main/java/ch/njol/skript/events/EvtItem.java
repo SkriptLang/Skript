1b43fc32c054ae4f46f4f7d3b8d99dbd839bbf62
Shane Bee <shanebolenback@me.com>
2023-01-10 07:26:10 -0800

    Update src/main/java/ch/njol/skript/events/EvtItem.java
    Co-authored-by: _tud <mmbakkar06@gmail.com>

--- src/main/java/ch/njol/skript/events/EvtItem.java
+++ src/main/java/ch/njol/skript/events/EvtItem.java
@@ -63,7 +63,7 @@ public class EvtItem extends SkriptEvent {
 						"\tbroadcast \"Someone dropped an iron sword!\"")
 				.since("<i>unknown</i> (before 2.1)");
 		Skript.registerEvent("Drop", EvtItem.class, CollectionUtils.array(PlayerDropItemEvent.class, EntityDropItemEvent.class),
-				"[player|1:entity] drop[ing] [[of] %-itemtypes%]")
+				"[player|1:entity] drop[ping] [[of] %-itemtypes%]")
 				.description("Called when a player drops an item from their inventory, or an entity drops an item, such as a chicken laying an egg.")
 				.examples("on drop:",
 						"\tif event-item is compass:",
