0cd42a2f5c341ec5d9804d7737bb05450699a487
ShaneBee <shanebolenback@me.com>
2023-01-18 10:22:40 -0800

    ExprPlayerViewDistance - cleanup

--- src/main/java/ch/njol/skript/expressions/ExprPlayerViewDistance.java
+++ src/main/java/ch/njol/skript/expressions/ExprPlayerViewDistance.java
@@ -34,10 +34,12 @@ import org.bukkit.event.Event;
 import org.eclipse.jdt.annotation.Nullable;
 
 @Name("View Distance")
-@Description({"The view distance of a player. Can be changed.",
+@Description({
+	"The view distance of a player. Can be changed.",
 	"NOTE: This may not work on some versions (such as MC 1.14.x).",
 	"The return value in this case will be the view distance set in system.properties."})
-@Examples({"set view distance of player to 10", "set {_view} to view distance of player",
+@Examples({
+	"set view distance of player to 10", "set {_view} to view distance of player",
 	"reset view distance of all players", "add 2 to view distance of player"})
 @RequiredPlugins("Paper")
 @Since("2.4")
