61916bb3a9390136245c12346296d353b739783f
cooffeeRequired <106232282+cooffeeRequired@users.noreply.github.com>
2023-01-20 23:32:44 +0100

    item is stackable
    Returns true if item can be stacked

---
+++ src/main/java/ch/njol/skript/conditions/CondIsStackable.java
@@ -0,0 +1,50 @@
+/**
+ *   This file is part of Skript.
+ * <p>
+ *  Skript is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * <p>
+ *  Skript is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * <p>
+ *  You should have received a copy of the GNU General Public License
+ *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * Copyright Peter Güttinger, SkriptLang team and contributors
+ */
+
+package ch.njol.skript.conditions;
+
+
+import ch.njol.skript.aliases.ItemType;
+import ch.njol.skript.conditions.base.PropertyCondition;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+
+@Name("is Stackable")
+@Description("Check if item is stackable.")
+@Examples("%item% is stackable")
+@Since("INSERT VERSION")
+
+public class CondIsStackable extends PropertyCondition<ItemType> {
+
+	static {
+		register(CondIsStackable.class, "stackable", "itemtypes");
+	}
+
+	@Override
+	public boolean check(ItemType itemType) {
+		return itemType.getMaterial().getMaxStackSize() > 1;
+	}
+
+	@Override
+	protected String getPropertyName() {
+		return "stackable";
+	}
+}
