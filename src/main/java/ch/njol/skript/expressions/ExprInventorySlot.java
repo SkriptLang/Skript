ff19779c2310dd1e009803b234d5b92e66702ec2
DelayedGaming <72163224+DelayedGaming@users.noreply.github.com>
2023-01-20 18:38:53 +0800

    test commit

--- src/main/java/ch/njol/skript/expressions/ExprInventorySlot.java
+++ src/main/java/ch/njol/skript/expressions/ExprInventorySlot.java
@@ -42,7 +42,7 @@ import ch.njol.skript.util.slot.Slot;
 import ch.njol.util.Kleenean;
 
 @Name("Inventory Slot")
-@Description({"Represents a slot in an inventory. It can be used to change the item in an inventory too."})
+@Description("Represents a slot in an inventory. It can be used to change the item in an inventory too.")
 @Examples({
 	"if slot 0 of player is air:",
 	"\tset slot 0 of player to 2 stones",
