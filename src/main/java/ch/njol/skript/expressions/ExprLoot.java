be3a375b6a4f0169b5c5ddf6d3a70baed2664d22
_tud <mmbakkar06@gmail.com>
2023-01-20 23:37:04 +0300

    Remove * imports

--- src/main/java/ch/njol/skript/expressions/ExprLoot.java
+++ src/main/java/ch/njol/skript/expressions/ExprLoot.java
@@ -20,7 +20,11 @@ package ch.njol.skript.expressions;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.classes.Changer.ChangeMode;
-import ch.njol.skript.doc.*;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.RequiredPlugins;
+import ch.njol.skript.doc.Since;
 import ch.njol.util.coll.CollectionUtils;
 import org.bukkit.inventory.ItemStack;
 import ch.njol.skript.lang.Expression;
