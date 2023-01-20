a64ca43d78d3783f74a67df065a987a9ff7b30a9
coffeeRequired <106232282+cooffeeRequired@users.noreply.github.com>
2023-01-20 23:03:03 +0100

    Update DefaultFunctions.java

--- src/main/java/ch/njol/skript/classes/data/DefaultFunctions.java
+++ src/main/java/ch/njol/skript/classes/data/DefaultFunctions.java
@@ -78,8 +78,8 @@ public class DefaultFunctions {
 				return new Number[] {(sum/params[0].length)};
 			}
 		}.description("Returns the average of all numbers in the list")
-			.examples("average(10,20) = 15", "average(11,99) = 41", "average(10,190,299.20) = 166.2")
-			.since("2.6.4"));
+			.examples("average(10, 20) = 15", "average(11, 99) = 41", "average(10, 190, 299.20) = 166.2")
+			.since("INSERT VERSION"));
 		
 		Functions.registerFunction(new SimpleJavaFunction<Long>("floor", numberParam, DefaultClasses.LONG, true) {
 			@Override
