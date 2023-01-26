8748f77e0bf7e1781c50fe6e33c75fb50abdaa21
Shane Bee <shanebolenback@me.com>
2023-01-25 21:45:05 -0800

    Update src/main/java/ch/njol/skript/expressions/ExprPlayerViewDistance.java
    Co-authored-by: Patrick Miller <apickledwalrus@gmail.com>

--- src/main/java/ch/njol/skript/expressions/ExprPlayerViewDistance.java
+++ src/main/java/ch/njol/skript/expressions/ExprPlayerViewDistance.java
@@ -37,7 +37,8 @@ import org.eclipse.jdt.annotation.Nullable;
 @Description({
 	"The view distance of a player. Can be changed.",
 	"NOTE: This may not work on some versions (such as MC 1.14.x).",
-	"The return value in this case will be the view distance set in system.properties."})
+	"The return value in this case will be the view distance set in system.properties."
+})
 @Examples({
 	"set view distance of player to 10", "set {_view} to view distance of player",
 	"reset view distance of all players", "add 2 to view distance of player"})
