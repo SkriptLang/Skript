369303aa02bb4ee106410eeb46c744f3a3926f15
Fusezion <fusezionstream@gmail.com>
2023-01-27 15:28:55 -0500

    Apply suggestions from code review
    Co-authored-by: Ayham Al Ali <alali_ayham@yahoo.com>

--- src/main/java/ch/njol/skript/effects/EffWorldUnload.java
+++ src/main/java/ch/njol/skript/effects/EffWorldUnload.java
@@ -33,7 +33,7 @@ import org.bukkit.event.Event;
 import org.eclipse.jdt.annotation.Nullable;
 
 @Name("Unload World")
-@Description({"Unload a given world with optional saving"})
+@Description("Unload a given world with optional saving")
 @Examples({
 	"unload \"world_nether\" and save",
 	"unload \"world_the_end\" and don't save",
