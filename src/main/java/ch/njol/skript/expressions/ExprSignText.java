9c7cc1f62d5f2e85a6dc3ace96eb4a07e9e0de41
DelayedGaming <simmz0403@gmail.com>
2023-02-20 22:30:26 +0800

    Initial (final)

--- src/main/java/ch/njol/skript/expressions/ExprSignText.java
+++ src/main/java/ch/njol/skript/expressions/ExprSignText.java
@@ -18,12 +18,6 @@
  */
 package ch.njol.skript.expressions;
 
-import org.bukkit.block.Block;
-import org.bukkit.block.Sign;
-import org.bukkit.event.Event;
-import org.bukkit.event.block.SignChangeEvent;
-import org.eclipse.jdt.annotation.Nullable;
-
 import ch.njol.skript.Skript;
 import ch.njol.skript.aliases.Aliases;
 import ch.njol.skript.aliases.ItemType;
@@ -32,145 +26,203 @@ import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
-import ch.njol.skript.effects.Delay;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.skript.lang.util.SimpleLiteral;
+import ch.njol.skript.util.Utils;
 import ch.njol.util.Kleenean;
+import ch.njol.util.coll.CollectionUtils;
 
-/**
- * @author Peter Güttinger
- */
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
+import org.bukkit.event.Event;
+import org.bukkit.event.block.SignChangeEvent;
+import org.eclipse.jdt.annotation.Nullable;
+
 @Name("Sign Text")
 @Description("A line of text on a sign. Can be changed, but remember that there is a 16 character limit per line (including color codes that use 2 characters each).")
-@Examples({"on rightclick on sign:",
-		"	line 2 of the clicked block is \"[Heal]\":",
-		"		heal the player",
-		"	set line 3 to \"%player%\""})
+@Examples({
+		"on rightclick on sign:",
+		"\tline 2 of the clicked block is \"[Heal]\":",
+		"\t\theal the player",
+		"\tset line 3 of block at {_sign} to \"%player%\""
+})
 @Since("1.3")
 public class ExprSignText extends SimpleExpression<String> {
-//	static {
-//		Skript.registerExpression(ExprSignText.class, String.class, ExpressionType.PROPERTY,
-//				"[the] line %number% [of %block%]", "[the] (1¦1st|1¦first|2¦2nd|2¦second|3¦3rd|3¦third|4¦4th|4¦fourth) line [of %block%]");
-//	}
-	
+
+	static {
+		Skript.registerExpression(ExprSignText.class, String.class, ExpressionType.PROPERTY,
+			"[the] lines [of %block%]",
+			"[the] line %number% [of %block%]",
+			"[the] (1¦1st|1¦first|2¦2nd|2¦second|3¦3rd|3¦third|4¦4th|4¦fourth) line [of %block%]"
+			);
+	}
+
 	private static final ItemType sign = Aliases.javaItemType("sign");
-	
+
+	private boolean isLines;
 	@SuppressWarnings("null")
 	private Expression<Number> line;
 	@SuppressWarnings("null")
-	private Expression<Block> block;
-	
-	@SuppressWarnings({"unchecked", "null"})
+	private Expression<Block> exprBlock;
+	private boolean delayed;
+
 	@Override
-	public boolean init(final Expression<?>[] exprs, final int matchedPattern, final Kleenean isDelayed, final ParseResult parseResult) {
-		if (matchedPattern == 0)
-			line = (Expression<Number>) exprs[0];
-		else
-			line = new SimpleLiteral<>(parseResult.mark, false);
-		block = (Expression<Block>) exprs[exprs.length - 1];
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+		isLines = (matchedPattern == 0);
+		if (!isLines)
+			line = matchedPattern == 1 ? (Expression<Number>) exprs[0] : new SimpleLiteral<>(parseResult.mark, false);
+		exprBlock = (Expression<Block>) exprs[exprs.length - 1];
+		delayed = isDelayed == Kleenean.TRUE;
 		return true;
 	}
-	
+
+	@Override
+	@Nullable
+	protected String[] get(Event event) {
+		Block block = exprBlock.getSingle(event);
+		if (isLines) {
+			if (block == null || !sign.isOfType(block))
+				return new String[0];
+			if ((event instanceof SignChangeEvent && getTime() >= 0 && block.equals(((SignChangeEvent) event).getBlock()) && !delayed))
+				return ((SignChangeEvent) event).getLines();
+			return ((Sign) (block.getState())).getLines();
+		} else {
+			Number lineNumber = line.getSingle(event);
+			if (lineNumber == null || lineNumber.intValue() < 1 || lineNumber.intValue() > 4)
+				return new String[0];
+			int index = lineNumber.intValue() - 1;
+			if (event instanceof SignChangeEvent && getTime() >= 0 && block == ((SignChangeEvent) event).getBlock() && !delayed) {
+				return CollectionUtils.array(((SignChangeEvent) event).getLine(index));
+			}
+			if (block == null || !sign.isOfType(block)) {
+				return new String[0];
+			}
+			return CollectionUtils.array(
+				((Sign) (block.getState())).getLine(index)
+			);
+		}
+	}
+
+	@Override
+	@Nullable
+	public Class<?>[] acceptChange(final ChangeMode mode) {
+		if (isLines) {
+			if (mode == ChangeMode.RESET) {
+				return null;
+			}
+			return CollectionUtils.array(String[].class);
+		} else if (mode == ChangeMode.SET || mode == ChangeMode.DELETE) {
+			return CollectionUtils.array(String.class);
+		}
+		return null;
+	}
+
+	@Override
+	public void change(Event event, Object @Nullable [] delta, ChangeMode mode) {
+		if (delta == null) {
+			if (mode != ChangeMode.DELETE && mode != ChangeMode.RESET)
+				return;
+		}
+		Block block = exprBlock.getSingle(event);
+		if (block == null)
+			return;
+		if (isLines) {
+			if (!sign.isOfType(block))
+				return;
+			Sign state = (Sign) block.getState();
+			switch (mode) {
+				case ADD:
+					for (String text : (String[]) delta) {
+						int index = 0;
+						while (!state.getLine(index).equals("")) {
+							index++;
+							if (index > 3)
+								break;
+						}
+						if (index <= 3)
+							state.setLine(index, Utils.replaceChatStyles(text));
+					}
+					break;
+				case DELETE:
+					for (int index = 0; index < 4; index++) {
+						state.setLine(index, "");
+					}
+					break;
+				case REMOVE:
+					for (String text : (String[]) delta) {
+						for (int index = 0; index <= 3; index++) {
+							if (state.getLine(index).equals(Utils.replaceChatStyles(text))) {
+								state.setLine(index, "");
+								break;
+							}
+						}
+					}
+					break;
+				case REMOVE_ALL:
+					for (String text : (String[]) delta) {
+						for (int index = 0; index <= 3; index++) {
+							if (state.getLine(index).equals(Utils.replaceChatStyles(text))) {
+								state.setLine(index, "");
+							}
+						}
+					}
+					break;
+				case SET:
+					for (int index = 0; index < Math.min(4, delta.length); index++) {
+						state.setLine(index, Utils.replaceChatStyles((String) delta[index]));
+					}
+					break;
+			}
+			state.update(false, false);
+		} else {
+			String text = "";
+			if (mode == ChangeMode.SET)
+				text = Utils.replaceChatStyles((String) delta[0]);
+			Number lineNumber = line.getSingle(event);
+			if (lineNumber == null || lineNumber.intValue() < 1 || lineNumber.intValue() > 4)
+				return;
+			int index = lineNumber.intValue() - 1;
+			if (event instanceof SignChangeEvent && getTime() >= 0 && block.equals(((SignChangeEvent) event).getBlock()) && !delayed) {
+				((SignChangeEvent) event).setLine(index, text);
+			} else {
+				if (!sign.isOfType(block)) {
+					return;
+				}
+				Sign state = (Sign) block.getState();
+				switch (mode) {
+					case SET:
+						state.setLine(index, text);
+						break;
+					case DELETE:
+						state.setLine(index, "");
+						break;
+				}
+				state.update(false, false);
+			}
+		}
+	}
+
 	@Override
 	public boolean isSingle() {
-		return true;
+		return !isLines;
 	}
-	
+
 	@Override
 	public Class<? extends String> getReturnType() {
 		return String.class;
 	}
-	
-	@Override
-	@Nullable
-	protected String[] get(final Event e) {
-		final Number l = line.getSingle(e);
-		if (l == null)
-			return new String[0];
-		final int line = l.intValue() - 1;
-		if (line < 0 || line > 3)
-			return new String[0];
-		if (getTime() >= 0 && block.isDefault() && e instanceof SignChangeEvent && !Delay.isDelayed(e)) {
-			return new String[] {((SignChangeEvent) e).getLine(line)};
-		}
-		final Block b = block.getSingle(e);
-		if (b == null)
-			return new String[0];
-		if (!sign.isOfType(b))
-			return new String[0];
-		return new String[] {((Sign) b.getState()).getLine(line)};
-	}
-	
-	@Override
-	public String toString(final @Nullable Event e, final boolean debug) {
-		return "line " + line.toString(e, debug) + " of " + block.toString(e, debug);
-	}
-	
-	// TODO allow add, remove, and remove all (see ExprLore)
-	@Override
-	@Nullable
-	public Class<?>[] acceptChange(final ChangeMode mode) {
-		if (mode == ChangeMode.DELETE || mode == ChangeMode.SET)
-			return new Class[] {String.class};
-		return null;
-	}
-	
-	static boolean hasUpdateBooleanBoolean = true;
-	
-	@SuppressWarnings("incomplete-switch")
-	@Override
-	public void change(final Event e, final @Nullable Object[] delta, final ChangeMode mode) throws UnsupportedOperationException {
-		final Number l = line.getSingle(e);
-		if (l == null)
-			return;
-		final int line = l.intValue() - 1;
-		if (line < 0 || line > 3)
-			return;
-		final Block b = block.getSingle(e);
-		if (b == null)
-			return;
-		if (getTime() >= 0 && e instanceof SignChangeEvent && b.equals(((SignChangeEvent) e).getBlock()) && !Delay.isDelayed(e)) {
-			switch (mode) {
-				case DELETE:
-					((SignChangeEvent) e).setLine(line, "");
-					break;
-				case SET:
-					assert delta != null;
-					((SignChangeEvent) e).setLine(line, (String) delta[0]);
-					break;
-			}
-		} else {
-			if (!sign.isOfType(b))
-				return;
-			final Sign s = (Sign) b.getState();
-			switch (mode) {
-				case DELETE:
-					s.setLine(line, "");
-					break;
-				case SET:
-					assert delta != null;
-					s.setLine(line, (String) delta[0]);
-					break;
-			}
-			if (hasUpdateBooleanBoolean) {
-				try {
-					s.update(false, false);
-				} catch (final NoSuchMethodError err) {
-					hasUpdateBooleanBoolean = false;
-					s.update();
-				}
-			} else {
-				s.update();
-			}
-		}
-	}
-	
+
 	@Override
 	public boolean setTime(final int time) {
-		return super.setTime(time, SignChangeEvent.class, block);
+		return super.setTime(time, SignChangeEvent.class, exprBlock);
 	}
-	
+
+	@Override
+	public String toString(@Nullable Event event, boolean debug) {
+		return (isLines ? "lines" : "line " + line.toString(event, debug)) + " of " + exprBlock.toString(event, debug);
+	}
+
 }
