f282275becc0d258c254426a38908c9c4d31584b
DelayedGaming <simmz0403@gmail.com>
2023-03-31 01:11:50 +0800

    Enhance (finish)

--- src/main/java/ch/njol/skript/effects/EffShear.java
+++ src/main/java/ch/njol/skript/effects/EffShear.java
@@ -27,44 +27,50 @@ import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.Kleenean;
+
 import io.papermc.paper.entity.Shearable;
+
 import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.MushroomCow;
 import org.bukkit.entity.Sheep;
 import org.bukkit.entity.Snowman;
 import org.bukkit.event.Event;
 import org.eclipse.jdt.annotation.Nullable;
 
-import java.util.Random;
-
 @Name("Shear")
-@Description("Shears or 'un-shears' a sheep. Please note that no wool is dropped, this only sets the 'sheared' state of the sheep.")
-@Examples({"on rightclick on a sheep holding a sword:",
-		"	shear the clicked sheep"})
-@Since("2.0")
+@Description({
+	"Shears or un-shears a shearable entity with drops by shearing and a 'sheared' sound. Using with 'force' will force this effect despite the entity's 'shear state'.",
+	"\nPlease note that..:",
+	"\n- If your server is not running with Paper 1.19.4 or higher, this effect will only change its 'shear state', and the 'force' effect is unavailable",
+	"\n- Force-shearing or un-shearing on a sheared mushroom cow is not possible"
+})
+@Examples({
+	"on rightclick on a sheep holding a sword:",
+		"\tshear the clicked sheep",
+		"\tchance of 10%",
+		"\tforce shear the clicked sheep"
+})
+@Since("2.0, INSERT VERSION (shearable)")
 public class EffShear extends Effect {
 
 	private static final boolean interfaceMethod = Skript.classExists("io.papermc.paper.entity.Shearable");
-	private static final boolean snowmanMethod = Skript.methodExists(Snowman.class, "setDerp", boolean.class);
-	private static final boolean mooshroomMethod = Skript.methodExists(MushroomCow.class, "setVariant", MushroomCow.Variant.class);
 
 	static {
 		Skript.registerEffect(EffShear.class,
-				(interfaceMethod ? "shear %livingentities% [nodrops:(without drops)]" : "shear %livingentities%"),
+				(interfaceMethod ? "[:force] shear %livingentities%" : "shear %livingentities%"),
 				"un[-]shear %livingentities%");
 	}
 	
 	@SuppressWarnings("null")
 	private Expression<LivingEntity> entity;
+	private boolean force;
 	private boolean shear;
-	private boolean drops;
 
 	@Override
 	@SuppressWarnings("unchecked")
 	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
 		entity = (Expression<LivingEntity>) exprs[0];
+		force = parseResult.hasTag("force");
 		shear = matchedPattern == 0;
-		drops = !parseResult.hasTag("nodrops");
 		return true;
 	}
 	
@@ -72,28 +78,25 @@ public class EffShear extends Effect {
 	protected void execute(Event event) {
 		for (LivingEntity entity : entity.getArray(event)) {
 			if (shear && interfaceMethod) {
-				if (drops && entity instanceof Shearable) {
-					((Shearable) entity).shear();
-				}
+				if (!(entity instanceof Shearable))
+					continue;
+				Shearable shearable = ((Shearable) entity);
+				if (!force && !shearable.readyToBeSheared())
+					continue;
+				shearable.shear();
+				continue;
 			}
 			if (entity instanceof Sheep) {
 				((Sheep) entity).setSheared(shear);
-			} else if (mooshroomMethod) {
-				if (!shear && entity instanceof MushroomCow) {
-					int rng = new Random().nextInt(MushroomCow.Variant.values().length);
-					((MushroomCow) entity).setVariant(MushroomCow.Variant.values()[rng]);
-				}
-			} else if (snowmanMethod) {
-				if (!shear && entity instanceof Snowman) {
-					((Snowman) entity).setDerp(false);
-				}
+			} else if (entity instanceof Snowman) {
+				((Snowman) entity).setDerp(shear);
 			}
 		}
 	}
 	
 	@Override
 	public String toString(@Nullable Event event, boolean debug) {
-		return (shear ? "" : "un") + "shear " + entity.toString(event, debug) + (interfaceMethod && drops ? " with drops" : "");
+		return (shear ? "" : "un") + "shear " + entity.toString(event, debug);
 	}
 	
 }
