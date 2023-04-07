243d402cd0937b144ebfb6f2aae3f384bcdf0a64
Fusezion <fusezionstream@gmail.com>
2023-04-07 12:28:39 -0400

    Added Adult Syntax
    :sparkles: Added Condition for checking is adult
    :sparkles: Added Effect to make an entity a baby or adult
    :clipboard: Added test for these syntaxes
    :large_blue_circle: 1.16 for any mob, below that is animals only

---
+++ src/main/java/ch/njol/skript/conditions/CondIsAdult.java
@@ -0,0 +1,61 @@
+/**
+ *   This file is part of Skript.
+ *
+ *  Skript is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Skript is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright Peter Güttinger, SkriptLang team and contributors
+ */
+package ch.njol.skript.conditions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.conditions.base.PropertyCondition;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import org.bukkit.entity.Ageable;
+import org.bukkit.entity.Animals;
+import org.bukkit.entity.LivingEntity;
+
+@Name("Is An Adult")
+@Description("Tells whether or not a living entity is an adult.")
+@Examples({
+	"on spawn:",
+	"\tif event-entity is not an adult:",
+	"\t\tmake event-entity an adult"
+})
+@Since("INSERT VERSION")
+public class CondIsAdult extends PropertyCondition<LivingEntity> {
+
+
+	static boolean HAS_MOB_SUPPORT = Skript.isRunningMinecraft(1,16,5);
+
+	static {
+		register(CondIsAdult.class, "[an] adult", "livingentities");
+	}
+
+	@Override
+	public boolean check(LivingEntity livingEntity) {
+		if (livingEntity instanceof Ageable)
+			if (HAS_MOB_SUPPORT || livingEntity instanceof Animals)
+				return ((Ageable) livingEntity).isAdult();
+		return false;
+	}
+
+	@Override
+	protected String getPropertyName() {
+		return "an adult";
+	}
+
+}
---
+++ src/main/java/ch/njol/skript/effects/EffMakeAdult.java
@@ -0,0 +1,84 @@
+/**
+ *   This file is part of Skript.
+ *
+ *  Skript is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Skript is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright Peter Güttinger, SkriptLang team and contributors
+ */
+package ch.njol.skript.effects;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.RequiredPlugins;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.util.Kleenean;
+import org.bukkit.entity.Ageable;
+import org.bukkit.entity.Animals;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.Event;
+import org.eclipse.jdt.annotation.Nullable;
+
+@Name("Make Adult")
+@Description("Force an entity to become an adult or a baby, requires MC 1.16 for any mob")
+@Examples({
+	"on spawn of mob:",
+	"\tif entity is not an adult:",
+	"\t\tmake entity an adult",
+})
+@Since("INSERT VERSION")
+@RequiredPlugins("1.13+ for animal, 1.16+ for mobs")
+public class EffMakeAdult extends Effect {
+
+
+	static boolean HAS_MOB_SUPPORT = Skript.isRunningMinecraft(1,16,5);
+
+	static {
+		Skript.registerEffect(EffMakeAdult.class, "make %livingentities% [a[n]] (adult|:baby)");
+	}
+
+	boolean makeBaby;
+	Expression<LivingEntity> entities;
+
+	@Override
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+		entities = (Expression<LivingEntity>) exprs[0];
+		makeBaby = parseResult.hasTag("baby");
+		return true;
+	}
+
+	@Override
+	protected void execute(Event event) {
+		for (LivingEntity livingEntity : entities.getArray(event)) {
+			if (livingEntity instanceof Ageable)
+				if (!HAS_MOB_SUPPORT && !(livingEntity instanceof Animals))
+					continue;
+				if (makeBaby) {
+					((Ageable) livingEntity).setBaby();
+				} else {
+					((Ageable) livingEntity).setAdult();
+				}
+		}
+	}
+
+	@Override
+	public String toString(@Nullable Event event, boolean debug) {
+		return "make " + entities + (makeBaby ? " a baby" : " an adult");
+	}
+
+}
