ceffb23d4bdd7a617402b8c9be893f29849e3def
Ayham Al Ali <20037329+AyhamAl-Ali@users.noreply.github.com>
2023-04-25 17:25:15 +0300

    Update src/main/java/ch/njol/skript/events/EvtClick.java
    Co-authored-by: Kiip <25848425+kiip1@users.noreply.github.com>

--- src/main/java/ch/njol/skript/events/EvtClick.java
+++ src/main/java/ch/njol/skript/events/EvtClick.java
@@ -67,8 +67,8 @@ public class EvtClick extends SkriptEvent {
 			PlayerInteractEvent.class, PlayerInteractEntityEvent.class, PlayerInteractAtEntityEvent.class
 		);
 		Skript.registerEvent("Click", EvtClick.class, eventTypes,
-				"[(" + RIGHT + "¦right|" + LEFT + "¦left)(| |-)][mouse(| |-)]click[ing] [on %-entitydata/itemtype%] [(with|using|holding) %-itemtype%]",
-				"[(" + RIGHT + "¦right|" + LEFT + "¦left)(| |-)][mouse(| |-)]click[ing] (with|using|holding) %itemtype% on %entitydata/itemtype%")
+				"[(" + RIGHT + ":right|" + LEFT + ":left)(| |-)][mouse(| |-)]click[ing] [on %-entitydata/itemtype%] [(with|using|holding) %-itemtype%]",
+				"[(" + RIGHT + ":right|" + LEFT + ":left)(| |-)][mouse(| |-)]click[ing] (with|using|holding) %itemtype% on %entitydata/itemtype%")
 				.description("Called when a user clicks on a block, an entity or air with or without an item in their hand.",
 						"Please note that rightclick events with an empty hand while not looking at a block are not sent to the server, so there's no way to detect them.",
 						"Also note that a leftclick on an entity is an attack and thus not covered by the 'click' event, but the 'damage' event.")
