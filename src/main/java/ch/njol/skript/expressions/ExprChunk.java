2ffc678c58e1faa68ef2478d9845ab8bae85fc7b
DelayedGaming <simmz0403@gmail.com>
2023-05-10 03:06:03 +0800

    Merge with ExprChunk

--- src/main/java/ch/njol/skript/expressions/ExprChunk.java
+++ src/main/java/ch/njol/skript/expressions/ExprChunk.java
@@ -18,68 +18,83 @@
  */
 package ch.njol.skript.expressions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import ch.njol.skript.Skript;
 import ch.njol.skript.classes.Changer.ChangeMode;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
-import ch.njol.skript.expressions.base.PropertyExpression;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.skript.util.Direction;
 import ch.njol.util.Kleenean;
 import org.bukkit.Chunk;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.event.Event;
-import org.eclipse.jdt.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
 
-/**
- * @author Peter Güttinger
- */
 @Name("Chunk")
-@Description("The <a href='./classes.html#chunk'>chunk</a> a block, location or entity is in.")
-@Examples("add the chunk at the player to {protected chunks::*}")
-@Since("2.0")
-public class ExprChunk extends PropertyExpression<Location, Chunk> {
+@Description("Returns the <a href='./classes.html#chunk'>chunk</a> of a block, location or entity is in, or a list of the loaded chunks of a world.")
+@Examples({
+	"add the chunk at the player to {protected chunks::*}",
+	"set {_chunks::*} to the loaded chunks of the player's world"
+})
+@Since("2.0, INSERT VERSION (loaded chunks)")
+public class ExprChunk extends SimpleExpression<Chunk> {
 	
 	static {
-		Skript.registerExpression(ExprChunk.class, Chunk.class, ExpressionType.PROPERTY, "[the] chunk[s] (of|%-directions%) %locations%", "%locations%'[s] chunk[s]");
+		Skript.registerExpression(ExprChunk.class, Chunk.class, ExpressionType.COMBINED,
+			"[the] chunk[s] (of|%-directions%) %locations%",
+			"%locations%'[s] chunk[s]",
+			"[all [of]] [the] loaded chunks (of|in) %worlds%"
+		);
 	}
-	
-	@SuppressWarnings("null")
+
+	private int pattern;
 	private Expression<Location> locations;
-	
-	@SuppressWarnings({"unchecked", "null"})
+	private Expression<World> worlds;
+
 	@Override
+	@SuppressWarnings("unchecked")
 	public boolean init(final Expression<?>[] exprs, final int matchedPattern, final Kleenean isDelayed, final ParseResult parseResult) {
-		if (matchedPattern == 0) {
+		pattern = matchedPattern;
+		if (pattern == 0) {
 			locations = (Expression<Location>) exprs[1];
-			if (exprs[0] != null)
+			if (exprs[0] != null) {
 				locations = Direction.combine((Expression<? extends Direction>) exprs[0], locations);
-		} else {
+			}
+		} else if (pattern == 1) {
 			locations = (Expression<Location>) exprs[0];
+		} else {
+			worlds = ((Expression<World>) exprs[0]);
 		}
-		setExpr(locations);
 		return true;
 	}
-	
+
 	@Override
-	protected Chunk[] get(final Event e, final Location[] source) {
-		return get(source, Location::getChunk);
+	protected @Nullable Chunk[] get(Event event) {
+		if (pattern != 2) {
+			return locations.stream(event)
+				.map(Location::getChunk)
+				.toArray(Chunk[]::new);
+		}
+		World[] worlds = this.worlds.getArray(event);
+		if (worlds.length == 0)
+			return new Chunk[0];
+		List<Chunk> chunks = new ArrayList<>();
+		for (World world : worlds) {
+			chunks.addAll(Arrays.asList(world.getLoadedChunks()));
+		}
+		return chunks.toArray(new Chunk[0]);
 	}
-	
-	@Override
-	public Class<? extends Chunk> getReturnType() {
-		return Chunk.class;
-	}
-	
-	@Override
-	public String toString(final @Nullable Event e, final boolean debug) {
-		return "the chunk at " + locations.toString(e, debug);
-	}
-	
+
 	@Override
 	@Nullable
 	public Class<?>[] acceptChange(final ChangeMode mode) {
@@ -87,14 +102,31 @@ public class ExprChunk extends PropertyExpression<Location, Chunk> {
 			return new Class[0];
 		return null;
 	}
-	
+
 	@Override
-	public void change(final Event e, final @Nullable Object[] delta, final ChangeMode mode) {
+	@SuppressWarnings("deprecation")
+	public void change(Event event, Object @Nullable [] delta, ChangeMode mode) {
 		assert mode == ChangeMode.RESET;
-		
-		final Chunk[] cs = getArray(e);
-		for (final Chunk c : cs)
-			c.getWorld().regenerateChunk(c.getX(), c.getZ());
+		Chunk[] chunks = get(event);
+		for (Chunk chunk : chunks)
+			chunk.getWorld().regenerateChunk(chunk.getX(), chunk.getZ());
+	}
+
+	@Override
+	public boolean isSingle() {
+		return false;
+	}
+
+	@Override
+	public Class<? extends Chunk> getReturnType() {
+		return Chunk.class;
 	}
 	
+	@Override
+	public String toString(@Nullable Event event, boolean debug) {
+		if (pattern == 2)
+			return "the loaded chunks of " + worlds.toString(event, debug);
+		return "the chunk at " + locations.toString(event, debug);
+	}
+
 }
