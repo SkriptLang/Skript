d58dbef05a7e6ebe98c52588cb972ba7823edec5
LimeGlass <16087552+TheLimeGlass@users.noreply.github.com>
2023-05-24 21:36:09 -0600

    Update ExprHotbarSlot.java

--- src/main/java/ch/njol/skript/expressions/ExprHotbarSlot.java
+++ src/main/java/ch/njol/skript/expressions/ExprHotbarSlot.java
@@ -41,7 +41,7 @@ import ch.njol.util.Kleenean;
 @Name("Hotbar Slot")
 @Description({
 	"The currently selected hotbar <a href='./classes.html#slot'>slot</a>.",
-  "To retrieve its number use <a href='#ExprSlotIndex'>Slot Index</a> expression.",
+	"To retrieve its number use <a href='#ExprSlotIndex'>Slot Index</a> expression.",
 	"Use future and past tense to grab the previous slot in an item change event, see example."
 })
 @Examples({
