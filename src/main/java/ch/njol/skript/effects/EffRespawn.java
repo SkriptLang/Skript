c646e379763ecdf888e3e19bc49f9ad1bf180735
Ayham Al-Ali <20037329+AyhamAl-Ali@users.noreply.github.com>
2023-09-13 14:46:27 +0300

    Fix other death event related effects/expressions

--- src/main/java/ch/njol/skript/effects/EffRespawn.java
+++ src/main/java/ch/njol/skript/effects/EffRespawn.java
@@ -62,7 +62,7 @@ public class EffRespawn extends Effect {
 		players = (Expression<Player>) exprs[0];
 		// Force a delay before respawning the player if we're in the death event and there isn't already a delay
 		// Unexpected behavior may occur if we don't do this
-		forceDelay = getParser().isCurrentEvent(PlayerDeathEvent.class) && isDelayed.isFalse();
+		forceDelay = getParser().isCurrentEvent(EntityDeathEvent.class) && isDelayed.isFalse();
 		return true;
 	}
 
