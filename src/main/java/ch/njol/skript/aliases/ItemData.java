c9e452297d8705c4a9b01dd10e2488b60abb394e
Pikachu920 <28607612+Pikachu920@users.noreply.github.com>
2023-12-20 00:14:58 -0600

    Remove useless import

--- src/main/java/ch/njol/skript/aliases/ItemData.java
+++ src/main/java/ch/njol/skript/aliases/ItemData.java
@@ -35,7 +35,6 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFactory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.BlockStateMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.eclipse.jdt.annotation.Nullable;
 
