35322948933aad9d6d5352cb6a6c0106728929ad
Fusezion <fusezionstream@gmail.com>
2023-12-30 15:31:28 -0500

    Use a stream Personally I'm not for this entirely Co-authored-by: TheLimeGlass <seantgrover@gmail.com>

--- src/main/java/ch/njol/skript/expressions/ExprRepeat.java
+++ src/main/java/ch/njol/skript/expressions/ExprRepeat.java
@@ -32,14 +32,14 @@ import ch.njol.util.StringUtils;
 import org.bukkit.event.Event;
 import org.eclipse.jdt.annotation.Nullable;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Name("Repeat String")
 @Description("Repeats inputted strings a given amount of times.")
 @Examples({
-	"broadcast \"Hello, World! \" repeated 2 times",
-	"broadcast nl and nl repeated 5 times"
+	"broadcast nl and nl repeated 200 times",
+	"broadcast \"Hello World \" repeated 5 times"
 })
 @Since("INSERT VERSION")
 public class ExprRepeat extends SimpleExpression<String> {
@@ -60,14 +60,12 @@ public class ExprRepeat extends SimpleExpression<String> {
 
 	@Override
 	protected @Nullable String[] get(Event event) {
-		List<String> newStrings = new ArrayList<>();
 		int repeatCount = this.repeatCount.getOptionalSingle(event).orElse(0);
 		if (repeatCount < 1)
 			return new String[0];
-		for (String string : strings.getArray(event)) {
-			newStrings.add(StringUtils.multiply(string, repeatCount));
-		}
-		return newStrings.toArray(new String[0]);
+		List<String> stringList = Arrays.stream(strings.getArray(event))
+			.map(string -> StringUtils.multiply(string, repeatCount)).toList();
+		return stringList.toArray(new String[0]);
 	}
 
 	@Override
@@ -82,6 +80,7 @@ public class ExprRepeat extends SimpleExpression<String> {
 
 	@Override
 	public String toString(@Nullable Event event, boolean debug) {
-		return strings.toString(event,debug) + " repeated " + repeatCount.toString(event,debug) + " times";
+		return strings.toString(event, debug) + " repeated " + repeatCount.toString(event, debug) + " times";
 	}
+
 }
