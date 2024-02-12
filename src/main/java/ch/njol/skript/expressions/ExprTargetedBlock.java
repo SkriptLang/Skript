92fd4a82799119d1b358bef91a8377ee64156289
ShaneBee <shanebolenback@me.com>
2024-02-11 18:29:06 -0800

    ExprTargetedBlock - apply a use for "actual"

--- src/main/java/ch/njol/skript/expressions/ExprTargetedBlock.java
+++ src/main/java/ch/njol/skript/expressions/ExprTargetedBlock.java
@@ -24,7 +24,7 @@ import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
-import ch.njol.skript.expressions.base.PropertyExpression;
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
@@ -32,43 +32,45 @@ import ch.njol.util.Kleenean;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
 import org.eclipse.jdt.annotation.Nullable;
 
 @Name("Targeted Block")
-@Description("The block at the crosshair. This regards all blocks that are not air as fully solid, e.g. torches will be like a solid stone block for this expression.")
+@Description({"The block at the crosshair. This regards all blocks that are not air as fully solid, e.g. torches will be like a solid stone block for this expression.",
+	"The actual target block will regard the actual hit box of the block."})
 @Examples({"# A command to set the block a player looks at to a specific type:",
-		"command /setblock &lt;material&gt;:",
-		"\ttrigger:",
-		"\t\tset targeted block to argument"})
+	"command /setblock &lt;material&gt;:",
+	"\ttrigger:",
+	"\t\tset targeted block to argument"})
 @Since("1.0")
-public class ExprTargetedBlock extends PropertyExpression<Player, Block> {
+public class ExprTargetedBlock extends SimplePropertyExpression<Player, Block> {
 
 	static {
 		Skript.registerExpression(ExprTargetedBlock.class, Block.class, ExpressionType.COMBINED,
-				"[the] target[ed] block[s] [of %players%]", "%players%'[s] target[ed] block[s]",
-				"[the] actual[ly] target[ed] block[s] [of %players%]", "%players%'[s] actual[ly] target[ed] block[s]");
+			"[the] target[ed] block[s] [of %players%]", "%players%'[s] target[ed] block[s]",
+			"[the] actual[ly] target[ed] block[s] [of %players%]", "%players%'[s] actual[ly] target[ed] block[s]");
 	}
+
+	private boolean actual;
 
 	@Override
 	@SuppressWarnings("unchecked")
 	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parser) {
 		setExpr((Expression<Player>) exprs[0]);
-		if (matchedPattern >= 2) {
-			// TODO remove 'actual' patterns in the future
-			Skript.warning("The 'actual' part of the targeted block expression is deprecated, it is now no longer required");
-		}
+		actual = matchedPattern > 1;
 		return true;
 	}
 
 	@Override
-	protected Block[] get(Event e, Player[] source) {
-		return get(source, p -> {
-			Block block = p.getTargetBlock(null, SkriptConfig.maxTargetBlockDistance.value());
-			if (block.getType() == Material.AIR)
-				return null;
-			return block;
-		});
+	public @Nullable Block convert(Player player) {
+		Integer distance = SkriptConfig.maxTargetBlockDistance.value();
+		Block block;
+		if (actual)
+			block = player.getTargetBlockExact(distance);
+		else
+			block = player.getTargetBlock(null, distance);
+		if (block != null && block.getType() == Material.AIR)
+			return null;
+		return block;
 	}
 
 	@Override
@@ -77,14 +79,8 @@ public class ExprTargetedBlock extends PropertyExpression<Player, Block> {
 	}
 
 	@Override
-	public boolean setTime(int time) {
-		super.setTime(time);
-		return true;
-	}
-
-	@Override
-	public String toString(@Nullable Event e, boolean debug) {
-		return "the targeted block" + (getExpr().isSingle() ? "" : "s") + " of " + getExpr().toString(e, debug);
+	protected String getPropertyName() {
+		return this.actual ? "actual target block" : "target block";
 	}
 
 }
