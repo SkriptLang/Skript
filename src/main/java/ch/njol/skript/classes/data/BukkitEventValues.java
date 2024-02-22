3f691e755dc190381304b014bb85e3dab450fea0
Vulcano771 <heroplayz176@gmail.com>
2024-02-22 11:29:20 +0100

    Return correct block for placing water in slabs, ...

--- src/main/java/ch/njol/skript/classes/data/BukkitEventValues.java
+++ src/main/java/ch/njol/skript/classes/data/BukkitEventValues.java
@@ -60,6 +60,7 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.Waterlogged;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.AbstractVillager;
 import org.bukkit.entity.Egg;
@@ -729,6 +730,9 @@ public final class BukkitEventValues {
 			@Override
 			@Nullable
 			public Block get(final PlayerBucketEmptyEvent e) {
+				if (e instanceof Waterlogged) {
+					return e.getBlockClicked();
+				}
 				return e.getBlockClicked().getRelative(e.getBlockFace());
 			}
 		}, -1);
