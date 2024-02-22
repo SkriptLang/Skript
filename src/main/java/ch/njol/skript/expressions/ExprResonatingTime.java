4f6f8c8352832e6b9ee88052d04486f5d423afec
bluelhf <ilari.suhonen@gmail.com>
2024-02-23 01:12:57 +0200

    feat(syntax): bell related properties

---
+++ src/main/java/ch/njol/skript/expressions/ExprResonatingTime.java
@@ -0,0 +1,67 @@
+/**
+ *   This file is part of Skript.
+ *
+ *  Skript is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Skript is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright Peter Güttinger, SkriptLang team and contributors
+ */
+package ch.njol.skript.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.RequiredPlugins;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import ch.njol.skript.util.Timespan;
+import org.bukkit.block.Bell;
+import org.bukkit.block.Block;
+import org.eclipse.jdt.annotation.Nullable;
+
+@Name("Resonating Time")
+@Description({
+	"Returns the resonating time of a bell.",
+	"A bell will start resonating five game ticks after being rung, and will continue to resonate for 40 game ticks."
+})
+@Examples("broadcast \"The bell has been ringing for %ringing time of target block%\"")
+@RequiredPlugins("Minecraft 1.19.4+")
+@Since("INSERT VERSION")
+public class ExprResonatingTime extends SimplePropertyExpression<Block, Timespan> {
+
+	static {
+		if (Skript.methodExists(Bell.class, "getResonatingTicks")) {
+			register(ExprResonatingTime.class, Timespan.class, "resonat(e|ing) time", "block");
+		}
+	}
+
+	@Override
+	public @Nullable Timespan convert(Block from) {
+		if (from.getState() instanceof Bell) {
+			return Timespan.fromTicks(((Bell) from.getState()).getResonatingTicks());
+		}
+
+		return null;
+	}
+
+	@Override
+	protected String getPropertyName() {
+		return "resonating time";
+	}
+
+	@Override
+	public Class<? extends Timespan> getReturnType() {
+		return Timespan.class;
+	}
+}
