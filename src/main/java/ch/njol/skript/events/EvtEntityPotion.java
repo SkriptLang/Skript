3a86677f4467feec4646fc1c2e9044ff635ca6d2
Asleepp <erosmos@hotmail.com>
2024-04-19 18:17:48 -0300

    silly me

--- src/main/java/ch/njol/skript/events/EvtEntityPotion.java
+++ src/main/java/ch/njol/skript/events/EvtEntityPotion.java
@@ -1,21 +1,3 @@
-/**
- *   This file is part of Skript.
- *
- *  Skript is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Skript is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright Peter Güttinger, SkriptLang team and contributors
- */
 package ch.njol.skript.events;
 
 import ch.njol.skript.Skript;
@@ -37,7 +19,7 @@ public class EvtEntityPotion extends SkriptEvent {
 			.description("Called when an entity's potion effect is modified.", "This modification can include adding, removing or changing their potion effect.")
 			.examples(
 				"on entity potion effect modification:",
-					"\t\tbroadcast \"A potion effect was added to %event-entity%!\" ",
+				"\t\tbroadcast \"A potion effect was added to %event-entity%!\" ",
 				"",
 				"on entity potion effect modification of night vision:")
 			.since("INSERT VERSION");
@@ -45,11 +27,13 @@ public class EvtEntityPotion extends SkriptEvent {
 
 	@SuppressWarnings("unchecked")
 	private Expression<PotionEffectType> potionEffects;
+	private Expression<EntityPotionEffectEvent.Cause> cause;
 
 	@Override
 	public boolean init(Literal<?>[] args, int matchedPattern, ParseResult parseResult) {
 		if (args.length > 0) {
 			potionEffects = (Expression<PotionEffectType>) args[0];
+			cause = (Expression<EntityPotionEffectEvent.Cause>) args[1];
 		}
 		return true;
 	}
@@ -69,12 +53,15 @@ public class EvtEntityPotion extends SkriptEvent {
 		PotionEffectType effectType = potionEvent.getNewEffect().getType();
 		for (PotionEffectType potionEffectType : potionEffects.getArray(event)) {
 			if (potionEffectType.equals(effectType)) {
-				return true;
+				if (cause == null || cause.getSingle(event).equals(potionEvent.getCause())) {
+					return true;
+				}
 			}
 		}
 
 		return false;
 	}
+
 
 
 	@Override
