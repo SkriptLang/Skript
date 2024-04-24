68feb17ec357ede076adc8352fee5e56038e9a00
NotSoDelayed <72163224+NotSoDelayed@users.noreply.github.com>
2024-04-24 18:36:07 +0800

    Requested change

--- src/main/java/ch/njol/skript/test/runner/ExprTestBlock.java
+++ src/main/java/ch/njol/skript/test/runner/ExprTestBlock.java
@@ -21,6 +21,7 @@ package ch.njol.skript.test.runner;
 import ch.njol.skript.Skript;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.NoDoc;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
