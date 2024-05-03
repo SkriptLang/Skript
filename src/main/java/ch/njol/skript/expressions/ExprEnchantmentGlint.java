1e4d32cfada22c99c4f2c3c79c334e4d456a4940
NotSoDelayed <simmz0403@gmail.com>
2024-05-03 23:41:38 +0800

    Add Skript docs and fix annotations

--- src/main/java/ch/njol/skript/expressions/ExprEnchantmentGlint.java
+++ src/main/java/ch/njol/skript/expressions/ExprEnchantmentGlint.java
@@ -1,13 +1,50 @@
+/**
+ *   This file is part of Skript.
+ *
+ *  Skript is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Skript is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright Peter Güttinger, SkriptLang team and contributors
+ */
 package ch.njol.skript.expressions;
 
 import ch.njol.skript.aliases.ItemType;
 import ch.njol.skript.classes.Changer.ChangeMode;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.RequiredPlugins;
+import ch.njol.skript.doc.Since;
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import ch.njol.util.coll.CollectionUtils;
 import org.bukkit.event.Event;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.Nullable;
 
+@Name("Enchantment Glint")
+@Description({
+	"Sets the 'enchantment_glint_override' on items.",
+	"If true, the item will glint, even without enchantments.",
+	"if false, the item will not glint, even with enchantments.",
+	"If cleared, the glint enforcement will be cleared."
+})
+@Examples({
+	"set the enchantment glint of player's tool to true",
+	"set the enchantment glint of {_items::*} to false",
+	"clear the enchantment glint of player's tool"
+})
+@RequiredPlugins("Spigot 1.20.5+")
+@Since("INSERT VERSION")
 public class ExprEnchantmentGlint extends SimplePropertyExpression<ItemType, Boolean> {
 
 	static {
@@ -37,7 +74,7 @@ public class ExprEnchantmentGlint extends SimplePropertyExpression<ItemType, Boo
 	}
 
 	@Override
-	public void change(Event event, Object @Nullable [] delta, ChangeMode mode) {
+	public void change(Event event, @Nullable Object[] delta, ChangeMode mode) {
 		switch (mode) {
 			case SET:
 				if (!(delta[0] instanceof Boolean))
