da99b2629786b47dddb76b46e4e26209c46fc818
NotSoDelayed <simmz0403@gmail.com>
2024-05-04 19:31:26 +0800

    Add condition item has enchantment glint

---
+++ src/main/java/ch/njol/skript/conditions/CondItemHasEnchantmentGlint.java
@@ -0,0 +1,35 @@
+package ch.njol.skript.conditions;
+
+import ch.njol.skript.aliases.ItemType;
+import ch.njol.skript.conditions.base.PropertyCondition;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.RequiredPlugins;
+import ch.njol.skript.doc.Since;
+
+@Name("Has Enchantment Glint Override")
+@Description("Checks whether an item has enchantment glint override set.")
+@Examples({
+	"if {_item} has enchantment glint override:",
+		"\tsend \"%{_item}% has enchantment glint override\" to player"
+})
+@RequiredPlugins("Spigot 1.20.5+")
+@Since("INSERT VERSION")
+public class CondItemHasEnchantmentGlint extends PropertyCondition<ItemType> {
+
+	static {
+		register(CondItemHasEnchantmentGlint.class, PropertyType.HAVE, "enchant[ment] glint overrid(e|den)", "itemtypes");
+	}
+
+	@Override
+	public boolean check(ItemType itemType) {
+		return itemType.getItemMeta().hasEnchantmentGlintOverride();
+	}
+
+	@Override
+	protected String getPropertyName() {
+		return "enchantment glint override";
+	}
+
+}
