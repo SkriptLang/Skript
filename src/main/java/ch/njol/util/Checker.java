c87166eaecee63f322263b39520248dcd790eb71
Moderocky <admin@moderocky.com>
2024-05-08 12:12:14 +0100

    Deprecate Checker for removal.

--- src/main/java/ch/njol/util/Checker.java
+++ src/main/java/ch/njol/util/Checker.java
@@ -18,9 +18,13 @@
  */
 package ch.njol.util;
 
+import org.jetbrains.annotations.ApiStatus;
+
 import java.util.function.Predicate;
 
+@Deprecated
 @FunctionalInterface
+@ApiStatus.ScheduledForRemoval
 public interface Checker<T> extends Predicate<T> {
 
 	boolean check(T o);
