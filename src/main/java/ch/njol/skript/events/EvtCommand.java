b44de5a6563fcbe8d626d9d8db2db49ebdcf3578
NotSoDelayed <simmz0403@gmail.com>
2024-05-17 21:19:15 +0800

    Event `on command` enhancements

--- src/main/java/ch/njol/skript/events/EvtCommand.java
+++ src/main/java/ch/njol/skript/events/EvtCommand.java
@@ -18,66 +18,135 @@
  */
 package ch.njol.skript.events;
 
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.server.ServerCommandEvent;
 import org.eclipse.jdt.annotation.Nullable;
 
 import ch.njol.skript.Skript;
+import ch.njol.skript.SkriptConfig;
 import ch.njol.skript.lang.Literal;
 import ch.njol.skript.lang.SkriptEvent;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.StringUtils;
 import ch.njol.util.coll.CollectionUtils;
 
-/**
- * @author Peter Güttinger
- */
-@SuppressWarnings("unchecked")
-public class EvtCommand extends SkriptEvent { // TODO condition to check whether a given command exists, & a conditon to check whether it's a custom skript command
+public class EvtCommand extends SkriptEvent {
+
 	static {
-		Skript.registerEvent("Command", EvtCommand.class, CollectionUtils.array(PlayerCommandPreprocessEvent.class, ServerCommandEvent.class), "command [%-string%]")
-				.description("Called when a player enters a command (not necessarily a Skript command) but you can check if command is a skript command, see <a href='conditions.html#CondIsSkriptCommand'>Is a Skript command condition</a>.")
-				.examples("on command:", "on command \"/stop\":", "on command \"pm Njol \":")
-				.since("2.0");
+		Skript.registerEvent("Command", EvtCommand.class, CollectionUtils.array(PlayerCommandPreprocessEvent.class, ServerCommandEvent.class),
+				"[1:console|2:player] command [%-string%]")
+				.description(
+						"Called when a player or the console enters a command (not necessarily a Skript command)" +
+						"but you can check if command is a skript command, see <a href='conditions.html#CondIsSkriptCommand'>Is a Skript command condition</a>.")
+				.examples(
+						"on command:",
+						"on command \"/stop\":",
+						"on command \"pm Njol\":",
+						"on console command:",
+						"on player command \"/op\"")
+				.since("2.0, INSERT VERSION (specific sender)");
 	}
-	
+
+	private enum Sender {
+
+		ANY(""),
+		CONSOLE("console"),
+		PLAYER("player");
+
+		final String toString;
+
+		Sender(String toString) {
+			this.toString = toString;
+		}
+
+		static Sender valueOf(CommandSender sender) {
+			if (sender instanceof ConsoleCommandSender) {
+				return CONSOLE;
+			} else if (sender instanceof Player) {
+				return PLAYER;
+			} else {
+				return ANY;
+			}
+		}
+
+		@Override
+		public String toString() {
+			return toString;
+		}
+
+	}
+
 	@Nullable
-	private String command = null;
+	private String command;
+	@SuppressWarnings("NotNullFieldNotInitialized")
+	private Sender sender;
 
 	@Override
-	@SuppressWarnings("null")
-	public boolean init(final Literal<?>[] args, final int matchedPattern, final ParseResult parser) {
+	@SuppressWarnings("unchecked")
+	public boolean init(Literal<?>[] args, int matchedPattern, ParseResult parseResult) {
 		if (args[0] != null) {
 			command = ((Literal<String>) args[0]).getSingle();
 			if (command.startsWith("/"))
 				command = command.substring(1);
 		}
+		sender = Sender.values()[parseResult.mark];
 		return true;
 	}
 
 	@Override
-	@SuppressWarnings("null")
-	public boolean check(final Event e) {
-		if (e instanceof ServerCommandEvent && ((ServerCommandEvent) e).getCommand().isEmpty())
-			return false;
-
-		if (command == null)
-			return true;
-		final String message;
-		if (e instanceof PlayerCommandPreprocessEvent) {
-			assert ((PlayerCommandPreprocessEvent) e).getMessage().startsWith("/");
-			message = ((PlayerCommandPreprocessEvent) e).getMessage().substring(1);
-		} else {
-			message = ((ServerCommandEvent) e).getCommand();
+	public boolean check(Event event) {
+		if (event instanceof ServerCommandEvent) {
+			String command = ((ServerCommandEvent) event).getCommand();
+			if (command.isEmpty() || command.startsWith(SkriptConfig.effectCommandToken.value()))
+				return false;
 		}
-		return StringUtils.startsWithIgnoreCase(message, command)
-				&& (command.contains(" ") || message.length() == command.length() || Character.isWhitespace(message.charAt(command.length()))); // if only the command is given, match that command only
+		Sender eventSender = Sender.valueOf(getSender(event));
+		if (command == null) {
+			if (sender != Sender.ANY) {
+				return eventSender == sender;
+			}
+			return true;
+		}
+		String message;
+		if (event instanceof PlayerCommandPreprocessEvent) {
+			assert ((PlayerCommandPreprocessEvent) event).getMessage().startsWith("/");
+			message = ((PlayerCommandPreprocessEvent) event).getMessage().substring(1);
+		} else {
+			message = ((ServerCommandEvent) event).getCommand();
+		}
+		// if only the command is given, match that command only
+		if (StringUtils.startsWithIgnoreCase(message, command)
+			&& (command.contains(" ") || message.length() == command.length()
+			|| Character.isWhitespace(message.charAt(command.length())))) {
+			if (sender != Sender.ANY) {
+				return eventSender == sender;
+			}
+			return true;
+		}
+		return false;
 	}
 	
 	@Override
-	public String toString(final @Nullable Event e, final boolean debug) {
-		return "command" + (command != null ? " /" + command : "");
+	public String toString(@Nullable Event event, boolean debug) {
+		String sender = "";
+		if (this.sender != Sender.ANY)
+			sender = this.sender + " ";
+		return sender + "command" + (command != null ? " /" + command : "");
 	}
-	
+
+	private static CommandSender getSender(Event event) {
+		if (event instanceof PlayerCommandPreprocessEvent) {
+			return ((PlayerCommandPreprocessEvent) event).getPlayer();
+		} else if (event instanceof ServerCommandEvent) {
+			return ((ServerCommandEvent) event).getSender();
+		} else {
+			assert false;
+			return null;
+		}
+	}
+
 }
