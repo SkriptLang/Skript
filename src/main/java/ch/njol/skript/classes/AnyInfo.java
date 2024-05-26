db2f8440b204db15db2418c4b2a93e350295b70b
Moderocky <admin@moderocky.com>
2024-05-26 13:26:23 +0100

    Register special any-info (for checking purposes).

---
+++ src/main/java/ch/njol/skript/classes/AnyInfo.java
@@ -0,0 +1,16 @@
+package ch.njol.skript.classes;
+
+import ch.njol.skript.lang.util.common.AnyProvider;
+
+public class AnyInfo<Type extends AnyProvider> extends ClassInfo<Type> {
+
+	/**
+	 * @param c        The class
+	 * @param codeName The name used in patterns
+	 */
+	public AnyInfo(Class<Type> c, String codeName) {
+		super(c, "any-" + codeName);
+		this.user("(any )?" + codeName + " (thing|object)s?");
+	}
+
+}
