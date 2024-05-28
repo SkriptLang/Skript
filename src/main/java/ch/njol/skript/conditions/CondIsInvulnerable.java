b022a6ec5614d92558325c2c8ae127aed336c5e7
Jake <gabrieleluparelloj@gmail.com>
2024-05-29 00:01:46 +0200

    checks if the method for gamemodes exists when registering the condition, fixed typo
    Took 9 minutes

--- src/main/java/ch/njol/skript/conditions/CondIsInvulnerable.java
+++ src/main/java/ch/njol/skript/conditions/CondIsInvulnerable.java
@@ -19,36 +19,36 @@
 package ch.njol.skript.conditions;
 
 import ch.njol.skript.Skript;
+import ch.njol.skript.doc.*;
 import org.bukkit.GameEvent;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Entity;
 
 import ch.njol.skript.conditions.base.PropertyCondition;
-import ch.njol.skript.doc.Description;
-import ch.njol.skript.doc.Examples;
-import ch.njol.skript.doc.Name;
-import ch.njol.skript.doc.Since;
 
 @Name("Is Invulnerable")
 @Description("Checks whether an entity or a gamemode is invulnerable.\nFor gamemodes, Paper and Minecraft 1.20.6 are required")
 @Examples({
 	"target entity is invulnerable",
+	"",
 	"loop {_gamemodes::*}:",
 		"\tif loop-value is not invulnerable:",
 			"\t\tbroadcast \"the gamemode %loop-value% is vulnerable!\""
 })
 @Since("2.5, INSERT VERSION (gamemode)")
+@RequiredPlugins("Paper 1.20.6+ (gamemodes)")
 public class CondIsInvulnerable extends PropertyCondition<Object> {
+	private static final boolean GAMEMODE = Skript.methodExists(GameMode.class,"isInvulnerable");
 	
 	static {
-		register(CondIsInvulnerable.class, PropertyType.BE, "invulnerable", "entities/gamemodes");
+		register(CondIsInvulnerable.class, PropertyType.BE, "invulnerable", "entities"+(GAMEMODE ? "/gamemodes" : ""));
 	}
 	
 	@Override
 	public boolean check(Object object) {
 		if (object instanceof Entity) {
 			return ((Entity) object).isInvulnerable();
-		} else if (Skript.methodExists(GameEvent.class,"isInvulnerable") && object instanceof GameMode) {
+		} else if (GAMEMODE && object instanceof GameMode) {
 			return ((GameMode) object).isInvulnerable();
 		}
 		return false;
