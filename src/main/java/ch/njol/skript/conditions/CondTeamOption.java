789dec240ded84ebba35be4c8a3754941d3f5fcd
Moderocky <admin@moderocky.com>
2024-06-02 16:06:29 +0100

    Support friendly fire in condition.

--- src/main/java/ch/njol/skript/conditions/CondTeamOption.java
+++ src/main/java/ch/njol/skript/conditions/CondTeamOption.java
@@ -9,6 +9,7 @@ import ch.njol.util.Kleenean;
 import org.bukkit.event.Event;
 import org.bukkit.scoreboard.Team;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.UnknownNullability;
 
 // todo doc
 public class CondTeamOption extends Condition {
@@ -16,15 +17,16 @@ public class CondTeamOption extends Condition {
 	static {
 		String optionStatus = EffTeamOption.optionStatus();
 		Skript.registerCondition(CondTeamOption.class,
-			"[:only] " + optionStatus + " can(not:not|n't) (collide:collide with|tag:see [the] name[ ]tag[s] of|death:see the death message[s] of) %teams%",
-			"[:only] " + optionStatus + " can(not:not|n't) see %teams%'[s] (tag:name[ ]tag[s]|death:death message[s])"
+			"[:only] " + optionStatus + " can(not:not|not:n't) (collide:collide with|tag:see [the] name[ ]tag[s] of|death:see the death message[s] of) %teams%",
+			"[:only] " + optionStatus + " can(not:not|not:n't) see %teams%'[s] (tag:name[ ]tag[s]|death:death message[s])",
+			"(friendly:friendly fire|seeing friendly invisible[ player]s) is(not:not|not:n't) (allowed|permitted) for %teams%"
 		);
 	}
 
-	private Team.Option option;
-	private Team.OptionStatus status;
+	private Team.@UnknownNullability Option option;
+	private Team.@UnknownNullability OptionStatus status;
 	private Expression<Team> teamExpression;
-	private boolean only;
+	private boolean only, friendlyFire;
 
 	@Override
 	public boolean init(Expression<?>[] expressions, int pattern, Kleenean delayed, ParseResult result) {
@@ -35,11 +37,11 @@ public class CondTeamOption extends Condition {
 		else if (result.hasTag("collide"))
 			this.option = Team.Option.COLLISION_RULE;
 		else { // how did we get here? No idea, but just in case
-			Skript.error("Unrecognized team option: choose one of name tags, death messages or collision.");
-			return false;
+			this.friendlyFire = result.hasTag("friendly");
 		}
 		assert result.mark < EffTeamOption.OPTIONS.length;
-		this.status = EffTeamOption.OPTIONS[result.mark];
+		if (pattern < 2)
+			this.status = EffTeamOption.OPTIONS[result.mark];
 		this.only = result.hasTag("only");
 		this.setNegated(result.hasTag("not"));
 		//noinspection unchecked
@@ -50,6 +52,11 @@ public class CondTeamOption extends Condition {
 	@Override
 	public boolean check(Event event) {
 		return teamExpression.check(event, team -> {
+			if (option == null) {
+				if (friendlyFire)
+					return team.allowFriendlyFire();
+				return team.canSeeFriendlyInvisibles();
+			}
 			Team.OptionStatus current = team.getOption(option);
 			if (only)
 				return current == status;
@@ -65,21 +72,30 @@ public class CondTeamOption extends Condition {
 
 	@Override
 	public String toString(@Nullable Event event, boolean debug) {
+		if (option == null) {
+			if (friendlyFire)
+				return "friendly fire"
+					+ (this.isNegated() ? " is not" : " is")
+					+ " allowed for " + teamExpression.toString(event, debug);
+			return "seeing friendly invisible players"
+				+ (this.isNegated() ? " is not" : " is")
+				+ " allowed for " + teamExpression.toString(event, debug);
+		}
 		switch (option) {
 			case COLLISION_RULE:
 				return (only ? "only " : "")
 					+ EffTeamOption.statusName(status)
-					+ (this.isNegated() ? " cannot" : "")
+					+ (this.isNegated() ? " cannot" : " can")
 					+ " collide with " + teamExpression.toString(event, debug);
 			case DEATH_MESSAGE_VISIBILITY:
 				return (only ? "only " : "")
 					+ EffTeamOption.statusName(status)
-					+ (this.isNegated() ? " cannot" : "")
+					+ (this.isNegated() ? " cannot" : "can")
 					+ " see the death messages of " + teamExpression.toString(event, debug);
 			case NAME_TAG_VISIBILITY:
 				return (only ? "only " : "")
 					+ EffTeamOption.statusName(status)
-					+ (this.isNegated() ? " cannot" : "")
+					+ (this.isNegated() ? " cannot" : "can")
 					+ " see the name tags of " + teamExpression.toString(event, debug);
 		}
 		assert false;
