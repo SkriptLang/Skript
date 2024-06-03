66574b1c0816fe22be133d31ead7a20252c7f4a7
XPYEX <XPYEX0@163.com>
2024-06-03 22:41:55 +0800

    feat:
    Mapped event-caught to PlayerFishEvent.getCaught()
    Mapped event-hook to PlayerFishEvent.getHook()

---
+++ src/main/java/ch/njol/skript/expressions/ExprFishingEntity.java
@@ -0,0 +1,60 @@
+package ch.njol.skript.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.skript.registrations.Classes;
+import ch.njol.util.Kleenean;
+import org.bukkit.entity.Entity;
+import org.bukkit.event.Event;
+import org.bukkit.event.player.PlayerFishEvent;
+import org.eclipse.jdt.annotation.Nullable;
+
+public class ExprFishingEntity extends SimpleExpression<Entity> {
+	private int pattern;
+	static {
+		Skript.registerExpression(ExprFishingEntity.class, Entity.class, ExpressionType.SIMPLE,
+			"1¦([(event|fish[ing])( |-)]caught)|2¦([(event|fish[ing])( |-)]hook)");
+	}
+	@Override
+	public String toString(@Nullable Event event, boolean debug) {
+		if (event == null)
+			return "the fishing entity";
+		return Classes.getDebugMessage(getSingle(event));
+	}
+
+	@Override
+	public boolean isSingle() {
+		return true;
+	}
+
+	@Override
+	public Class<? extends Entity> getReturnType() {
+		return Entity.class;
+	}
+
+	@Override
+	public boolean init(Expression<?>[] expressions, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+		if (!getParser().isCurrentEvent(PlayerFishEvent.class)) {
+			Skript.error("The 'fishing entity' expression can only be used in fish event");
+			return false;
+		}
+		pattern = parseResult.mark;
+		return true;
+	}
+
+	@Override
+	protected @Nullable Entity[] get(Event event) {
+		if (event instanceof PlayerFishEvent) {
+			switch (pattern) {
+				case 1:
+					return new Entity[]{((PlayerFishEvent) event).getCaught()};
+				case 2:
+					return new Entity[]{((PlayerFishEvent) event).getHook()};
+			}
+		}
+		return null;
+	}
+}
--- src/main/java/ch/njol/skript/expressions/ExprFishingState.java
+++ src/main/java/ch/njol/skript/expressions/ExprFishingState.java
@@ -10,6 +10,7 @@ import ch.njol.skript.lang.SkriptParser;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.skript.registrations.Classes;
 import ch.njol.util.Kleenean;
+import java.util.NoSuchElementException;
 import org.bukkit.event.Event;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.eclipse.jdt.annotation.Nullable;
@@ -18,8 +19,10 @@ import org.eclipse.jdt.annotation.Nullable;
 @Description({"Returns fish state in FishEvent"})
 @Since("2.8.9")
 public class ExprFishingState extends SimpleExpression<PlayerFishEvent.State> {
+	private int pattern;
 	static {
-		Skript.registerExpression(ExprFishingState.class, PlayerFishEvent.State.class, ExpressionType.SIMPLE, "[(event|fish[ing])( |-)]state");
+		Skript.registerExpression(ExprFishingState.class, PlayerFishEvent.State.class, ExpressionType.SIMPLE,
+			"1¦([(event|fish[ing])( |-)]state)|2¦fishing|3¦caught fish|4¦caught entity|5¦in ground|6¦reel in|7¦bite|8¦lured");
 	}
 
 	@Override
@@ -45,11 +48,36 @@ public class ExprFishingState extends SimpleExpression<PlayerFishEvent.State> {
 			Skript.error("The 'fishing state' expression can only be used in fish event");
 			return false;
 		}
+		pattern = parseResult.mark;
+		if (pattern == 8) {
+			try {
+				PlayerFishEvent.State test = PlayerFishEvent.State.LURED;
+			} catch (NoSuchElementException e) {
+				Skript.error("The fishing state 'lured' is not supported in your server platform. It need PaperAPI");
+				return false;
+			}
+		}
 		return true;
 	}
 
 	@Override
-	protected @Nullable PlayerFishEvent.State[] get(Event event) {
+	protected PlayerFishEvent.@Nullable State[] get(Event event) {
+		switch (pattern) {
+			case 2:
+				return new PlayerFishEvent.State[]{PlayerFishEvent.State.FISHING};
+			case 3:
+				return new PlayerFishEvent.State[]{PlayerFishEvent.State.CAUGHT_FISH};
+			case 4:
+				return new PlayerFishEvent.State[]{PlayerFishEvent.State.CAUGHT_ENTITY};
+			case 5:
+				return new PlayerFishEvent.State[]{PlayerFishEvent.State.IN_GROUND};
+			case 6:
+				return new PlayerFishEvent.State[]{PlayerFishEvent.State.REEL_IN};
+			case 7:
+				return new PlayerFishEvent.State[]{PlayerFishEvent.State.BITE};
+			case 8:
+				return new PlayerFishEvent.State[]{PlayerFishEvent.State.LURED};
+		}
 		if (event instanceof PlayerFishEvent) {
 			return new PlayerFishEvent.State[] {((PlayerFishEvent) event).getState()};
 		}
