e7dd0dbd5119e1c3e91608081010b05e8f92702d
Moderocky <admin@moderocky.com>
2024-06-03 10:10:02 +0100

    Switch to property condition.

--- src/main/java/ch/njol/skript/conditions/CondObjectiveDisplay.java
+++ src/main/java/ch/njol/skript/conditions/CondObjectiveDisplay.java
@@ -1,51 +1,42 @@
 package ch.njol.skript.conditions;
 
-import ch.njol.skript.Skript;
-import ch.njol.skript.lang.Condition;
+import ch.njol.skript.conditions.base.PropertyCondition;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.util.Kleenean;
-import org.bukkit.event.Event;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 import org.jetbrains.annotations.Nullable;
 
 // todo doc
-public class CondObjectiveDisplay extends Condition {
+public class CondObjectiveDisplay extends PropertyCondition<Objective> {
 
 	static {
-		Skript.registerCondition(CondObjectiveDisplay.class,
-			"%objectives% (is|are)[not: not|not:n't] displayed (0:below [the] name)",
-			"%objectives% (is|are)[not: not|not:n't] displayed in [the] (1:player[ ]list|2:side[ ]bar)",
-			"%objectives% (is|are)[not: not|not:n't] displayed");
+		PropertyCondition.register(CondObjectiveDisplay.class,
+			PropertyType.BE,
+			"displayed (0:below [the] name|1:in [the] player[ ]list|2:in [the] side[ ]bar|3:)",
+			"objectives");
 	}
 
-	private Expression<Objective> objectiveExpression;
 	private @Nullable DisplaySlot slot;
 
 	@Override
 	public boolean init(Expression<?>[] expressions, int pattern, Kleenean delayed, SkriptParser.ParseResult result) {
-		if (pattern < 2) {
-			DisplaySlot[] slots = DisplaySlot.values();
-			if (result.mark < 3) // paper added some pointless extra display slot enums
-				this.slot = slots[result.mark];
-		}
-		this.objectiveExpression = (Expression<Objective>) expressions[0];
-		this.setNegated(result.hasTag("not"));
+		DisplaySlot[] slots = DisplaySlot.values();
+		if (result.mark < 3)
+			this.slot = slots[result.mark];
 		return true;
 	}
 
 	@Override
-	public boolean check(Event event) {
-		return objectiveExpression.check(event, objective -> {
-			if (slot == null)
-				return objective.getDisplaySlot() != null; // is displayed
-			return objective.getDisplaySlot() == slot;
-		}, this.isNegated());
+	public boolean check(Objective objective) {
+		if (slot == null)
+			return objective.getDisplaySlot() != null; // is displayed
+		return objective.getDisplaySlot() == slot;
 	}
 
 	@Override
-	public String toString(@Nullable Event event, boolean debug) {
+	protected String getPropertyName() {
 		String place = "";
 		if (slot != null) {
 			switch (slot) {
@@ -59,11 +50,7 @@ public class CondObjectiveDisplay extends Condition {
 					place = " in the side bar";
 			}
 		}
-		return objectiveExpression.toString(event, debug)
-			+ (objectiveExpression.isSingle() ? " is " : " are ")
-			+ (this.isNegated() ? "not " : "")
-			+ "displayed"
-			+ place;
+		return "displayed" + place;
 	}
 
 }
