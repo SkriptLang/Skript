8b7968223272eb34b9f0eb1c989fe78f8bc0dcb5
Efnilite <35348263+Efnilite@users.noreply.github.com>
2024-07-21 20:34:44 +0200

    update annotation

--- src/main/java/ch/njol/skript/expressions/ExprJoinSplit.java
+++ src/main/java/ch/njol/skript/expressions/ExprJoinSplit.java
@@ -14,6 +14,7 @@ import ch.njol.util.Kleenean;
 import ch.njol.util.StringUtils;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.UnknownNullability;
 
 import java.util.regex.Pattern;
 
@@ -40,9 +41,9 @@ public class ExprJoinSplit extends SimpleExpression<String> {
 	private boolean caseSensitivity;
 	private boolean removeTrailing;
 
-	@SuppressWarnings("NotNullFieldNotInitialized")
+	@UnknownNullability
 	private Expression<String> strings;
-	@SuppressWarnings("NotNullFieldNotInitialized")
+	@UnknownNullability
 	private Expression<String> delimiter;
 
 	@Override
