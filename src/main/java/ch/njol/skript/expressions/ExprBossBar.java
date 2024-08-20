3664d5c0d65aef379baa6ed998a2fd1af272f69c
Moderocky <admin@moderocky.com>
2024-08-20 18:10:30 +0100

    Space in boss bar optional.

--- src/main/java/ch/njol/skript/expressions/ExprBossBar.java
+++ src/main/java/ch/njol/skript/expressions/ExprBossBar.java
@@ -36,8 +36,8 @@ public class ExprBossBar extends SimpleExpression<BossBar> {
 
 	static {
 		Skript.registerExpression(ExprBossBar.class, BossBar.class, ExpressionType.SIMPLE,
-			"[a] new boss bar [name:(named|titled|with title) %-string%]",
-			"[a] new %color% boss bar [name:(named|titled|with title) %-string%]"
+			"[a] new boss[ ]bar [name:(named|titled|with title) %-string%]",
+			"[a] new %color% boss[ ]bar [name:(named|titled|with title) %-string%]"
 		);
 	}
 
