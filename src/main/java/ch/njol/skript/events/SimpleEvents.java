dd4f4f4eaa296f3c217aeb2a1a081928ee94e155
Phill310 <mcphill310@gmail.com>
2024-08-29 20:55:57 -0700

    Specify paper version

--- src/main/java/ch/njol/skript/events/SimpleEvents.java
+++ src/main/java/ch/njol/skript/events/SimpleEvents.java
@@ -710,26 +710,28 @@ public class SimpleEvents {
 					"Called when a world border changes its bounds, either over time, or instantly.",
 					"Note: event-number will be the diameter of the world border."
 				)
-				.requiredPlugins("Paper")
+				.requiredPlugins("Paper 1.16+")
 				.examples(
 					"on border bounds change:",
 						"\tbroadcast \"You better get moving!\""
 				)
 				.since("INSERT VERSION");
+
 			Skript.registerEvent("World Border Bounds Finish Change", SimpleEvent.class, WorldBorderBoundsChangeFinishEvent.class, "[world[ ]]border [bounds] finish chang(e|ing)")
 				.description(
 					"Called when a moving world border has finished its move.",
 					"Note: event-number will be the diameter of the world border."
 				)
-				.requiredPlugins("Paper")
+				.requiredPlugins("Paper 1.16+")
 				.examples(
 					"on border bounds finish change:",
 						"\tbroadcast \"Get inside the borders!\""
 				)
 				.since("INSERT VERSION");
+
 			Skript.registerEvent("World Border Center Change", SimpleEvent.class, WorldBorderCenterChangeEvent.class, "[world[ ]]border center chang(e|ing)")
 				.description("Called when a world border's center has changed.")
-				.requiredPlugins("Paper")
+				.requiredPlugins("Paper 1.16+")
 				.examples(
 					"on border center change:",
 						"\tbroadcast \"The center has moved\""
