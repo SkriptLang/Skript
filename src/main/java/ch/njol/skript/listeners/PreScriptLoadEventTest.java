19cb7eff9f775ce0487e5e2a3aff4b53e8205b3b
Lexi Nuutrai <Nuutralex@gmail.com>
2024-08-31 11:02:44 -0400

    Finished (presumed) that the node modification is finished

--- src/main/java/ch/njol/skript/listeners/PreScriptLoadEventTest.java
+++ src/main/java/ch/njol/skript/listeners/PreScriptLoadEventTest.java
@@ -24,16 +24,21 @@ public class PreScriptLoadEventTest implements Listener {
 	}
 
 	public void processSectionNode(SectionNode sectionNode) {
+		int c = 0;
 		for (Node node : sectionNode) {
 			if (node instanceof SimpleNode && isElseIf(node.getKey())) {
 				String condKey = node.getKey().split("(\\) (.+))", Pattern.CASE_INSENSITIVE)[0] + ")";
 				String effKey = node.getKey().split("(else )?if (\\((.+)\\) )", Pattern.CASE_INSENSITIVE)[1];
 				SectionNode newSectionNode = new SectionNode(condKey,"", node.getParent(), node.getLine());
 				SimpleNode simpleNode = new SimpleNode(effKey, "", node.getLine(), newSectionNode);
+				newSectionNode.add(simpleNode);
+				sectionNode.remove(node);
+				sectionNode.insert(newSectionNode, c);
 				Skript.adminBroadcast("Node: " + node.getKey()); // Debugging purposes, relatively useless right now though
 			} else if (node instanceof SectionNode) {
 				processSectionNode((SectionNode) node);
 			}
+			c++;
 		}
 	}
 
