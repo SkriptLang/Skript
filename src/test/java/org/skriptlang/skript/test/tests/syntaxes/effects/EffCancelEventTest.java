a2919a1433c9150666a69e461173927218939c89
Efnilite <35348263+Efnilite@users.noreply.github.com>
2024-09-10 17:57:48 +0200

    init commit

---
+++ src/test/java/org/skriptlang/skript/test/tests/syntaxes/effects/EffCancelEventTest.java
@@ -0,0 +1,13 @@
+package org.skriptlang.skript.test.tests.syntaxes.effects;
+
+import ch.njol.skript.test.runner.SkriptJUnitTest;
+import org.junit.Test;
+
+public class EffCancelEventTest extends SkriptJUnitTest {
+
+	@Test
+	public void test() {
+		spawnTestPig();
+	}
+
+}
