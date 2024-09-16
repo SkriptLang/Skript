7486ff962d2233bd7c6a6146248a509ada3b5005
Efnilite <35348263+Efnilite@users.noreply.github.com>
2024-09-16 10:21:09 +0200

    fix breaking changes

--- src/main/java/ch/njol/skript/expressions/ExprTime.java
+++ src/main/java/ch/njol/skript/expressions/ExprTime.java
@@ -1,5 +1,6 @@
 package ch.njol.skript.expressions;
 
+import ch.njol.skript.Skript;
 import ch.njol.skript.classes.Changer.ChangeMode;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
@@ -7,6 +8,7 @@ import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
 import ch.njol.skript.expressions.base.PropertyExpression;
 import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.util.Time;
 import ch.njol.skript.util.Timeperiod;
@@ -39,7 +41,8 @@ public class ExprTime extends PropertyExpression<World, Time> {
 	private static final int TIME_TO_TIMESPAN_OFFSET = 18000;
 
 	static {
-		registerDefault(ExprTime.class, Time.class, "time", "world");
+		Skript.registerExpression(ExprTime.class, Time.class, ExpressionType.PROPERTY,
+			"[the] time[s] [([with]in|of) %worlds%]", "%worlds%'[s] time[s]");
 	}
 
 	@SuppressWarnings("unchecked")
