a0c36ca00f500386e92940bf23bc7e48cf823028
Lexi Nuutrai <Nuutralex@gmail.com>
2024-09-16 15:32:36 -0400

    Added CondIsTicking

---
+++ src/main/java/ch/njol/skript/conditions/CondIsTicking.java
@@ -0,0 +1,32 @@
+package ch.njol.skript.conditions;
+
+import ch.njol.skript.conditions.base.PropertyCondition;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import org.bukkit.entity.Entity;
+
+@Name("Is ticking")
+@Description("Check if an entity is ticking.")
+@Examples({
+	"send true if target is ticking"
+})
+@Since("INSERT VERSION")
+public class CondIsTicking extends PropertyCondition<Entity> {
+
+	static {
+		register(CondIsTicking.class, "ticking", "entities");
+	}
+
+	@Override
+	public boolean check(Entity value) {
+		return value.isTicking();
+	}
+
+	@Override
+	protected String getPropertyName() {
+		return "ticking";
+	}
+
+}
