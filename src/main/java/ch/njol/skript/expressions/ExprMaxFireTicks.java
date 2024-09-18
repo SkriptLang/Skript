133aef7822ce2a8a5246ce5924c7841f8f4fbd83
Lexi Nuutrai <Nuutralex@gmail.com>
2024-09-18 00:40:58 -0400

    Updated syntax for ExprMaxFireTicks

--- src/main/java/ch/njol/skript/expressions/ExprMaxFireTicks.java
+++ src/main/java/ch/njol/skript/expressions/ExprMaxFireTicks.java
@@ -12,13 +12,13 @@ import org.jetbrains.annotations.Nullable;
 @Name("Maximum Time On Fire")
 @Description("Returns the maximum amount of time before an entity begins getting damaged by fire.")
 @Examples({
-	"send target's max time on fire"
+	"send target's max fire time"
 })
 @Since("INSERT VERSION")
 public class ExprMaxFireTicks extends SimplePropertyExpression<Entity, Timespan> {
 
 	static {
-		register(ExprMaxFireTicks.class, Timespan.class, "max[imum] time on fire", "entities");
+		register(ExprMaxFireTicks.class, Timespan.class, "max[imum] fire time", "entities");
 	}
 
 	@Override
@@ -34,7 +34,7 @@ public class ExprMaxFireTicks extends SimplePropertyExpression<Entity, Timespan>
 
 	@Override
 	protected String getPropertyName() {
-		return "maximum time on fire";
+		return "maximum fire time";
 	}
 
 }
