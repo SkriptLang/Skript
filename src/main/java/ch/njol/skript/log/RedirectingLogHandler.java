4294e46c7a55bd5492b3803ade689bc5f08d8e56
EquipableMC <66171067+EquipableMC@users.noreply.github.com>
2024-09-21 08:03:08 -0400

    Revert "Operators reloading info (#6780)"
    This reverts commit c8cd975ec301a7e8de3aeeb16466954de1a12d17.

--- src/main/java/ch/njol/skript/log/RedirectingLogHandler.java
+++ src/main/java/ch/njol/skript/log/RedirectingLogHandler.java
@@ -18,51 +18,43 @@
  */
 package ch.njol.skript.log;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.logging.Level;
 
 /**
- * Redirects the log to one or more {@link CommandSender}s.
+ * Redirects the log to a {@link CommandSender}.
  */
 public class RedirectingLogHandler extends LogHandler {
 
-	private final Collection<CommandSender> recipients;
-	private int numErrors = 0;
+	private final CommandSender recipient;
+	
 	private final String prefix;
-
+	
+	private int numErrors = 0;
+	
 	public RedirectingLogHandler(CommandSender recipient, @Nullable String prefix) {
-		this(Collections.singletonList(recipient), prefix);
-	}
-
-	public RedirectingLogHandler(Collection<CommandSender> recipients, @Nullable String prefix) {
-		this.recipients = new ArrayList<>(recipients);
+		this.recipient = recipient;
 		this.prefix = prefix == null ? "" : prefix;
 	}
-
+	
 	@Override
 	public LogResult log(LogEntry entry) {
-		String formattedMessage = prefix + entry.toFormattedString();
-		for (CommandSender recipient : recipients) {
-			SkriptLogger.sendFormatted(recipient, formattedMessage);
-		}
-		if (entry.level == Level.SEVERE) {
+		SkriptLogger.sendFormatted(recipient, prefix + entry.toFormattedString());
+		if (entry.level == Level.SEVERE)
 			numErrors++;
-		}
 		return LogResult.DO_NOT_LOG;
 	}
-
+	
 	@Override
 	public RedirectingLogHandler start() {
 		return SkriptLogger.startLogHandler(this);
 	}
-
+	
 	public int numErrors() {
 		return numErrors;
 	}
-}
 
+}
