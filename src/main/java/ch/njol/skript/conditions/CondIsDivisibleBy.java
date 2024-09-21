8afc2a543dd4db04e3bb8fb36907c1dcc242917c
EquipableMC <66171067+EquipableMC@users.noreply.github.com>
2024-09-21 08:44:06 -0400

    requested changes + adds tests

--- src/main/java/ch/njol/skript/conditions/CondIsDivisibleBy.java
+++ src/main/java/ch/njol/skript/conditions/CondIsDivisibleBy.java
@@ -1,6 +1,10 @@
 package ch.njol.skript.conditions;
 
 import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.Condition;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
@@ -8,12 +12,15 @@ import ch.njol.util.Kleenean;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 
+@Name("Divisible By")
+@Description("Check if a number is divisible by another number.")
+@Examples("5 is divisible by 5")
+@Since("INSERT VERSION")
 public class CondIsDivisibleBy extends Condition {
 
 	static {
 		Skript.registerCondition(CondIsDivisibleBy.class,
-			"%number% is divisible by %number%",
-			"%number% can be divisible by %number%");
+			"%number% (is|can be) (divisible|divided) by %number%");
 	}
 
 	@SuppressWarnings("null")
@@ -48,7 +55,7 @@ public class CondIsDivisibleBy extends Condition {
 
 	@Override
 	public String toString(@Nullable Event event, boolean debug) {
-		return "is divisible by " + num1.toString(event, debug) + num2.toString(event, debug);
+		return num1.toString(event, debug) + " is divisible by " + num2.toString(event, debug);
 	}
 
 }
