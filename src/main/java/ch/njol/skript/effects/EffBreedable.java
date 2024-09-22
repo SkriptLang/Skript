803c718e3e577c6cd15e7cd570b71ded146d749e
Efnilite <35348263+Efnilite@users.noreply.github.com>
2024-09-22 13:27:35 +0200

    update effs

--- src/main/java/ch/njol/skript/effects/EffBreedable.java
+++ src/main/java/ch/njol/skript/effects/EffBreedable.java
@@ -1,28 +1,9 @@
-/**
- *   This file is part of Skript.
- *
- *  Skript is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Skript is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright Peter Güttinger, SkriptLang team and contributors
- */
 package ch.njol.skript.effects;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
-import ch.njol.skript.doc.RequiredPlugins;
 import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
@@ -31,48 +12,51 @@ import ch.njol.util.Kleenean;
 import org.bukkit.entity.Breedable;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.Event;
-import org.eclipse.jdt.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
 
 @Name("Make Breedable")
-@Description({
-		"Picks whether or not entities will be able to breed.",
-		"Only works on animals, not all living entities"
-})
+@Description("Sets whether or not entities will be able to breed. Only works on animals.")
 @Examples({
 	"on spawn of animal:",
 		"\tmake entity unbreedable"
 })
 @Since("INSERT VERSION")
-@RequiredPlugins("MC 1.16+")
 public class EffBreedable extends Effect {
 
 	static {
 		if (Skript.classExists("org.bukkit.entity.Breedable"))
-			Skript.registerEffect(EffBreedable.class, "make %livingentities% [negate:(not |non(-| )|un)]breedable");
+			Skript.registerEffect(EffBreedable.class,
+				"make %livingentities% breedable",
+				"unsterilize %livingentities%",
+				"make %livingentities% (not |non(-| )|un)breedable",
+				"sterilize %livingentities%");
 	}
 
-	private boolean canBreed;
+	private boolean sterilize;
 	private Expression<LivingEntity> entities;
 
 	@Override
-	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
-		canBreed = !parseResult.hasTag("negate");
-		entities = (Expression<LivingEntity>) exprs[0];
+	@SuppressWarnings("unchecked")
+	public boolean init(Expression<?>[] expressions, int matchedPattern,
+						Kleenean isDelayed, ParseResult parseResult) {
+		sterilize = matchedPattern > 1;
+		entities = (Expression<LivingEntity>) expressions[0];
 		return true;
 	}
 
 	@Override
 	protected void execute(Event event) {
-		for (LivingEntity livingEntity : entities.getArray(event)) {
-			if (livingEntity instanceof Breedable) {
-				((Breedable) livingEntity).setBreed(canBreed);
-			}
+		for (LivingEntity entity : entities.getArray(event)) {
+			if (!(entity instanceof Breedable breedable))
+				continue;
+
+			breedable.setBreed(!sterilize);
 		}
 	}
 
 	@Override
 	public String toString(@Nullable Event event, boolean debug) {
-		return "make " + entities.toString(event, debug) + (canBreed ? " " : " non-") + "breedable";
+		return "make " + entities.toString(event, debug) + (sterilize ? " non-" : " ") + "breedable";
 	}
 
 }
--- src/main/java/ch/njol/skript/effects/EffMakeAdult.java
+++ src/main/java/ch/njol/skript/effects/EffMakeAdult.java
@@ -1,85 +1,61 @@
-/**
- *   This file is part of Skript.
- *
- *  Skript is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Skript is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright Peter Güttinger, SkriptLang team and contributors
- */
 package ch.njol.skript.effects;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
-import ch.njol.skript.doc.RequiredPlugins;
 import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.Kleenean;
 import org.bukkit.entity.Ageable;
-import org.bukkit.entity.Animals;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.Event;
-import org.eclipse.jdt.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
 
-@Name("Make Adult")
-@Description("Force a animal to become an adult or baby, requires MC 1.16 for any mob")
+@Name("Make Adult/Baby")
+@Description("Force a animal to become an adult or baby.")
 @Examples({
 	"on spawn of mob:",
-		"\tentity is not an adult",
-		"\tmake entity an adult",
+	"\tentity is not an adult",
+	"\tmake entity an adult",
 })
 @Since("INSERT VERSION")
-@RequiredPlugins("1.16+ (Mobs)")
 public class EffMakeAdult extends Effect {
-
-	// This is required since before 1.16 the `setBaby`/'setAdult' method only supported Animals
-	private static final boolean HAS_MOB_SUPPORT = Skript.isRunningMinecraft(1, 16, 5);
 
 	static {
 		Skript.registerEffect(EffMakeAdult.class, "make %livingentities% [a[n]] (adult|:baby)");
 	}
 
-	private boolean makeBaby;
+	private boolean baby;
 	private Expression<LivingEntity> entities;
 
 	@Override
-	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
-		entities = (Expression<LivingEntity>) exprs[0];
-		makeBaby = parseResult.hasTag("baby");
+	public boolean init(Expression<?>[] expressions, int matchedPattern,
+						Kleenean isDelayed, ParseResult parseResult) {
+		baby = parseResult.hasTag("baby");
+		entities = (Expression<LivingEntity>) expressions[0];
 		return true;
 	}
 
 	@Override
 	protected void execute(Event event) {
-		for (LivingEntity livingEntity : entities.getArray(event)) {
-			if (livingEntity instanceof Ageable) {
-				if (!HAS_MOB_SUPPORT && !(livingEntity instanceof Animals))
-					continue;
-				if (makeBaby) {
-					((Ageable) livingEntity).setBaby();
-				} else {
-					((Ageable) livingEntity).setAdult();
-				}
+		for (LivingEntity entity : entities.getArray(event)) {
+			if (!(entity instanceof Ageable ageable))
+				continue;
+
+			if (baby) {
+				ageable.setBaby();
+			} else {
+				ageable.setAdult();
 			}
 		}
 	}
 
 	@Override
 	public String toString(@Nullable Event event, boolean debug) {
-		return "make " + entities + (makeBaby ? " a baby" : " an adult");
+		return "make " + entities + (baby ? " a baby" : " an adult");
 	}
 
 }
