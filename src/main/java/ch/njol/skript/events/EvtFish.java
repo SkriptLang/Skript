a8bdb764fb253c16dee64bb97bc2318ee836e4b1
Efnilite <35348263+Efnilite@users.noreply.github.com>
2024-10-08 19:59:23 +0200

    small touches

--- src/main/java/ch/njol/skript/events/EvtFish.java
+++ src/main/java/ch/njol/skript/events/EvtFish.java
@@ -1,28 +1,9 @@
-/**
- * This file is part of Skript.
- *
- * Skript is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Skript is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Skript.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright Peter Güttinger, SkriptLang team and contributors
- */
 package ch.njol.skript.events;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
-import ch.njol.skript.doc.RequiredPlugins;
 import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.Literal;
 import ch.njol.skript.lang.SkriptEvent;
@@ -35,7 +16,7 @@ import org.bukkit.entity.FishHook;
 import org.bukkit.event.Event;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.event.player.PlayerFishEvent.State;
-import org.eclipse.jdt.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -49,29 +30,28 @@ import java.util.List;
 	"on fishing state of caught entity:",
 		"\tpush event-entity vector from entity to player"
 })
-@RequiredPlugins("MC 1.14+ (reel in)")
 @Since("1.0, INSERT VERSION (fishing states, entity and hook)")
 public class EvtFish extends SkriptEvent {
 
 	static {
-		Skript.registerEvent("Fishing", EvtFish.class, PlayerFishEvent.class, "[player] fish[ing] [state[s] [of] %-fishingstates%]");
+		Skript.registerEvent("Fishing", EvtFish.class, PlayerFishEvent.class,
+			"[player] fish[ing] [state[s] [of] %-fishingstates%]");
 
-		EventValues.registerEventValue(PlayerFishEvent.class, FishHook.class, new Getter<FishHook, PlayerFishEvent>() {
+		EventValues.registerEventValue(PlayerFishEvent.class, FishHook.class, new Getter<>() {
 			@Override
 			public FishHook get(PlayerFishEvent event) {
 				return event.getHook();
 			}
 		}, EventValues.TIME_NOW);
-		EventValues.registerEventValue(PlayerFishEvent.class, State.class, new Getter<State, PlayerFishEvent>() {
+		EventValues.registerEventValue(PlayerFishEvent.class, State.class, new Getter<>() {
 			@Override
 			public State get(PlayerFishEvent event) {
 				return event.getState();
 			}
 		}, EventValues.TIME_NOW);
-		EventValues.registerEventValue(PlayerFishEvent.class, Entity.class, new Getter<Entity, PlayerFishEvent>() {
+		EventValues.registerEventValue(PlayerFishEvent.class, Entity.class, new Getter<>() {
 			@Override
-			@Nullable
-			public Entity get(PlayerFishEvent event) {
+			public @Nullable Entity get(PlayerFishEvent event) {
 				return event.getCaught();
 			}
 		}, EventValues.TIME_NOW);
@@ -80,21 +60,27 @@ public class EvtFish extends SkriptEvent {
 	private List<State> states = new ArrayList<>(0);
 
 	@Override
-	@SuppressWarnings("unchecked")
 	public boolean init(Literal<?>[] args, int matchedPattern, ParseResult parseResult) {
 		if (args[0] != null)
+			//noinspection unchecked
 			states = Arrays.asList(((Literal<State>) args[0]).getAll());
+
 		return true;
 	}
 
 	@Override
 	public boolean check(Event event) {
-		return states.isEmpty() || states.contains(((PlayerFishEvent) event).getState());
+		if (!(event instanceof PlayerFishEvent fishEvent))
+			return false;
+
+		return states.isEmpty() || states.contains(fishEvent.getState());
 	}
 
 	@Override
 	public String toString(@Nullable Event event, boolean debug) {
-		return states.isEmpty() ? "fishing" : "fishing states of " + StringUtils.join(states, ", ", " and ");
+		return states.isEmpty() ?
+			"fishing" :
+			"fishing states of " + StringUtils.join(states, ", ", " and ");
 	}
 
 }
