3a2a71af4bc36f79b2292e1e2f56519870c76ffb
XPYEX <XPYEX0@163.com>
2024-10-16 01:08:54 +0800

    refactor: sort codes

--- src/main/java/ch/njol/skript/hooks/economy/classes/Money.java
+++ src/main/java/ch/njol/skript/hooks/economy/classes/Money.java
@@ -1,82 +1,60 @@
-/**
- *   This file is part of Skript.
- *
- *  Skript is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Skript is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright Peter Güttinger, SkriptLang team and contributors
- */
 package ch.njol.skript.hooks.economy.classes;
 
 import ch.njol.skript.Skript;
-import ch.njol.skript.classes.data.JavaClasses;
-import org.jetbrains.annotations.Nullable;
-
 import ch.njol.skript.classes.ClassInfo;
-import org.skriptlang.skript.lang.arithmetic.Arithmetics;
-import org.skriptlang.skript.lang.arithmetic.Operator;
-import org.skriptlang.skript.lang.comparator.Comparator;
-import org.skriptlang.skript.lang.converter.Converter;
 import ch.njol.skript.classes.Parser;
+import ch.njol.skript.classes.data.JavaClasses;
 import ch.njol.skript.hooks.VaultHook;
 import ch.njol.skript.lang.ParseContext;
 import ch.njol.skript.registrations.Classes;
-import org.skriptlang.skript.lang.comparator.Comparators;
-import org.skriptlang.skript.lang.converter.Converters;
 import ch.njol.util.StringUtils;
+import org.jetbrains.annotations.Nullable;
+import org.skriptlang.skript.lang.arithmetic.Arithmetics;
+import org.skriptlang.skript.lang.arithmetic.Operator;
+import org.skriptlang.skript.lang.comparator.Comparator;
+import org.skriptlang.skript.lang.comparator.Comparators;
 import org.skriptlang.skript.lang.comparator.Relation;
+import org.skriptlang.skript.lang.converter.Converter;
+import org.skriptlang.skript.lang.converter.Converters;
 
-/**
- * @author Peter Güttinger
- */
-public class Money {
+public record Money(double amount) {
 	static {
 		Classes.registerClass(new ClassInfo<>(Money.class, "money")
-				.user("money")
-				.name("Money")
-				.description("A certain amount of money. Please note that this differs from <a href='#number'>numbers</a> as it includes a currency symbol or name, but usually the two are interchangeable, e.g. you can both <code>add 100$ to the player's balance</code> and <code>add 100 to the player's balance</code>.")
-				.usage("&lt;number&gt; $ or $ &lt;number&gt;, where '$' is your server's currency, e.g. '10 rupees' or '£5.00'")
-				.examples("add 10£ to the player's account",
-						"remove Fr. 9.95 from the player's money",
-						"set the victim's money to 0",
-						"increase the attacker's balance by the level of the victim * 100")
-				.since("2.0")
-				.before("itemtype", "itemstack")
-				.requiredPlugins("Vault", "an economy plugin that supports Vault")
-				.parser(new Parser<Money>() {
-					@Override
-					@Nullable
-					public Money parse(final String s, final ParseContext context) {
-						return Money.parse(s);
-					}
-					
-					@Override
-					public String toString(final Money m, final int flags) {
-						return m.toString();
-					}
-					
-					@Override
-					public String toVariableNameString(final Money o) {
-						return "money:" + o.amount;
-					}
-                }));
-		
+								  .user("money")
+								  .name("Money")
+								  .description("A certain amount of money. Please note that this differs from <a href='#number'>numbers</a> as it includes a currency symbol or name, but usually the two are interchangeable, e.g. you can both <code>add 100$ to the player's balance</code> and <code>add 100 to the player's balance</code>.")
+								  .usage("&lt;number&gt; $ or $ &lt;number&gt;, where '$' is your server's currency, e.g. '10 rupees' or '£5.00'")
+								  .examples("add 10£ to the player's account",
+									  "remove Fr. 9.95 from the player's money",
+									  "set the victim's money to 0",
+									  "increase the attacker's balance by the level of the victim * 100")
+								  .since("2.0")
+								  .before("itemtype", "itemstack")
+								  .requiredPlugins("Vault", "an economy plugin that supports Vault")
+								  .parser(new Parser<Money>() {
+									  @Override
+									  @Nullable
+									  public Money parse(final String s, final ParseContext context) {
+										  return Money.parse(s);
+									  }
+
+									  @Override
+									  public String toString(final Money m, final int flags) {
+										  return m.toString();
+									  }
+
+									  @Override
+									  public String toVariableNameString(final Money o) {
+										  return "money:" + o.amount;
+									  }
+								  }));
+
 		Comparators.registerComparator(Money.class, Money.class, new Comparator<Money, Money>() {
 			@Override
 			public Relation compare(final Money m1, final Money m2) {
 				return Relation.get(m1.amount - m2.amount);
 			}
-			
+
 			@Override
 			public boolean supportsOrdering() {
 				return true;
@@ -87,43 +65,34 @@ public class Money {
 			public Relation compare(final Money m, final Number n) {
 				return Relation.get(m.amount - n.doubleValue());
 			}
-			
+
 			@Override
 			public boolean supportsOrdering() {
 				return true;
 			}
 		});
-		
+
 		Converters.registerConverter(Money.class, Double.class, new Converter<Money, Double>() {
 			@Override
 			public Double convert(final Money m) {
-				return Double.valueOf(m.getAmount());
+				return Double.valueOf(m.amount());
 			}
 		});
 
-		Arithmetics.registerOperation(Operator.ADDITION, Money.class, (left, right) -> new Money(left.getAmount() + right.getAmount()));
-		Arithmetics.registerOperation(Operator.SUBTRACTION, Money.class, (left, right) -> new Money(left.getAmount() - right.getAmount()));
-		Arithmetics.registerOperation(Operator.MULTIPLICATION, Money.class, (left, right) -> new Money(left.getAmount() * right.getAmount()));
-		Arithmetics.registerOperation(Operator.DIVISION, Money.class, (left, right) -> new Money(left.getAmount() / right.getAmount()));
+		Arithmetics.registerOperation(Operator.ADDITION, Money.class, (left, right) -> new Money(left.amount() + right.amount()));
+		Arithmetics.registerOperation(Operator.SUBTRACTION, Money.class, (left, right) -> new Money(left.amount() - right.amount()));
+		Arithmetics.registerOperation(Operator.MULTIPLICATION, Money.class, (left, right) -> new Money(left.amount() * right.amount()));
+		Arithmetics.registerOperation(Operator.DIVISION, Money.class, (left, right) -> new Money(left.amount() / right.amount()));
 		Arithmetics.registerDifference(Money.class, (left, right) -> {
-			double result = Math.abs(left.getAmount() - right.getAmount());
+			double result = Math.abs(left.amount() - right.amount());
 			if (result < Skript.EPSILON)
 				return new Money(0);
 			return new Money(result);
 		});
 		Arithmetics.registerDefaultValue(Money.class, () -> new Money(0));
 	}
-	
-	final double amount;
-	
-	public Money(final double amount) {
-		this.amount = amount;
-	}
-	
-	public double getAmount() {
-		return amount;
-	}
-	
+
+
 	@SuppressWarnings({"null", "unused"})
 	@Nullable
 	public static Money parse(final String s) {
@@ -166,10 +135,10 @@ public class Money {
 			return null;
 		}
 	}
-	
+
 	@Override
 	public String toString() {
-		return "" + VaultHook.economy.format(amount);
+		return VaultHook.economy.format(amount);
 	}
-	
+
 }
--- src/main/java/ch/njol/skript/hooks/economy/expressions/ExprBalance.java
+++ src/main/java/ch/njol/skript/hooks/economy/expressions/ExprBalance.java
@@ -1,26 +1,4 @@
-/**
- *   This file is part of Skript.
- *
- *  Skript is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Skript is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright Peter Güttinger, SkriptLang team and contributors
- */
 package ch.njol.skript.hooks.economy.expressions;
-
-import org.bukkit.OfflinePlayer;
-import org.bukkit.event.Event;
-import org.jetbrains.annotations.Nullable;
 
 import ch.njol.skript.classes.Changer.ChangeMode;
 import ch.njol.skript.doc.Description;
@@ -31,6 +9,9 @@ import ch.njol.skript.doc.Since;
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import ch.njol.skript.hooks.VaultHook;
 import ch.njol.skript.hooks.economy.classes.Money;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
 
 @Name("Money")
 @Description("How much virtual money a player has (can be changed).")
@@ -46,7 +27,7 @@ public class ExprBalance extends SimplePropertyExpression<OfflinePlayer, Money> 
 	static {
 		register(ExprBalance.class, Money.class, "(money|balance|[bank] account)", "offlineplayers");
 	}
-	
+
 	@Override
 	public Money convert(OfflinePlayer player) {
 		try {
@@ -55,25 +36,25 @@ public class ExprBalance extends SimplePropertyExpression<OfflinePlayer, Money> 
 			return new Money(VaultHook.economy.getBalance(player.getName()));
 		}
 	}
-	
-	@Override
-	public Class<? extends Money> getReturnType() {
-		return Money.class;
-	}
-	
+
 	@Override
 	protected String getPropertyName() {
 		return "money";
 	}
-	
+
+	@Override
+	public Class<? extends Money> getReturnType() {
+		return Money.class;
+	}
+
 	@Override
 	@Nullable
 	public Class<?>[] acceptChange(ChangeMode mode) {
 		if (mode == ChangeMode.REMOVE_ALL)
 			return null;
-		return new Class[] {Money.class, Number.class};
+		return new Class[]{Money.class, Number.class};
 	}
-	
+
 	@Override
 	public void change(Event event, @Nullable Object[] delta, ChangeMode mode) {
 		if (delta == null) { // RESET/DELETE
@@ -82,7 +63,7 @@ public class ExprBalance extends SimplePropertyExpression<OfflinePlayer, Money> 
 			return;
 		}
 
-		double money = delta[0] instanceof Number ? ((Number) delta[0]).doubleValue() : ((Money) delta[0]).getAmount();
+		double money = delta[0] instanceof Number ? ((Number) delta[0]).doubleValue() : ((Money) delta[0]).amount();
 		for (OfflinePlayer player : getExpr().getArray(event)) {
 			switch (mode) {
 				case SET:
@@ -102,5 +83,5 @@ public class ExprBalance extends SimplePropertyExpression<OfflinePlayer, Money> 
 			}
 		}
 	}
-	
+
 }
