32416bd9cd7cef008fb19e1866ec8f548bdca2af
Efnilite <35348263+Efnilite@users.noreply.github.com>
2024-10-29 18:13:41 +0100

    update javadoc

--- src/main/java/ch/njol/skript/lang/Debuggable.java
+++ src/main/java/ch/njol/skript/lang/Debuggable.java
@@ -26,25 +26,72 @@ public interface Debuggable {
 	@Override
 	String toString();
 
+	/**
+	 * Formats a string with the given (optional) arguments, for constructing a {@link #toString(Event, boolean)}
+	 * representation.
+	 *
+	 * @param string The string to format.
+	 * @param args The args to replace.
+	 * @return The formatted string.
+	 * @see String#format(String, Object...)
+	 */
 	default String formattedToString(@NotNull String string, Object... args) {
 		return string.formatted(args).replaceAll("  +", " ");
 	}
 
-	default <T> OptionalDebugParameter<T> optional(T value, Function<@NotNull T, String> ifNotNull) {
+	/**
+	 * Represents an optional value in a {@link #toString(Event, boolean)} method.
+	 * <ul>
+	 *     <li>If {@code value} is not null, returns a class which returns the result of the function {@code ifNotNull}.
+	 *     This function applies a not-null {@code value} to get a string, which removes null warnings.</li>
+	 *     <li>If {@code value} is null, returns a class which returns an empty string.</li>
+	 * </ul>
+	 *
+	 * @param value The potentially null value.
+	 * @param ifNotNull The function to apply if {@code value} is not null.
+	 * @return A class which gives the string representation of the value, or an empty string if it is null.
+	 * @param <T> The type of the value.
+	 */
+	default <T> OptionalDebugParameter<T> optional(
+		@Nullable T value,
+		@NotNull Function<@NotNull T, String> ifNotNull
+	) {
 		return optional(value, ifNotNull, "");
 	}
 
-	default <T> OptionalDebugParameter<T> optional(T value, Function<@NotNull T, String> ifNotNull, String ifNull) {
+	/**
+	 * Represents an optional value in a {@link #toString(Event, boolean)} method.
+	 * <ul>
+	 *     <li>If {@code value} is not null, returns a class which returns the result of the function {@code ifNotNull}.
+	 *     This function applies a not-null {@code value} to get a string, which removes null warnings.</li>
+	 *     <li>If {@code value} is null, returns a class which returns {@code ifNull}.</li>
+	 * </ul>
+	 *
+	 * @param value The potentially null value.
+	 * @param ifNotNull The function to apply if {@code value} is not null.
+	 * @param ifNull The string to return if {@code value} is null.
+	 * @return A class which gives the string representation of the value, or {@code ifNull} if it is null.
+	 * @param <T> The type of the value.
+	 */
+	default <T> OptionalDebugParameter<T> optional(
+		@Nullable T value,
+		@NotNull Function<@NotNull T, String> ifNotNull,
+		@NotNull String ifNull
+	) {
 		return new OptionalDebugParameter<>(value, ifNotNull, ifNull);
 	}
 
 	final class OptionalDebugParameter<T> {
 
-		private final T value;
-		private final Function<@NotNull T, String> ifNotNull;
-		private final String ifNull;
+		private final @Nullable T value;
+		private final @NotNull Function<@NotNull T, String> ifNotNull;
+		private final @NotNull String ifNull;
 
-		public OptionalDebugParameter(T value, Function<@NotNull T, String> ifNotNull, String ifNull) {
+		public OptionalDebugParameter(
+			@Nullable T value,
+			@NotNull Function<@NotNull T, String> ifNotNull,
+			@NotNull String ifNull
+		) {
 			this.value = value;
 			this.ifNotNull = ifNotNull;
 			this.ifNull = ifNull;
