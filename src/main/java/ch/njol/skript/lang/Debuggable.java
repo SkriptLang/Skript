8c24f24b5af5fd968d6583fa31d558a297c09dc3
Efnilite <35348263+Efnilite@users.noreply.github.com>
2024-10-29 23:28:41 +0100

    add docs

--- src/main/java/ch/njol/skript/lang/Debuggable.java
+++ src/main/java/ch/njol/skript/lang/Debuggable.java
@@ -29,7 +29,7 @@ public interface Debuggable {
 	/**
 	 * @param event The event to get information from. This is always null if debug == false.
 	 * @param debug If true this should print more information, if false this should print what is shown to the end user
-	 * @return String representation of this object.
+	 * @return String representation of this object
 	 */
 	String toString(@Nullable Event event, boolean debug);
 
--- src/main/java/ch/njol/skript/lang/SyntaxStringBuilder.java
+++ src/main/java/ch/njol/skript/lang/SyntaxStringBuilder.java
@@ -6,22 +6,101 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.StringJoiner;
 
+/**
+ * Utility class to build syntax strings, used in {@link Debuggable#toString(Event, boolean)}.
+ */
 public class SyntaxStringBuilder {
 
 	private final boolean debug;
 	private final @Nullable Event event;
 	private final StringJoiner joiner = new StringJoiner(" ");
 
+	/**
+	 * Creates a new SyntaxStringBuilder.
+	 *
+	 * @param event The event to get information from. This is always null if debug == false.
+	 * @param debug If true this should print more information, if false this should print what is shown to the end user
+	 */
 	public SyntaxStringBuilder(@Nullable Event event, boolean debug) {
 		this.event = event;
 		this.debug = debug;
 	}
 
+	/**
+	 * Adds a boolean to the string.
+	 * @param b The boolean to add.
+	 * @return This SyntaxStringBuilder.
+	 */
+	public SyntaxStringBuilder add(boolean b) {
+		joiner.add(Boolean.toString(b));
+		return this;
+	}
+
+	/**
+	 * Adds an integer to the string.
+	 * @param i The integer to add.
+	 * @return This SyntaxStringBuilder.
+	 */
+	public SyntaxStringBuilder add(int i) {
+		joiner.add(Integer.toString(i));
+		return this;
+	}
+
+	/**
+	 * Adds a long to the string.
+	 * @param l The long to add.
+	 * @return This SyntaxStringBuilder.
+	 */
+	public SyntaxStringBuilder add(long l) {
+		joiner.add(Long.toString(l));
+		return this;
+	}
+
+	/**
+	 * Adds a double to the string.
+	 * @param d The double to add.
+	 * @return This SyntaxStringBuilder.
+	 */
+	public SyntaxStringBuilder add(double d) {
+		joiner.add(Double.toString(d));
+		return this;
+	}
+
+	/**
+	 * Adds a float to the string.
+	 * @param f The float to add.
+	 * @return This SyntaxStringBuilder.
+	 */
+	public SyntaxStringBuilder add(float f) {
+		joiner.add(Float.toString(f));
+		return this;
+	}
+
+	/**
+	 * Adds a char to the string.
+	 * @param c The char to add.
+	 * @return This SyntaxStringBuilder.
+	 */
+	public SyntaxStringBuilder add(char c) {
+		joiner.add(Character.toString(c));
+		return this;
+	}
+
+	/**
+	 * Adds a string to the string.
+	 * @param string The string to add.
+	 * @return This SyntaxStringBuilder.
+	 */
 	public SyntaxStringBuilder add(@NotNull String string) {
 		joiner.add(string);
 		return this;
 	}
 
+	/**
+	 * Adds a {@link Debuggable} object to the string, which is usually an expression or literal.
+	 * @param debuggable The {@link Debuggable} to add.
+	 * @return This SyntaxStringBuilder.
+	 */
 	public SyntaxStringBuilder add(@NotNull Debuggable debuggable) {
 		joiner.add(debuggable.toString(event, debug));
 		return this;
