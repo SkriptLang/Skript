5bbccfecdb29f14efd3bfcd2cf2f880d6ea6f6a5
Efnilite <35348263+Efnilite@users.noreply.github.com>
2024-11-02 14:40:04 +0100

    remove deprecated method usage

--- src/test/java/org/skriptlang/skript/test/tests/utils/DateTest.java
+++ src/test/java/org/skriptlang/skript/test/tests/utils/DateTest.java
@@ -3,8 +3,6 @@ package org.skriptlang.skript.test.tests.utils;
 import ch.njol.skript.util.Date;
 import org.junit.Test;
 
-import java.util.TimeZone;
-
 import static org.junit.Assert.assertEquals;
 
 public class DateTest {
@@ -12,21 +10,14 @@ public class DateTest {
 	@Test
 	public void testNow() {
 		Date now = Date.now();
-		assertEquals(System.currentTimeMillis(), now.getTimestamp());
+		assertEquals(System.currentTimeMillis(), now.getTime());
 	}
 
 	@Test
 	public void testFromJavaDate() {
 		java.util.Date javaDate = new java.util.Date();
 		Date date = Date.fromJavaDate(javaDate);
-		assertEquals(javaDate.getTime(), date.getTimestamp());
-	}
-
-	@Test
-	public void testToJavaDate() {
-		Date date = Date.now();
-		java.util.Date javaDate = date.toJavaDate();
-		assertEquals(date.getTimestamp(), javaDate.getTime());
+		assertEquals(javaDate.getTime(), date.getTime());
 	}
 
 }
