31e0039fb0d39ed57e6e5deb04763c37a0a4cdfe
Moderocky <admin@moderocky.com>
2024-11-21 08:03:20 +0000

    Script type & expression. (#6702)
    * Create Script type.
    
    * Support script string/name conversion.
    
    * Script expression.
    
    * Add script lang entry.
    
    * Tests for script expression & names.
    
    * Support all scripts expression.
    
    * Script effects & tests.
    
    * Create dummy Script handle for disabled scripts.
    
    * Script reflection feature flag.
    
    * Restrict literal parsing to commands & parse.
    
    * Test feature flag for resolving name.
    
    * Split ExprScripts by feature to support disabled scripts.
    
    * Fix ExprName tests for new & old behaviour.
    
    * Add tests for disabled script handles.
    
    * Apply suggestions from code review
    
    Co-authored-by: Patrick Miller <apickledwalrus@gmail.com>
    
    * Improve script loading/unloading safety.
    
    * Add feature check for script hotswapping.
    
    * Use expression stream.
    
    * Conformity for file names and proper loading safety.
    
    * Document validity & add condition support.
    
    * Add script is loaded condition + tests.
    
    * Some changes from code review.
    
    * More changes.
    
    * Apply suggestions from code review
    
    Co-authored-by: Patrick Miller <apickledwalrus@gmail.com>
    
    * Clean up EffScriptFile and rename ExprScripts variations.
    
    * Finish code review changes.
    
    * Clean up script loader stuff.
    
    * Fix conflicts.
    
    * Suggested changes.
    
    * Apply suggestions from code review
    
    Co-authored-by: Patrick Miller <apickledwalrus@gmail.com>
    
    * Finished.
    
    ---------
    
    Co-authored-by: Patrick Miller <apickledwalrus@gmail.com>
    Co-authored-by: sovdee <10354869+sovdeeth@users.noreply.github.com>

--- src/main/java/ch/njol/skript/config/Config.java
+++ src/main/java/ch/njol/skript/config/Config.java
@@ -23,6 +23,7 @@ import java.util.Map;
 import java.util.Set;
 
 import ch.njol.skript.log.SkriptLogger;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
