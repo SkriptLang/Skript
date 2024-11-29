992bba1e154b49eb378373874950110109873361
Moderocky <admin@moderocky.com>
2024-11-29 09:59:51 +0000

    Fix return type.

--- src/main/java/ch/njol/skript/expressions/ExprElement.java
+++ src/main/java/ch/njol/skript/expressions/ExprElement.java
@@ -230,6 +230,8 @@ public class ExprElement<T> extends SimpleExpression<T> {
 
 	@Override
 	public Class<? extends T> getReturnType() {
+		if (queue)
+			return (Class<? extends T>) Object.class;
 		return expr.getReturnType();
 	}
 
