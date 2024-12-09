e14ff8dd6d2f978108848c57aaef8099fa9fa31f
Burbulinis <martynasaleksandras19@gmail.com>
2024-12-09 19:18:51 +0200

    change exampled for loot table seed

--- src/main/java/org/skriptlang/skript/bukkit/loottables/elements/expressions/ExprLootTableSeed.java
+++ src/main/java/org/skriptlang/skript/bukkit/loottables/elements/expressions/ExprLootTableSeed.java
@@ -15,7 +15,7 @@ import org.skriptlang.skript.bukkit.loottables.LootTableUtils;
 @Name("Loot Table Seed")
 @Description("Returns the seed of a loot table. Setting the seed of a block or entity that does not have a loot table will not do anything.")
 @Examples({
-	"loot seed of block",
+	"set {_seed} loot table seed of block",
 	"set loot table seed of entity to 123456789"
 })
 @Since("INSERT VERSION")
