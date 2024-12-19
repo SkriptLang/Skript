2e750369e1a88c87a565db684538f77dd0caa573
Efnilite <35348263+Efnilite@users.noreply.github.com>
2024-12-19 14:39:25 +0100

    update docs, update message

--- src/main/java/ch/njol/skript/lang/SyntaxElement.java
+++ src/main/java/ch/njol/skript/lang/SyntaxElement.java
@@ -1,21 +1,3 @@
-/**
- *   This file is part of Skript.
- *
- *  Skript is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Skript is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright Peter Güttinger, SkriptLang team and contributors
- */
 package ch.njol.skript.lang;
 
 import ch.njol.skript.lang.SkriptParser.ParseResult;
@@ -49,11 +31,15 @@ public interface SyntaxElement {
 	}
 
 	/**
-	 * Returns all supported events for this syntax element.
+	 * Returns all supported events for this syntax element. By default, all events are accepted.
 	 * <p>
-	 * If the event is
+	 * Before {@link #init(Expression[], int, Kleenean, ParseResult)} is called, checks
+	 * to see if the current event is supported by this syntax element.
+	 * If it is not, an error will be printed and the syntax element will not be initialised.
 	 * </p>
-	 * @return All supported
+	 *
+	 * @return All supported event classes.
+	 * @see ch.njol.util.coll.CollectionUtils#array(Object[])
 	 */
 	default Class<? extends Event>[] supportedEvents() {
 		//noinspection unchecked
