5ac41dc2149617adaeb4151579a45101b9050c9a
ShaneBeee <shanebolenback@me.com>
2025-01-03 12:19:34 -0800

    HTMLGenerator - fix "new" thingy in docs

--- src/main/java/ch/njol/skript/doc/HTMLGenerator.java
+++ src/main/java/ch/njol/skript/doc/HTMLGenerator.java
@@ -45,8 +45,8 @@ import java.util.stream.Collectors;
  */
 public class HTMLGenerator extends DocumentationGenerator {
 
-	private static final String SKRIPT_VERSION = Skript.getVersion().toString().replaceAll("-(dev|alpha|beta)\\d*", ""); // Filter branches
-	private static final Pattern NEW_TAG_PATTERN = Pattern.compile(SKRIPT_VERSION + "(?!\\.)"); // (?!\\.) to avoid matching 2.6 in 2.6.1 etc.
+	private static final String SKRIPT_VERSION = Skript.getVersion().toString().replaceAll("-(dev|alpha|beta|pre)\\d*", "").replace(".0", ""); // Filter branches
+	private static final Pattern NEW_TAG_PATTERN = Pattern.compile(SKRIPT_VERSION + "(?!\\.[1-9])"); // (?!\\.) to avoid matching 2.6 in 2.6.1 etc.
 	private static final Pattern RETURN_TYPE_LINK_PATTERN = Pattern.compile("( ?href=\"(classes\\.html|)#|)\\$\\{element\\.return-type-linkcheck}");
 
 	private final String skeleton;
