87410dcbc2c8ce9bb60b186e3ed57a97b60d0687
Burbulinis <martynasaleksandras19@gmail.com>
2025-01-04 15:26:05 +0200

    Add condition and types in default.lang

--- src/main/java/org/skriptlang/skript/bukkit/spawner/util/SpawnRuleWrapper.java
+++ src/main/java/org/skriptlang/skript/bukkit/spawner/util/SpawnRuleWrapper.java
@@ -10,28 +10,28 @@ public class SpawnRuleWrapper extends SpawnRule {
 
 	@Override
 	public void setMinBlockLight(int minBlockLight) {
-		if (getMaxBlockLight() >= minBlockLight) {
+		if (getMaxBlockLight() >= minBlockLight && minBlockLight >= 0 && minBlockLight <= 15) {
 			super.setMinBlockLight(minBlockLight);
 		}
 	}
 
 	@Override
 	public void setMaxBlockLight(int maxBlockLight) {
-		if (getMinBlockLight() <= maxBlockLight) {
+		if (getMinBlockLight() <= maxBlockLight && maxBlockLight >= 0 && maxBlockLight <= 15) {
 			super.setMaxBlockLight(maxBlockLight);
 		}
 	}
 
 	@Override
 	public void setMinSkyLight(int minSkyLight) {
-		if (getMaxSkyLight() >= minSkyLight) {
+		if (getMaxSkyLight() >= minSkyLight && minSkyLight >= 0 && minSkyLight <= 15) {
 			super.setMinSkyLight(minSkyLight);
 		}
 	}
 
 	@Override
 	public void setMaxSkyLight(int maxSkyLight) {
-		if (getMinSkyLight() <= maxSkyLight) {
+		if (getMinSkyLight() <= maxSkyLight && maxSkyLight >= 0 && maxSkyLight <= 15) {
 			super.setMaxSkyLight(maxSkyLight);
 		}
 	}
