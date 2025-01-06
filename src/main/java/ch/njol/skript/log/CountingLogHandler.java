3d8aff9ff60392b34efd86d2d49fcce2f7b7a3a8
ShaneBeee <shanebolenback@me.com>
2025-01-05 17:22:11 -0800

    CountingLogHandler - revert

--- src/main/java/ch/njol/skript/log/CountingLogHandler.java
+++ src/main/java/ch/njol/skript/log/CountingLogHandler.java
@@ -1,12 +1,5 @@
 package ch.njol.skript.log;
 
-import ch.njol.skript.config.Node;
-import ch.njol.skript.lang.parser.ParserInstance;
-import ch.njol.skript.test.runner.EvtTestCase;
-import ch.njol.skript.test.runner.TestMode;
-import ch.njol.skript.test.runner.TestTracker;
-import org.skriptlang.skript.lang.structure.Structure;
-
 import java.util.logging.Level;
 
 /**
@@ -28,20 +21,6 @@ public class CountingLogHandler extends LogHandler {
 	public LogResult log(LogEntry entry) {
 		if (entry.level.intValue() >= minimum) {
 			count++;
-			if (TestMode.ENABLED) {
-				ParserInstance parser = ParserInstance.get();
-				Structure struct = parser.getCurrentStructure();
-				Node node = parser.getNode();
-
-				String name = struct instanceof EvtTestCase test ? test.getTestName() : struct != null ? struct.getSyntaxTypeName() : null;
-				TestTracker.parsingStarted(name);
-
-				if (node != null) {
-					TestTracker.testFailed(entry.getMessage(), parser.getCurrentScript(), node.getLine());
-				} else {
-					TestTracker.testFailed(entry.getMessage());
-				}
-			}
 		}
 		return LogResult.LOG;
 	}
