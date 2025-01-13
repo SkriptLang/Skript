377e7c110008527cebc2bf0d10f63ffa149991d5
APickledWalrus <apickledwalrus@gmail.com>
2025-01-13 16:54:15 -0500

    Include example scripts in tests

--- src/main/java/ch/njol/skript/Skript.java
+++ src/main/java/ch/njol/skript/Skript.java
@@ -813,9 +813,14 @@ public final class Skript extends JavaPlugin implements Listener {
 			TestingLogHandler errorCounter = new TestingLogHandler(Level.SEVERE);
 			try {
 				errorCounter.start();
-				File testDir = TestMode.TEST_DIR.toFile();
-				assert testDir != null;
-				ScriptLoader.loadScripts(testDir, errorCounter);
+
+				// load example scripts (cleanup after)
+				ScriptLoader.unloadScripts(ScriptLoader.getLoadedScripts());
+				ScriptLoader.loadScripts(new File(getScriptsFolder(), "-examples" + File.separator), errorCounter);
+				ScriptLoader.unloadScripts(ScriptLoader.getLoadedScripts());
+
+				// load test directory scripts
+				ScriptLoader.loadScripts(TestMode.TEST_DIR.toFile(), errorCounter);
 			} finally {
 				errorCounter.stop();
 			}
