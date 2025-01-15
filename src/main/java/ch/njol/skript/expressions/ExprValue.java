04d22b87c20fa2b56e2897cdf8681e5c81c2b636
Moderocky <admin@moderocky.com>
2025-01-15 10:16:38 +0000

    Clean up some bad imports.

--- src/main/java/ch/njol/skript/expressions/ExprValue.java
+++ src/main/java/ch/njol/skript/expressions/ExprValue.java
@@ -3,7 +3,6 @@ package ch.njol.skript.expressions;
 import ch.njol.skript.Skript;
 import ch.njol.skript.classes.Changer.ChangeMode;
 import ch.njol.skript.classes.ClassInfo;
-import ch.njol.skript.config.EntryNode;
 import ch.njol.skript.config.Node;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
@@ -22,7 +21,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.lang.reflect.Array;
-import java.util.Arrays;
 
 @Name("Value")
 @Description({
