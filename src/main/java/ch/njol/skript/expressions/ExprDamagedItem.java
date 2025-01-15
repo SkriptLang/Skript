08d7a5b3026a07fbe9db901025f793251506d24a
NagasonicDev <153145285+NagasonicDev@users.noreply.github.com>
2025-01-15 19:37:11 +1100

    Fix Issues

--- src/main/java/ch/njol/skript/expressions/ExprDamagedItem.java
+++ src/main/java/ch/njol/skript/expressions/ExprDamagedItem.java
@@ -1,9 +1,8 @@
 package ch.njol.skript.expressions;
 
-import ch.njol.skript.aliases.ItemData;
 import ch.njol.skript.bukkitutil.ItemUtils;
+import ch.njol.skript.lang.SkriptParser;
 import org.bukkit.event.Event;
-import org.bukkit.inventory.meta.Damageable;
 import org.jetbrains.annotations.Nullable;
 
 import ch.njol.skript.Skript;
@@ -15,24 +14,23 @@ import ch.njol.skript.doc.Since;
 import ch.njol.skript.expressions.base.PropertyExpression;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
-import ch.njol.skript.lang.SkriptParser;
 import ch.njol.util.Kleenean;
 
 @Name("Damaged Item")
 @Description({
-	"Directly changes the durability of an item.",
-	"Damage is used to remove the specified number from the base durability of an item. E.g 400 - damage",
-	"Durability is used to modify the total durability"
+		"Directly changes the durability of an item.",
+		"Damage is used to remove the specified number from the base durability of an item. E.g 400 - damage",
+		"Durability is used to modify the total durability"
 })
 @Examples({"give player diamond sword with damage value 100", "set player's tool to diamond hoe damaged by 250",
-	"give player diamond sword with damage 700 named \"BROKEN SWORD\"",
-	"set {_item} to diamond hoe with damage value 50 named \"SAD HOE\"",
-	"set target block of player to wool with data value 1", "set target block of player to potato plant with data value 7",
-	"give player wooden sword with 1 durability named \"VERY BROKEN SWORD\"",
-	"set player's tool to diamond hoe with durability 500"})
-@Since("2.4")
+		"give player diamond sword with damage 700 named \"BROKEN SWORD\"",
+		"set {_item} to diamond hoe with damage value 50 named \"SAD HOE\"",
+		"set target block of player to wool with data value 1", "set target block of player to potato plant with data value 7",
+		"give player wooden sword with 1 durability named \"VERY BROKEN SWORD\"",
+		"set player's tool to diamond hoe with durability 500"})
+@Since("2.4, 2.10 (with durability)")
 public class ExprDamagedItem extends PropertyExpression<ItemType, ItemType> {
-	
+
 	static {
 		Skript.registerExpression(ExprDamagedItem.class, ItemType.class, ExpressionType.COMBINED,
 				"%itemtype% with (damage|data) [value] %number%",
@@ -40,27 +38,30 @@ public class ExprDamagedItem extends PropertyExpression<ItemType, ItemType> {
 				"%itemtype% with durability %number%",
 				"%itemtype% with %number% durability");
 	}
-	
+
 	@SuppressWarnings("null")
 	private Expression<Number> damage;
-	private int pattern;
-	
+	private boolean hasDurability;
+
 	@Override
 	@SuppressWarnings({"unchecked", "null"})
-	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
 		setExpr((Expression<ItemType>) exprs[0]);
 		damage = (Expression<Number>) exprs[1];
-		pattern = matchedPattern;
+		hasDurability = matchedPattern > 1 ? true : false;
+		if (matchedPattern == 0) {
+			Skript.warning("Data value is deprecated and is only used in older Minecraft versions that do not support this Skript version.");
+		}
 		return true;
 	}
-	
+
 	@Override
 	protected ItemType[] get(Event e, ItemType[] source) {
 		Number damage = this.damage.getSingle(e);
 		if (damage == null)
 			return source;
 		return get(source.clone(), item -> {
-			if (pattern > 1) {
+			if (hasDurability == true) {
 				ItemUtils.setDamage(item, ItemUtils.getMaxDamage(item) - damage.intValue());
 			} else {
 				item.iterator().forEachRemaining(i -> i.setDurability(damage.intValue()));
@@ -68,15 +69,15 @@ public class ExprDamagedItem extends PropertyExpression<ItemType, ItemType> {
 			return item;
 		});
 	}
-	
+
 	@Override
 	public Class<? extends ItemType> getReturnType() {
 		return ItemType.class;
 	}
-	
+
 	@Override
-	public String toString(Nullable Event event, boolean debug) {
-		if (pattern > 1)
+	public String toString(@Nullable Event event, boolean debug) {
+		if (hasDurability == true)
 			return getExpr().toString(event, debug) + " with durability " + damage.toString(event, debug);
 
 		return getExpr().toString(event, debug) + " with damage value " + damage.toString(event, debug);
