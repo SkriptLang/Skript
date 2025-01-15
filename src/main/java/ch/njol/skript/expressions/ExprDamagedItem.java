cc6bdc3078d1123bc668f816c028a42eecc91a78
NagasonicDev <153145285+NagasonicDev@users.noreply.github.com>
2025-01-15 18:30:15 +1100

    Reverse Durability

--- src/main/java/ch/njol/skript/expressions/ExprDamagedItem.java
+++ src/main/java/ch/njol/skript/expressions/ExprDamagedItem.java
@@ -1,6 +1,9 @@
 package ch.njol.skript.expressions;
 
+import ch.njol.skript.aliases.ItemData;
+import ch.njol.skript.bukkitutil.ItemUtils;
 import org.bukkit.event.Event;
+import org.bukkit.inventory.meta.Damageable;
 import org.jetbrains.annotations.Nullable;
 
 import ch.njol.skript.Skript;
@@ -16,28 +19,37 @@ import ch.njol.skript.lang.SkriptParser;
 import ch.njol.util.Kleenean;
 
 @Name("Damaged Item")
-@Description("Directly damages an item. In MC versions 1.12.2 and lower, this can be used to apply data values to items/blocks")
+@Description({"Directly changes the durability of an item. In MC versions 1.12.2 and lower, this can be used to apply data values to items/blocks",
+	"Damage is used to remove the specified number from the base durability of an item. E.g 400 - damage",
+	"Durability is used to modify the total durability"})
+
 @Examples({"give player diamond sword with damage value 100", "set player's tool to diamond hoe damaged by 250",
 	"give player diamond sword with damage 700 named \"BROKEN SWORD\"",
 	"set {_item} to diamond hoe with damage value 50 named \"SAD HOE\"",
-	"set target block of player to wool with data value 1", "set target block of player to potato plant with data value 7"})
+	"set target block of player to wool with data value 1", "set target block of player to potato plant with data value 7",
+	"give player wooden sword with 1 durability named \"VERY BROKEN SWORD\"",
+	"set player's tool to diamond hoe with durability 500"})
 @Since("2.4")
 public class ExprDamagedItem extends PropertyExpression<ItemType, ItemType> {
 	
 	static {
 		Skript.registerExpression(ExprDamagedItem.class, ItemType.class, ExpressionType.COMBINED,
 				"%itemtype% with (damage|data) [value] %number%",
-				"%itemtype% damaged by %number%");
+				"%itemtype% damaged by %number%",
+				"%itemtype% with durability %number%",
+				"%itemtype% with %number% durability");
 	}
 	
 	@SuppressWarnings("null")
 	private Expression<Number> damage;
+	private int pattern;
 	
 	@SuppressWarnings({"unchecked", "null"})
 	@Override
 	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
 		setExpr((Expression<ItemType>) exprs[0]);
 		damage = (Expression<Number>) exprs[1];
+		pattern = matchedPattern;
 		return true;
 	}
 	
@@ -47,7 +59,11 @@ public class ExprDamagedItem extends PropertyExpression<ItemType, ItemType> {
 		if (damage == null)
 			return source;
 		return get(source.clone(), item -> {
-			item.iterator().forEachRemaining(i -> i.setDurability(damage.intValue()));
+			if (pattern > 1){
+				ItemUtils.setDamage(item, (ItemUtils.getMaxDamage(item) - damage.intValue()));
+			}else{
+				item.iterator().forEachRemaining(i -> i.setDurability(damage.intValue()));
+			}
 			return item;
 		});
 	}
@@ -59,7 +75,8 @@ public class ExprDamagedItem extends PropertyExpression<ItemType, ItemType> {
 	
 	@Override
 	public String toString(final @Nullable Event e, boolean debug) {
-		return getExpr().toString(e, debug) + " with damage value " + damage.toString(e, debug);
+		if (pattern > 1){
+			return getExpr().toString(e, debug) + " with durability " + damage.toString(e, debug);
+		}else return getExpr().toString(e, debug) + " with damage value " + damage.toString(e, debug);
 	}
-	
 }
