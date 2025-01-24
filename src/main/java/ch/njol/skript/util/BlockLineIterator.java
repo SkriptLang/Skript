c1b07364e435d261eb0936d17328b7cb3b5f675b
Pesek <jakub.zahomolkou@gmail.com>
2025-01-24 16:27:39 +0100

    fixed block duplicates, mimics the behaviour of previous block line iterator (using grid traversal algorithm instead of Bresenham's)

--- src/main/java/ch/njol/skript/util/BlockLineIterator.java
+++ src/main/java/ch/njol/skript/util/BlockLineIterator.java
@@ -42,8 +42,66 @@ public class BlockLineIterator implements Iterator<Block> {
 		if (!hasNext()) throw new NoSuchElementException("Reached the final block destination");
 		if (current.toCenterLocation().toVector().equals(end)) finished = true;
 		Block block = current.getBlock();
-		current.add(step);
+		// moves the current position just slightly from the edge to the next block
+		double eps = 1 + Math.ulp(1) * (Math.abs(current.getBlockX()) + Math.abs(current.getBlockZ()));
+		double t = calculateExact(current.toVector()) * eps;
+		current.add(step.clone().multiply(t));
 		return block;
+	}
+
+	private record Plane(double a, double b, double c, double d) {
+		static Plane create(Vector p1, Vector p2, Vector p3) {
+			Vector v1 = p2.clone().subtract(p1);
+			Vector v2 = p3.clone().subtract(p1);
+			Vector normal = v1.clone().crossProduct(v2);
+			double a = normal.getX();
+			double b = normal.getY();
+			double c = normal.getZ();
+			double d = -(a * p1.getX() + b * p1.getY() + c * p1.getZ());
+			return new Plane(a, b, c, d);
+		}
+	}
+
+	private double calculateExact(Vector from) {
+		double xSgn = Math.signum(step.getX());
+		double ySgn = Math.signum(step.getY());
+		double zSgn = Math.signum(step.getZ());
+		Vector center = center(from);
+		Vector xPlanePoint = center.clone().add(new Vector(xSgn > 0 ? 0.5 : -0.5, 0, 0));
+		Vector yPlanePoint = center.clone().add(new Vector(0, ySgn > 0 ? 0.5 : -0.5, 0));
+		Vector zPlanePoint = center.clone().add(new Vector(0, 0, zSgn > 0 ? 0.5 : -0.5));
+		double xDist = planeDist(from, Plane.create(
+			xPlanePoint,
+			xPlanePoint.clone().add(new Vector(0, 1, 0)),
+			xPlanePoint.clone().add(new Vector(0, 0, 1))
+		));
+		double yDist = planeDist(from, Plane.create(
+			yPlanePoint,
+			yPlanePoint.clone().add(new Vector(1, 0, 0)),
+			yPlanePoint.clone().add(new Vector(0, 0, 1))
+		));
+		double zDist = planeDist(from, Plane.create(
+			zPlanePoint,
+			zPlanePoint.clone().add(new Vector(1, 0, 0)),
+			zPlanePoint.clone().add(new Vector(0, 1, 0))
+		));
+		return Math.min(xDist, Math.min(yDist, zDist));
+	}
+
+	private double planeDist(Vector from, Plane plane) {
+		double v = 0 - plane.d - plane.a() * from.getX() - plane.b() * from.getY() - plane.c() * from.getZ();
+		double t = plane.a() * step.getX() + plane.b() * step.getY() + plane.c() * step.getZ();
+		double r = v / t;
+		if (!Double.isFinite(r)) return 2;
+		return r;
+	}
+
+	private static Vector center(Vector vector) {
+		Vector center = vector.clone();
+		center.setX(vector.getBlockX() + 0.5);
+		center.setY(vector.getBlockY() + 0.5);
+		center.setZ(vector.getBlockZ() + 0.5);
+		return center;
 	}
 
 }
