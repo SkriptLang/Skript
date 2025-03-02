4d61948d5237b670e7cd090d7c980342a894e3f0
Lexî Nuutraî <info@nuutrai.com>
2025-03-02 21:29:47 +0100

    Adds a little documentation to the annotations

--- src/main/java/ch/njol/skript/doc/AvailableEvent.java
+++ src/main/java/ch/njol/skript/doc/AvailableEvent.java
@@ -4,13 +4,22 @@ import org.bukkit.event.Event;
 
 import java.lang.annotation.*;
 
+// FIXME Adjust the link if necessary
+/**
+ * Provides a {@link org.bukkit.event.Event} that the syntax element can be used in.
+ * This annotation can be stacked multiple times
+ * <p>
+ * This annotation should only include a single event class, for multiple, use {@link ch.njol.skript.doc.AvailableEvents}
+ */
+
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Repeatable(AvailableEvent.AvailableEvents.class)
 @Documented
+@Events("WOw!")
 public @interface AvailableEvent {
 
-	public Class<? extends Event>[] value();
+	Class<? extends Event> value();
 
 
 	@Target(ElementType.TYPE)
--- src/main/java/ch/njol/skript/doc/AvailableEvents.java
+++ src/main/java/ch/njol/skript/doc/AvailableEvents.java
@@ -4,10 +4,16 @@ import org.bukkit.event.Event;
 
 import java.lang.annotation.*;
 
+/**
+ * Provides a list of {@link org.bukkit.event.Event} that the syntax element can be used in.
+ * <p>
+ * This annotation can only be used once per element, for stacking, see {@link AvailableEvent}
+ */
+
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@interface AvailableEvents {
+public @interface AvailableEvents {
 
 	Class<? extends Event>[] value();
 
