7939ade3d81718906f302d6d3a3850df33889000
TheMug <TheMug06@gmail.com>
2025-03-07 10:12:50 +0100

    Update FishingModule.java
    Registers PlayerFishEvent.State as EnumClassInfo

--- src/main/java/org/skriptlang/skript/bukkit/fishing/FishingModule.java
+++ src/main/java/org/skriptlang/skript/bukkit/fishing/FishingModule.java
@@ -1,6 +1,9 @@
 package org.skriptlang.skript.bukkit.fishing;
 
 import ch.njol.skript.Skript;
+import ch.njol.skript.classes.EnumClassInfo;
+import ch.njol.skript.registrations.Classes;
+import org.bukkit.event.player.PlayerFishEvent;
 
 import java.io.IOException;
 
@@ -8,6 +11,14 @@ public class FishingModule {
 
 	public static void load() throws IOException {
 		Skript.getAddonInstance().loadClasses("org.skriptlang.skript.bukkit.fishing", "elements");
-	}
 
+		// Register the Fishing State enum as a Skript type
+		Classes.registerClass(new EnumClassInfo<>(PlayerFishEvent.State.class, "fishingstate", "fishing states")
+			.user("fishing ?states?")
+			.name("Fishing State")
+			.description("Represents the different states of a fishing event.")
+			.since("2.10")
+		);
+
+	}
 }
