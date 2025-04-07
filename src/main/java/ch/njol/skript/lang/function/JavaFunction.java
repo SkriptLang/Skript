7bf3846ac76b23350316b11b14238084cebc13d0
Efnilite <35348263+Efnilite@users.noreply.github.com>
2025-04-07 17:26:10 +0200

    add way to add local javafunctions

--- src/main/java/ch/njol/skript/lang/function/JavaFunction.java
+++ src/main/java/ch/njol/skript/lang/function/JavaFunction.java
@@ -17,8 +17,16 @@ public abstract class JavaFunction<T> extends Function<T> {
 		this(name, parameters, returnType, single, null);
 	}
 
+	public JavaFunction(String name, Parameter<?>[] parameters, ClassInfo<T> returnType, boolean single, boolean local) {
+		this(name, parameters, returnType, single, local, null);
+	}
+
 	public JavaFunction(String name, Parameter<?>[] parameters, ClassInfo<T> returnType, boolean single, @Nullable Contract contract) {
-		this(new Signature<>("none", name, parameters, false, returnType, single, Thread.currentThread().getStackTrace()[3].getClassName(), contract));
+		this(name, parameters, returnType, single, false, contract);
+	}
+
+	public JavaFunction(String name, Parameter<?>[] parameters, ClassInfo<T> returnType, boolean single, boolean local, @Nullable Contract contract) {
+		this(new Signature<>("none", name, parameters, local, returnType, single, Thread.currentThread().getStackTrace()[3].getClassName(), contract));
 	}
 	
 	@Override
