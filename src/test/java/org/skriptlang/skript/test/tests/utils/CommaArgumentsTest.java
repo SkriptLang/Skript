c49de2918ca32b0b617d8a8c102931bc7b13b16c
SirSmurfy2 <82696841+TheAbsolutionism@users.noreply.github.com>
2025-04-12 11:27:36 -0400

    Update CommaArgumentsTest.java

--- src/test/java/org/skriptlang/skript/test/tests/utils/CommaArgumentsTest.java
+++ src/test/java/org/skriptlang/skript/test/tests/utils/CommaArgumentsTest.java
@@ -1,20 +1,15 @@
 package org.skriptlang.skript.test.tests.utils;
 
-import ch.njol.skript.Skript;
 import ch.njol.skript.test.runner.SkriptJUnitTest;
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.List;
 
 import static ch.njol.skript.SkriptCommand.separateCommaArguments;
 
 public class CommaArgumentsTest extends SkriptJUnitTest {
 
 	private String[] getArgs(boolean reverse, boolean excludeEmptyArgs, String ... strings) {
-		List<String> result = separateCommaArguments(reverse, excludeEmptyArgs, strings);
-		Skript.adminBroadcast(result.toString());
-		return result.toArray(String[]::new);
+		return separateCommaArguments(reverse, excludeEmptyArgs, strings).toArray(String[]::new);
 	}
 
 	@Test
