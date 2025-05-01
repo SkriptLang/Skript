1169fede4c9ccd373d5fe838e8bb846c5187364b
SirSmurfy2 <absolutionism1@gmail.com>
2025-05-01 17:34:49 -0400

    Update PaperUtils.java

--- src/main/java/ch/njol/skript/util/PaperUtils.java
+++ src/main/java/ch/njol/skript/util/PaperUtils.java
@@ -12,12 +12,22 @@ public class PaperUtils {
 	private static final boolean REGISTRY_ACCESS_EXISTS = Skript.classExists("io.papermc.paper.registry.RegistryAccess");
 	private static final boolean REGISTRY_KEY_EXISTS = Skript.classExists("io.papermc.paper.registry.RegistryKey");
 
+	/**
+	 * Check if a registry exists within {@link RegistryKey}
+	 * @param registry Registry to check for (Fully qualified name of registry)
+	 * @return True if registry exists else false
+	 */
 	public static boolean registryExists(String registry) {
 		return REGISTRY_ACCESS_EXISTS
 			&& REGISTRY_KEY_EXISTS
 			&& Skript.fieldExists(RegistryKey.class, registry);
 	}
 
+	/**
+	 * Gets the Bukkit {@link Registry} from Paper's {@link RegistryKey}.
+	 * @param registry Registry to get (Fully qualified name of registry).
+	 * @return The Bukkit {@link Registry} if registry exists else {@code null}.
+	 */
 	public static <T extends Keyed> @Nullable Registry<T> getBukkitRegistry(String registry) {
 		if (!registryExists(registry))
 			return null;
