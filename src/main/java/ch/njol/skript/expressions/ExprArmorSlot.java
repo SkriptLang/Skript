334daa1044ae30b582ca42f182f019d1514f5ee7
SirSmurfy2 <absolutionism1@gmail.com>
2025-05-31 00:08:40 -0400

    Initial Commit

--- src/main/java/ch/njol/skript/expressions/ExprArmorSlot.java
+++ src/main/java/ch/njol/skript/expressions/ExprArmorSlot.java
@@ -1,11 +1,13 @@
 package ch.njol.skript.expressions;
 
+import ch.njol.skript.Skript;
 import ch.njol.skript.doc.*;
 import ch.njol.skript.expressions.base.PropertyExpression;
 import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.Literal;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.SyntaxStringBuilder;
 import ch.njol.skript.util.slot.EquipmentSlot;
-import ch.njol.skript.util.slot.EquipmentSlot.EquipSlot;
 import ch.njol.skript.util.slot.Slot;
 import ch.njol.util.Kleenean;
 import org.bukkit.Material;
@@ -14,10 +16,7 @@ import org.bukkit.event.Event;
 import org.bukkit.inventory.EntityEquipment;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Locale;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Stream;
 
 @Name("Armor Slot")
@@ -28,75 +27,110 @@ import java.util.stream.Stream;
 	"<li>Horses: Horse armour (doesn't work on zombie or skeleton horses)</li>",
 	"<li>Wolves: Wolf Armor</li>",
 	"<li>Llamas (regular or trader): Carpet</li>",
-	"</ul>"
+	"</ul>",
+	"Saddle is a special slot that can only be used for: pigs, striders and horse types (horse, camel, llama, mule, donkey)."
 })
 @Examples({
 	"set chestplate of the player to a diamond chestplate",
 	"helmet of player is neither tag values of tag \"paper:helmets\" nor air # player is wearing a block, e.g. from another plugin"
 })
 @Keywords("armor")
-@Since("1.0, 2.8.0 (armor), 2.10 (body armor)")
+@Since("1.0, 2.8.0 (armor), 2.10 (body armor), INSERT VERSION (saddle)")
 public class ExprArmorSlot extends PropertyExpression<LivingEntity, Slot> {
 
-	private static final Set<Class<?>> bodyEntities = new HashSet<>(Arrays.asList(Horse.class, Llama.class, TraderLlama.class));
+	private static final Set<Class<? extends Entity>> bodyEntities =
+		new HashSet<>(Arrays.asList(Horse.class, Llama.class, TraderLlama.class));
+	private static final Set<Class<? extends Entity>> saddleEntities =
+		new HashSet<>(Arrays.asList(Pig.class, Strider.class, AbstractHorse.class));
+
+	private static final boolean HAS_BODY_SLOT =
+		Skript.fieldExists(org.bukkit.inventory.EquipmentSlot.class, "BODY");
+	private static final boolean HAS_SADDLE_SLOT =
+		Skript.fieldExists(org.bukkit.inventory.EquipmentSlot.class, "SADDLE");
 
 	static {
 		if (Material.getMaterial("WOLF_ARMOR") != null)
 			bodyEntities.add(Wolf.class);
 
-		register(ExprArmorSlot.class, Slot.class, "((boots:(boots|shoes)|leggings:leg[ging]s|chestplate:chestplate[s]|helmet:helmet[s]) [(item|:slot)]|armour:armo[u]r[s]|bodyarmor:body armo[u]r)", "livingentities");
+		register(ExprArmorSlot.class, Slot.class, "(%-*equipmentslots%|[the] armo[u]r)[s] [item:item[s]]", "livingentities");
 	}
 
-	private @Nullable EquipSlot slot;
+	private @Nullable Literal<org.bukkit.inventory.EquipmentSlot> slots;
 	private boolean explicitSlot;
-	private boolean isArmor;
-	private boolean isBody;
 
 	@Override
-	@SuppressWarnings("unchecked")
 	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
-		isBody = parseResult.hasTag("bodyarmor");
-		isArmor = parseResult.hasTag("armour");
-		slot = (isArmor || isBody) ? null : EquipSlot.valueOf(parseResult.tags.get(0).toUpperCase(Locale.ENGLISH));
-		explicitSlot = parseResult.hasTag("slot"); // User explicitly asked for SLOT, not item
-		setExpr((Expression<? extends LivingEntity>) exprs[0]);
+		Expression<?> slots = exprs[0];
+		Expression<?> expr = exprs[1];
+		if (matchedPattern == 1) {
+			slots = exprs[1];
+			expr = exprs[0];
+		}
+		if (slots != null)
+			//noinspection unchecked
+			this.slots = (Literal<org.bukkit.inventory.EquipmentSlot>) slots;
+		explicitSlot = !parseResult.hasTag("item");
+		//noinspection unchecked
+		setExpr((Expression<? extends LivingEntity>) expr);
 		return true;
 	}
 
 	@Override
-	protected Slot[] get(Event event, LivingEntity[] source) {
-		if (slot == null) { // All Armour
+	protected Slot @Nullable [] get(Event event, LivingEntity[] source) {
+		if (slots == null) {
 			return Arrays.stream(source)
-					.map(LivingEntity::getEquipment)
-					.flatMap(equipment -> {
-						if (equipment == null)
-							return null;
-						if (isBody) {
-							if (!bodyEntities.contains(equipment.getHolder().getType().getEntityClass()))
-								return null;
-							return Stream.of(new EquipmentSlot(equipment, org.bukkit.inventory.EquipmentSlot.BODY, explicitSlot));
-						}
-						return Stream.of(
-								new EquipmentSlot(equipment, org.bukkit.inventory.EquipmentSlot.HEAD, explicitSlot),
-								new EquipmentSlot(equipment, org.bukkit.inventory.EquipmentSlot.CHEST, explicitSlot),
-								new EquipmentSlot(equipment, org.bukkit.inventory.EquipmentSlot.LEGS, explicitSlot),
-								new EquipmentSlot(equipment, org.bukkit.inventory.EquipmentSlot.FEET, explicitSlot)
-						);
-					})
-					.toArray(Slot[]::new);
+				.map(LivingEntity::getEquipment)
+				.flatMap(equipment -> {
+					if (equipment == null)
+						return null;
+					// Entities with 'body armor' don't and shouldn't have other equipment slots (for now)
+					if (HAS_BODY_SLOT && canUseEquipmentSlot(equipment.getHolder(), org.bukkit.inventory.EquipmentSlot.BODY))
+						return Stream.of(new EquipmentSlot(equipment, org.bukkit.inventory.EquipmentSlot.BODY, explicitSlot));
+					return Stream.of(
+						new EquipmentSlot(equipment, org.bukkit.inventory.EquipmentSlot.HEAD, explicitSlot),
+						new EquipmentSlot(equipment, org.bukkit.inventory.EquipmentSlot.CHEST, explicitSlot),
+						new EquipmentSlot(equipment, org.bukkit.inventory.EquipmentSlot.LEGS, explicitSlot),
+						new EquipmentSlot(equipment, org.bukkit.inventory.EquipmentSlot.FEET, explicitSlot)
+					);
+				}).toArray(Slot[]::new);
 		}
-
-		return get(source, entity -> {
+		org.bukkit.inventory.EquipmentSlot[] equipmentSlots = this.slots.getArray(event);
+		if (equipmentSlots == null || equipmentSlots.length == 0)
+			return null;
+		List<EquipmentSlot> slots = new ArrayList<>();
+		for (LivingEntity entity : source) {
 			EntityEquipment equipment = entity.getEquipment();
 			if (equipment == null)
-				return null;
-			return new EquipmentSlot(equipment, slot, explicitSlot);
-		});
+				continue;
+			for (org.bukkit.inventory.EquipmentSlot equipmentSlot : equipmentSlots) {
+				if (!canUseEquipmentSlot(entity, equipmentSlot))
+					continue;
+				slots.add(new EquipmentSlot(equipment, equipmentSlot, explicitSlot));
+			}
+		}
+		return slots.toArray(EquipmentSlot[]::new);
+	}
+
+	private boolean canUseEquipmentSlot(Entity entity, org.bukkit.inventory.EquipmentSlot equipmentSlot) {
+		Class<? extends Entity> entityClass = entity.getType().getEntityClass();
+		String slotName = equipmentSlot.name();
+		if (HAS_BODY_SLOT && slotName.equalsIgnoreCase("body")) {
+			return bodyEntities.contains(entityClass);
+		} else if (HAS_SADDLE_SLOT && slotName.equalsIgnoreCase("saddle")) {
+			if (saddleEntities.contains(entityClass))
+				return true;
+			for (Class<? extends Entity> type : saddleEntities) {
+				if ((entityClass != null && type.isAssignableFrom(entityClass)) || type.isInstance(entity))
+					return true;
+			}
+			return false;
+		}
+		return true;
 	}
 
 	@Override
 	public boolean isSingle() {
-		return isBody || (!isArmor && super.isSingle());
+		return slots != null && slots.isSingle();
 	}
 
 	@Override
@@ -106,7 +140,17 @@ public class ExprArmorSlot extends PropertyExpression<LivingEntity, Slot> {
 
 	@Override
 	public String toString(@Nullable Event event, boolean debug) {
-		return slot == null ? "armour" : slot.name().toLowerCase(Locale.ENGLISH) + " of " + getExpr().toString(event, debug);
+		SyntaxStringBuilder builder = new SyntaxStringBuilder(event, debug);
+		builder.append("the");
+		if (slots != null) {
+			builder.append(slots);
+		} else {
+			builder.append("armour");
+		}
+		if (!explicitSlot)
+			builder.append("items");
+		builder.append("of", getExpr());
+		return builder.toString();
 	}
 
 }
