539a0a0396fae0976d07063e603f95c76f71495a
SirSmurfy2 <absolutionism1@gmail.com>
2025-05-31 22:15:41 -0400

    Initial Commit

---
+++ src/main/java/ch/njol/skript/conditions/CondCanCopy.java
@@ -0,0 +1,36 @@
+package ch.njol.skript.conditions;
+
+import ch.njol.skript.classes.ClassInfo;
+import ch.njol.skript.conditions.base.PropertyCondition;
+import ch.njol.skript.doc.*;
+import ch.njol.skript.registrations.Classes;
+
+@Name("Object Can Be Copied")
+@Description("Whether an object can be copied.")
+@Example("if a diamond can be copied: # True")
+@Example("if the last spawned entity can be copied: # False")
+@Since("INSERT VERSION")
+@Keywords({"copy"})
+public class CondCanCopy extends PropertyCondition<Object> {
+
+	static {
+		register(CondCanCopy.class, PropertyType.CAN, "be copied", "objects");
+	}
+
+	@Override
+	public boolean check(Object object) {
+		ClassInfo<?> classInfo = Classes.getSuperClassInfo(object.getClass());
+		return classInfo.getCloner() != null;
+	}
+
+	@Override
+	protected PropertyType getPropertyType() {
+		return PropertyType.CAN;
+	}
+
+	@Override
+	protected String getPropertyName() {
+		return "be copied";
+	}
+
+}
---
+++ src/main/java/ch/njol/skript/expressions/ExprCopy.java
@@ -0,0 +1,93 @@
+package ch.njol.skript.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.classes.ClassInfo;
+import ch.njol.skript.doc.*;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.SyntaxStringBuilder;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.skript.registrations.Classes;
+import ch.njol.skript.util.LiteralUtils;
+import ch.njol.util.Kleenean;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Name("Copy of Object")
+@Description({
+	"Get a copy of an object. Please note that not all objects are able to be copied.",
+	"If an object is not able to be copied, the object will be excluded from the result.",
+	"However, by including 'with fallback', you will get the original object, if it can not be copied."
+})
+@Example("set {_copy} to a copy of player's tool")
+@Example("""
+	set {_objects::*} to a diamond, chest[], and location(0, 0, 0)
+	set {_copies::*} to the copies of {_objects::*}
+	""")
+@Example("""
+	# This will return nothing because an entity can not be copied
+	set {_copy} to the copy of last spawned entity
+	
+	# This will return the original object
+	set {_copy} to the copy of last spawned entity with fallback
+	""")
+@Since("INSERT VERSION")
+@Keywords({"copy"})
+public class ExprCopy extends SimpleExpression<Object> {
+
+	static {
+		Skript.registerExpression(ExprCopy.class, Object.class, ExpressionType.SIMPLE,
+			"[the|a] copy of %objects% [with:with fallback]",
+			"[the] copies of %objects% [with:with fallback]",
+			"[the] copied %objects% [with:with fallback]");
+	}
+
+	private Expression<?> objects;
+	private boolean withFallback;
+
+	@Override
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+		objects = LiteralUtils.defendExpression(exprs[0]);
+		withFallback = parseResult.hasTag("with");
+		return LiteralUtils.canInitSafely(objects);
+	}
+
+	@Override
+	protected Object @Nullable [] get(Event event) {
+		List<Object> copies = new ArrayList<>();
+		for (Object object : objects.getArray(event)) {
+			ClassInfo classInfo = Classes.getSuperClassInfo(object.getClass());
+			if (classInfo.getCloner() != null) {
+				//noinspection unchecked
+				copies.add(classInfo.clone(object));
+			} else if (withFallback) {
+				copies.add(object);
+			}
+		}
+		return copies.toArray();
+	}
+
+	@Override
+	public boolean isSingle() {
+		return objects.isSingle();
+	}
+
+	@Override
+	public Class<?> getReturnType() {
+		return Object.class;
+	}
+
+	@Override
+	public String toString(@Nullable Event event, boolean debug) {
+		SyntaxStringBuilder builder = new SyntaxStringBuilder(event, debug);
+		builder.append("the copies of", objects);
+		if (withFallback)
+			builder.append("with fallback");
+		return builder.toString();
+	}
+
+}
