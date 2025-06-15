f46ef74a0472dfc5ce662b1eb87bbb0b6adf7d0a
SirSmurfy2 <absolutionism1@gmail.com>
2025-06-15 16:08:32 -0400

    Sovdes Fixes

--- src/main/java/ch/njol/skript/entity/LlamaData.java
+++ src/main/java/ch/njol/skript/entity/LlamaData.java
@@ -8,58 +8,72 @@ import org.bukkit.entity.Llama.Color;
 import org.bukkit.entity.TraderLlama;
 import org.jetbrains.annotations.Nullable;
 
-public class LlamaData<E extends Llama> extends EntityData<E> {
+public class LlamaData extends EntityData<Llama> {
+
+	private static final Color[] LLAMA_COLORS = Color.values();
 
 	static {
-		//noinspection unchecked
 		EntityData.register(LlamaData.class, "llama", Llama.class, 0,
-			"llama", "creamy llama",
-			"white llama", "brown llama", "gray llama");
+			"llama", "creamy llama", "white llama", "brown llama", "gray llama",
+			"trader llama", "creamy trader llama", "white trader llama", "brown trader llama", "gray trader llama");
 	}
 
 	private @Nullable Color color = null;
+	private boolean isTrader;
 	
 	public LlamaData() {}
 	
-	public LlamaData(@Nullable Color color) {
+	public LlamaData(@Nullable Color color, boolean isTrader) {
 		this.color = color;
+		this.isTrader = isTrader;
+		super.matchedPattern = (color != null ? (color.ordinal() + 1) : 0) + (isTrader ? 5 : 0);
 	}
 	
 	@Override
 	protected boolean init(Literal<?>[] exprs, int matchedPattern, ParseResult parseResult) {
-		if (matchedPattern > 0 && matchedPattern < 5)
-			color = Color.values()[matchedPattern - 1];
+		isTrader = matchedPattern > 4;
+		if (matchedPattern > 5) {
+			color = LLAMA_COLORS[matchedPattern - 6];
+		} else if (matchedPattern > 0 && matchedPattern < 5) {
+			color = LLAMA_COLORS[matchedPattern - 1];
+		}
+		super.matchedPattern = (color != null ? (color.ordinal() + 1) : 0) + (isTrader ? 5 : 0);
 		return true;
 	}
 	
 	@Override
-	protected boolean init(@Nullable Class<? extends E> entityClass, @Nullable E llama) {
-		if (llama != null)
+	protected boolean init(@Nullable Class<? extends Llama> entityClass, @Nullable Llama llama) {
+		if (entityClass != null)
+			isTrader = TraderLlama.class.isAssignableFrom(entityClass);
+		if (llama != null) {
 			color = llama.getColor();
+			isTrader = llama instanceof TraderLlama;
+		}
 		return true;
 	}
 	
 	@Override
-	public void set(E entity) {
-		Color randomColor = color == null ? CollectionUtils.getRandom(Color.values()) : color;
+	public void set(Llama entity) {
+		Color randomColor = color == null ? CollectionUtils.getRandom(LLAMA_COLORS) : color;
 		assert randomColor != null;
 		entity.setColor(randomColor);
 	}
 	
 	@Override
-	protected boolean match(E entity) {
+	protected boolean match(Llama entity) {
+		if (isTrader && !(entity instanceof TraderLlama))
+			return false;
 		return color == null || color == entity.getColor();
 	}
 	
 	@Override
-	public Class<E> getType() {
-		//noinspection unchecked
-		return (Class<E>) Llama.class;
+	public Class<? extends Llama> getType() {
+		return isTrader ? TraderLlama.class : Llama.class;
 	}
 	
 	@Override
 	public EntityData getSuperType() {
-		return new LlamaData(color);
+		return new LlamaData(color, isTrader);
 	}
 	
 	@Override
@@ -67,6 +81,7 @@ public class LlamaData<E extends Llama> extends EntityData<E> {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + (color != null ? color.hashCode() : 0);
+		result = prime * result + (isTrader ? 1 : 0);
 		return result;
 	}
 	
@@ -74,63 +89,17 @@ public class LlamaData<E extends Llama> extends EntityData<E> {
 	protected boolean equals_i(EntityData<?> data) {
 		if (!(data instanceof LlamaData other))
 			return false;
-		return other.color == color;
+		return isTrader == other.isTrader && other.color == color;
 	}
 	
 	@Override
 	public boolean isSupertypeOf(EntityData<?> data) {
 		if (!(data instanceof LlamaData other))
 			return false;
-		return color == null || other.color == color;
-	}
 
-	public @Nullable Color getColor() {
-		return color;
-	}
-
-	public static class TraderLlamaData extends LlamaData<TraderLlama> {
-
-		static {
-			EntityData.register(TraderLlamaData.class, "trader llama", TraderLlama.class, 0,
-				"trader llama", "creamy trader llama", "white trader llama", "brown trader llama", "gray trader llama");
-		}
-
-		public TraderLlamaData() {
-			super();
-		}
-
-		public TraderLlamaData(@Nullable Color color) {
-			super(color);
-		}
-
-		@Override
-		public Class<TraderLlama> getType() {
-			return TraderLlama.class;
-		}
-
-		@Override
-		public EntityData getSuperType() {
-			return new TraderLlamaData();
-		}
-
-		@Override
-		protected int hashCode_i() {
-			return super.hashCode_i() + 1;
-		}
-
-		@Override
-		protected boolean equals_i(EntityData<?> data) {
-			if (!(data instanceof TraderLlamaData other))
-				return false;
-			return super.equals_i(data);
-		}
-
-		@Override
-		public boolean isSupertypeOf(EntityData<?> data) {
-			if (!(data instanceof TraderLlamaData other))
-				return false;
-			return super.isSupertypeOf(data);
-		}
+		if (isTrader && !other.isTrader)
+			return false;
+		return color == null || color == other.color;
 	}
 	
 }
