888727a6c1176e315f9c28034a0a227f7ef764a4
SirSmurfy2 <absolutionism1@gmail.com>
2025-06-29 20:50:21 -0400

    Update EquippableModule.java

--- src/main/java/org/skriptlang/skript/bukkit/itemcomponents/equippable/EquippableModule.java
+++ src/main/java/org/skriptlang/skript/bukkit/itemcomponents/equippable/EquippableModule.java
@@ -3,7 +3,9 @@ package org.skriptlang.skript.bukkit.itemcomponents.equippable;
 import ch.njol.skript.Skript;
 import ch.njol.skript.aliases.ItemType;
 import ch.njol.skript.classes.ClassInfo;
+import ch.njol.skript.classes.Parser;
 import ch.njol.skript.expressions.base.EventValueExpression;
+import ch.njol.skript.lang.ParseContext;
 import ch.njol.skript.registrations.Classes;
 import ch.njol.skript.util.ItemSource;
 import ch.njol.skript.util.slot.Slot;
@@ -32,6 +34,22 @@ public class EquippableModule implements AddonModule {
 			.requiredPlugins("Minecraft 1.21.2+")
 			.since("INSERT VERSION")
 			.defaultExpression(new EventValueExpression<>(EquippableWrapper.class))
+			.parser(new Parser<>() {
+				@Override
+				public boolean canParse(ParseContext context) {
+					return false;
+				}
+
+				@Override
+				public String toString(EquippableWrapper wrapper, int flags) {
+					return "equippable component";
+				}
+
+				@Override
+				public String toVariableNameString(EquippableWrapper wrapper) {
+					return "equippable component#" + wrapper.hashCode();
+				}
+			})
 		);
 
 		Converters.registerConverter(EquippableComponent.class, EquippableWrapper.class, EquippableWrapper::new, Converter.NO_RIGHT_CHAINING);
