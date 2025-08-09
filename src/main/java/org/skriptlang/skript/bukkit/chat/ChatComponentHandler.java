f03b868247bb2a56129b165c528532b3be4138a6
Patrick Miller <apickledwalrus@icloud.com>
2025-08-09 01:54:58 -0400

    Finish implementing legacy tags

--- src/main/java/org/skriptlang/skript/bukkit/chat/ChatComponentHandler.java
+++ src/main/java/org/skriptlang/skript/bukkit/chat/ChatComponentHandler.java
@@ -21,16 +21,20 @@ import java.util.Map;
 
 public final class ChatComponentHandler {
 
-	private static final Map<String, Tag> SIMPLE_PLACEHOLDERS = new HashMap<>();
-	private static final List<TagResolver> RESOLVERS = new ArrayList<>();
+	private record SkriptTag(Tag tag, boolean safe) { }
+	private record SkriptTagResolver(TagResolver resolver, boolean safe) { }
+
+	private static final Map<String, SkriptTag> SIMPLE_PLACEHOLDERS = new HashMap<>();
+	private static final List<SkriptTagResolver> RESOLVERS = new ArrayList<>();
 
 	/**
 	 * Registers a simple key-value placeholder with Skript's message parsers.
 	 * @param name The name/key of the placeholder.
 	 * @param result The result/value of the placeholder.
+	 * @param safe Whether the placeholder can be used in the safe parser.
 	 */
-	public static void registerPlaceholder(String name, Tag result) {
-		SIMPLE_PLACEHOLDERS.put(name, result);
+	public static void registerPlaceholder(String name, Tag result, boolean safe) {
+		SIMPLE_PLACEHOLDERS.put(name, new SkriptTag(result, safe));
 	}
 
 	/**
@@ -44,9 +48,11 @@ public final class ChatComponentHandler {
 	/**
 	 * Registers a TagResolver with Skript's message parsers.
 	 * @param resolver The TagResolver to register.
+	 * @param safe Whether the placeholder can be used in the safe parser.
 	 */
-	public static void registerResolver(TagResolver resolver) {
-		RESOLVERS.add(resolver);
+	public static void registerResolver(TagResolver resolver, boolean safe) {
+		unregisterResolver(resolver); // just to be safe
+		RESOLVERS.add(new SkriptTagResolver(resolver, safe));
 	}
 
 	/**
@@ -54,58 +60,65 @@ public final class ChatComponentHandler {
 	 * @param resolver The TagResolver to unregister.
 	 */
 	public static void unregisterResolver(TagResolver resolver) {
-		RESOLVERS.remove(resolver);
+		RESOLVERS.remove(new SkriptTagResolver(resolver, false));
+		RESOLVERS.remove(new SkriptTagResolver(resolver, true));
 	}
 
-	private static final TagResolver SKRIPT_TAG_RESOLVER = new TagResolver() {
-		@Override
-		public @Nullable Tag resolve(@NotNull String name, @NotNull ArgumentQueue arguments, @NotNull Context ctx) throws ParsingException {
-			Tag simple = SIMPLE_PLACEHOLDERS.get(name);
-			if (simple != null)
-				return simple;
-			for (TagResolver resolver : RESOLVERS) {
-				Tag resolved = resolver.resolve(name, arguments, ctx);
-				if (resolved != null)
-					return resolved;
+	private static TagResolver createSkriptTagResolver(boolean safe) {
+		return new TagResolver() {
+			@Override
+			public @Nullable Tag resolve(@NotNull String name, @NotNull ArgumentQueue arguments, @NotNull Context ctx) throws ParsingException {
+				SkriptTag simple = SIMPLE_PLACEHOLDERS.get(name);
+				if (simple != null) {
+					return !safe || simple.safe ? simple.tag : null;
+				}
+				for (SkriptTagResolver skriptResolver : RESOLVERS) {
+					if (safe && !skriptResolver.safe) {
+						continue;
+					}
+					Tag resolved = skriptResolver.resolver.resolve(name, arguments, ctx);
+					if (resolved != null) {
+						return resolved;
+					}
+				}
+				return null;
 			}
-			return null;
-		}
 
-		@Override
-		public boolean has(@NotNull String name) {
-			if (SIMPLE_PLACEHOLDERS.containsKey(name))
-				return true;
-			for (TagResolver resolver : RESOLVERS) {
-				if (resolver.has(name))
-					return true;
+			@Override
+			public boolean has(@NotNull String name) {
+				SkriptTag simple = SIMPLE_PLACEHOLDERS.get(name);
+				if (simple != null) {
+					return !safe || simple.safe;
+				}
+				for (SkriptTagResolver skriptResolver : RESOLVERS) {
+					if ((!safe || skriptResolver.safe) && skriptResolver.resolver.has(name)) {
+						return true;
+					}
+				}
+				return false;
 			}
-			return false;
-		}
-	};
+		};
+	}
 
 	// The normal parser will process any proper tags
 	private static final MiniMessage parser = MiniMessage.builder()
 		.strict(false)
 		.tags(TagResolver.builder()
 			.resolver(StandardTags.defaults())
-			.resolver(SKRIPT_TAG_RESOLVER)
-			.build()
-		)
+			.resolver(createSkriptTagResolver(false))
+			.build())
 		.build();
 
 	// The safe parser only parses color/decoration/formatting related tags
 	private static final MiniMessage safeParser = MiniMessage.builder()
 		.strict(false)
 		.tags(TagResolver.builder()
-			.resolvers(
-				StandardTags.color(), StandardTags.decorations(), StandardTags.font(),
+			.resolvers(StandardTags.color(), StandardTags.decorations(), StandardTags.font(),
 				StandardTags.gradient(), StandardTags.rainbow(), StandardTags.newline(),
 				StandardTags.reset(), StandardTags.transition(), StandardTags.pride(),
-				StandardTags.shadowColor()
-			)
-			.resolver(SKRIPT_TAG_RESOLVER)
-			.build()
-		)
+				StandardTags.shadowColor())
+			.resolver(createSkriptTagResolver(true))
+			.build())
 		.build();
 
 	/**
@@ -139,14 +152,16 @@ public final class ChatComponentHandler {
 			int length = messageChars.length;
 			for (int i = 0; i < length; i++) {
 				char current = messageChars[i];
-				if (current == '§')
+				if (current == '§') {
 					current = '&';
+				}
 				char next = (i + 1 != length) ? messageChars[i + 1] : ' ';
 				boolean isCode = current == '&';
 				if (isCode && next == 'x') { // Try to parse as hex -> &x&1&2&3&4&5&6
 					reconstructedMessage.append("<#");
-					for (int i2 = i + 3; i2 < i + 14; i2 += 2) // Isolate the specific numbers
+					for (int i2 = i + 3; i2 < i + 14; i2 += 2) { // Isolate the specific numbers
 						reconstructedMessage.append(messageChars[i2]);
+					}
 					reconstructedMessage.append('>');
 					i += 13; // Skip to the end
 				} else if (isCode) {
--- src/main/java/org/skriptlang/skript/bukkit/chat/ChatModule.java
+++ src/main/java/org/skriptlang/skript/bukkit/chat/ChatModule.java
@@ -5,19 +5,22 @@ import ch.njol.skript.classes.Parser;
 import ch.njol.skript.lang.ParseContext;
 import ch.njol.skript.registrations.Classes;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.minimessage.tag.ParserDirective;
 import net.kyori.adventure.text.minimessage.tag.Tag;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
+import net.kyori.adventure.text.minimessage.tag.standard.StandardTags;
 import org.skriptlang.skript.addon.AddonModule;
 import org.skriptlang.skript.addon.SkriptAddon;
-import org.skriptlang.skript.bukkit.chat.elements.EffActionBar;
-import org.skriptlang.skript.bukkit.chat.elements.EffBroadcast;
-import org.skriptlang.skript.bukkit.chat.elements.EffMessage;
+import org.skriptlang.skript.bukkit.chat.elements.*;
 import org.skriptlang.skript.lang.converter.Converters;
 import org.skriptlang.skript.registration.SyntaxRegistry;
 
 import java.util.Locale;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -54,41 +57,94 @@ public class ChatModule implements AddonModule {
 	@Override
 	public void load(SkriptAddon addon) {
 		// register Skript's legacy color tags for compatibility
-		ChatComponentHandler.registerPlaceholder("dark_cyan", Tag.styling(NamedTextColor.DARK_AQUA));
-		ChatComponentHandler.registerPlaceholder("dark_turquoise", Tag.styling(NamedTextColor.DARK_AQUA));
-		ChatComponentHandler.registerPlaceholder("cyan", Tag.styling(NamedTextColor.DARK_AQUA));
+		ChatComponentHandler.registerPlaceholder("dark_cyan", Tag.styling(NamedTextColor.DARK_AQUA), true);
+		ChatComponentHandler.registerPlaceholder("dark_turquoise", Tag.styling(NamedTextColor.DARK_AQUA), true);
+		ChatComponentHandler.registerPlaceholder("cyan", Tag.styling(NamedTextColor.DARK_AQUA), true);
 
-		ChatComponentHandler.registerPlaceholder("purple", Tag.styling(NamedTextColor.DARK_PURPLE));
+		ChatComponentHandler.registerPlaceholder("purple", Tag.styling(NamedTextColor.DARK_PURPLE), true);
 
-		ChatComponentHandler.registerPlaceholder("dark_yellow", Tag.styling(NamedTextColor.GOLD));
-		ChatComponentHandler.registerPlaceholder("orange", Tag.styling(NamedTextColor.GOLD));
+		ChatComponentHandler.registerPlaceholder("dark_yellow", Tag.styling(NamedTextColor.GOLD), true);
+		ChatComponentHandler.registerPlaceholder("orange", Tag.styling(NamedTextColor.GOLD), true);
 
-		ChatComponentHandler.registerPlaceholder("light_grey", Tag.styling(NamedTextColor.GRAY));
-		ChatComponentHandler.registerPlaceholder("light_gray", Tag.styling(NamedTextColor.GRAY));
-		ChatComponentHandler.registerPlaceholder("silver", Tag.styling(NamedTextColor.GRAY));
+		ChatComponentHandler.registerPlaceholder("light_grey", Tag.styling(NamedTextColor.GRAY), true);
+		ChatComponentHandler.registerPlaceholder("light_gray", Tag.styling(NamedTextColor.GRAY), true);
+		ChatComponentHandler.registerPlaceholder("silver", Tag.styling(NamedTextColor.GRAY), true);
 
-		ChatComponentHandler.registerPlaceholder("dark_silver", Tag.styling(NamedTextColor.DARK_GRAY));
+		ChatComponentHandler.registerPlaceholder("dark_silver", Tag.styling(NamedTextColor.DARK_GRAY), true);
 
-		ChatComponentHandler.registerPlaceholder("light_blue", Tag.styling(NamedTextColor.BLUE));
-		ChatComponentHandler.registerPlaceholder("indigo", Tag.styling(NamedTextColor.BLUE));
+		ChatComponentHandler.registerPlaceholder("light_blue", Tag.styling(NamedTextColor.BLUE), true);
+		ChatComponentHandler.registerPlaceholder("indigo", Tag.styling(NamedTextColor.BLUE), true);
 
-		ChatComponentHandler.registerPlaceholder("light_green", Tag.styling(NamedTextColor.GREEN));
-		ChatComponentHandler.registerPlaceholder("lime_green", Tag.styling(NamedTextColor.GREEN));
-		ChatComponentHandler.registerPlaceholder("lime", Tag.styling(NamedTextColor.GREEN));
+		ChatComponentHandler.registerPlaceholder("light_green", Tag.styling(NamedTextColor.GREEN), true);
+		ChatComponentHandler.registerPlaceholder("lime_green", Tag.styling(NamedTextColor.GREEN), true);
+		ChatComponentHandler.registerPlaceholder("lime", Tag.styling(NamedTextColor.GREEN), true);
 
-		ChatComponentHandler.registerPlaceholder("light_cyan", Tag.styling(NamedTextColor.AQUA));
-		ChatComponentHandler.registerPlaceholder("light_aqua", Tag.styling(NamedTextColor.AQUA));
-		ChatComponentHandler.registerPlaceholder("turquoise", Tag.styling(NamedTextColor.AQUA));
+		ChatComponentHandler.registerPlaceholder("light_cyan", Tag.styling(NamedTextColor.AQUA), true);
+		ChatComponentHandler.registerPlaceholder("light_aqua", Tag.styling(NamedTextColor.AQUA), true);
+		ChatComponentHandler.registerPlaceholder("turquoise", Tag.styling(NamedTextColor.AQUA), true);
 
-		ChatComponentHandler.registerPlaceholder("light_red", Tag.styling(NamedTextColor.RED));
+		ChatComponentHandler.registerPlaceholder("light_red", Tag.styling(NamedTextColor.RED), true);
 
+		ChatComponentHandler.registerPlaceholder("pink", Tag.styling(NamedTextColor.LIGHT_PURPLE), true);
+		ChatComponentHandler.registerPlaceholder("magenta", Tag.styling(NamedTextColor.LIGHT_PURPLE), true);
 
-		ChatComponentHandler.registerPlaceholder("pink", Tag.styling(NamedTextColor.LIGHT_PURPLE));
-		ChatComponentHandler.registerPlaceholder("magenta", Tag.styling(NamedTextColor.LIGHT_PURPLE));
+		ChatComponentHandler.registerPlaceholder("light_yellow", Tag.styling(NamedTextColor.YELLOW), true);
 
-		ChatComponentHandler.registerPlaceholder("light_yellow", Tag.styling(NamedTextColor.YELLOW));
+		ChatComponentHandler.registerPlaceholder("magic", Tag.styling(TextDecoration.OBFUSCATED), true);
 
-		ChatComponentHandler.registerPlaceholder("underline", Tag.styling(TextDecoration.UNDERLINED));
+		ChatComponentHandler.registerPlaceholder("strike", Tag.styling(TextDecoration.STRIKETHROUGH), true);
+		ChatComponentHandler.registerPlaceholder("s", Tag.styling(TextDecoration.STRIKETHROUGH), true);
+
+		ChatComponentHandler.registerPlaceholder("underline", Tag.styling(TextDecoration.UNDERLINED), true);
+
+		ChatComponentHandler.registerPlaceholder("italics", Tag.styling(TextDecoration.ITALIC), true);
+
+		ChatComponentHandler.registerPlaceholder("r", ParserDirective.RESET, true);
+
+		ChatComponentHandler.registerResolver(TagResolver.resolver(Set.of("open_url", "link", "url"), (argumentQueue, context) -> {
+			String url = argumentQueue.popOr("A link tag must have an argument of the url").value();
+			return Tag.styling(ClickEvent.openUrl(url));
+		}), false);
+
+		ChatComponentHandler.registerResolver(TagResolver.resolver(Set.of("run_command", "command", "cmd"), (argumentQueue, context) -> {
+			String command = argumentQueue.popOr("A run command tag must have an argument of the command to execute").value();
+			return Tag.styling(ClickEvent.runCommand(command));
+		}), false);
+
+		ChatComponentHandler.registerResolver(TagResolver.resolver(Set.of("suggest_command", "sgt"), (argumentQueue, context) -> {
+			String command = argumentQueue.popOr("A suggest command tag must have an argument of the command to suggest").value();
+			return Tag.styling(ClickEvent.suggestCommand(command));
+		}), false);
+
+		ChatComponentHandler.registerResolver(TagResolver.resolver(Set.of("change_page"), (argumentQueue, context) -> {
+			String rawPage = argumentQueue.popOr("A change page tag must have an argument of the page number").value();
+			int page;
+			try {
+				page = Integer.parseInt(rawPage);
+			} catch (NumberFormatException e) {
+				throw context.newException(e.getMessage(), argumentQueue);
+			}
+			return Tag.styling(ClickEvent.changePage(page));
+		}), false);
+
+		ChatComponentHandler.registerResolver(TagResolver.resolver(Set.of("copy_to_clipboard", "copy", "clipboard"), (argumentQueue, context) -> {
+			String string = argumentQueue.popOr("A copy to clipboard tag must have an argument of the string to copy").value();
+			return Tag.styling(ClickEvent.copyToClipboard(string));
+		}), false);
+
+		ChatComponentHandler.registerResolver(TagResolver.resolver(Set.of("show_text", "tooltip", "ttp"), (argumentQueue, context) -> {
+			String tooltip = argumentQueue.popOr("A tooltip tag must have an argument of the message to show").value();
+			return Tag.styling(HoverEvent.showText(context.deserialize(tooltip)));
+		}), false);
+
+		ChatComponentHandler.registerResolver(TagResolver.resolver(Set.of("f"),
+			(argumentQueue, context) -> StandardTags.font().resolve("font", argumentQueue, context)), false);
+
+		ChatComponentHandler.registerResolver(TagResolver.resolver(Set.of("insertion", "ins"),
+			(argumentQueue, context) -> StandardTags.insertion().resolve("insert", argumentQueue, context)), false);
+
+		ChatComponentHandler.registerResolver(TagResolver.resolver(Set.of("keybind"),
+			(argumentQueue, context) -> StandardTags.keybind().resolve("key", argumentQueue, context)), false);
 
 		Pattern unicodePattern = Pattern.compile("[0-9a-f]{4,}");
 		ChatComponentHandler.registerResolver(TagResolver.resolver("unicode", (argumentQueue, context) -> {
@@ -98,7 +154,7 @@ public class ChatModule implements AddonModule {
 				throw context.newException("Invalid unicode tag");
 			String unicode = Character.toString(Integer.parseInt(matcher.group(), 16));
 			return Tag.selfClosingInserting(Component.text(unicode));
-		}));
+		}), true);
 
 		// register syntax
 		SyntaxRegistry syntaxRegistry = addon.syntaxRegistry();
