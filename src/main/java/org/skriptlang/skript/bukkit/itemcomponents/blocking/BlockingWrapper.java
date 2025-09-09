9dc3bbba3ca812826910bd2d4e76790ef8847cde
SirSmurfy2 <absolutionism1@gmail.com>
2025-09-09 17:18:06 -0400

    Javadocs

--- src/main/java/org/skriptlang/skript/bukkit/itemcomponents/blocking/BlockingWrapper.java
+++ src/main/java/org/skriptlang/skript/bukkit/itemcomponents/blocking/BlockingWrapper.java
@@ -94,6 +94,9 @@ public class BlockingWrapper extends ComponentWrapper<BlocksAttacks, BlockingBui
 		return newInstance();
 	}
 
+	/**
+	 * @return {@link DamageReduction}s in the form of {@link DamageReductionWrapper}s.
+	 */
 	public List<DamageReductionWrapper> getDamageReductions() {
 		List<DamageReductionWrapper> wrappers = new ArrayList<>();
 		getComponent().damageReductions().forEach(reduction ->
@@ -101,15 +104,25 @@ public class BlockingWrapper extends ComponentWrapper<BlocksAttacks, BlockingBui
 		return wrappers;
 	}
 
+	/**
+	 * @return {@link ItemDamageFunction} in the form of {@link DamageFunctionWrapper}.
+	 */
 	public DamageFunctionWrapper getDamageFunction() {
 		ItemDamageFunction damageFunction = getComponent().itemDamage();
 		return new DamageFunctionWrapper(this, damageFunction);
 	}
 
+	/**
+	 * Creates a new instance of {@link BlockingWrapper} with a blank {@link BlocksAttacks}.
+	 * @return The new {@link BlockingWrapper}.
+	 */
 	public static BlockingWrapper newInstance() {
 		return new BlockingWrapper(new BlockingBuilder().build());
 	}
 
+	/**
+	 * Custom builder class for {@link BlocksAttacks}.
+	 */
 	@SuppressWarnings("NonExtendableApiUsage")
 	public static class BlockingBuilder implements DataComponentBuilder<BlocksAttacks> {
 
@@ -121,8 +134,15 @@ public class BlockingWrapper extends ComponentWrapper<BlocksAttacks, BlockingBui
 		private @Nullable Key blockSound = null;
 		private @Nullable Key disableSound = null;
 
+		/**
+		 * Construct a new {@link BlockingBuilder}.
+		 */
 		public BlockingBuilder() {}
 
+		/**
+		 * Construct a new {@link BlockingBuilder} with the data from {@code component}.
+		 * @param component The {@link BlocksAttacks} to copy data from.
+		 */
 		public BlockingBuilder(BlocksAttacks component) {
 			damageReductions.addAll(new ArrayList<>(component.damageReductions()));
 			blockDelaySeconds = component.blockDelaySeconds();
@@ -133,57 +153,112 @@ public class BlockingWrapper extends ComponentWrapper<BlocksAttacks, BlockingBui
 			disableSound = component.disableSound();
 		}
 
+		/**
+		 * Add a {@link DamageReduction}.
+		 * @param reduction The {@link DamageReduction} to add.
+		 * @return {@code this}
+		 */
 		public BlockingBuilder addDamageReduction(DamageReduction reduction) {
 			damageReductions.add(reduction);
 			return this;
 		}
 
+		/**
+		 * Add {@link DamageReduction}s.
+		 * @param reductions The {@link DamageReduction}s to add.
+		 * @return {@code this}
+		 */
 		public BlockingBuilder addDamageReductions(List<DamageReduction> reductions) {
 			damageReductions.addAll(reductions);
 			return this;
 		}
 
+		/**
+		 * Set {@link DamageReduction}s.
+		 * @param reductions The {@link DamageReduction} to set.
+		 * @return {@code this}
+		 */
 		public BlockingBuilder damageReductions(List<DamageReduction> reductions) {
 			damageReductions.clear();
 			damageReductions.addAll(reductions);
 			return this;
 		}
 
+		/**
+		 * Remove a {@link DamageReduction}.
+		 * @param reduction The {@link DamageReduction} to remove.
+		 * @return {@code this}
+		 */
 		public BlockingBuilder removeReduction(DamageReduction reduction) {
 			damageReductions.remove(reduction);
 			return this;
 		}
 
+		/**
+		 * Remove {@link DamageReduction}s.
+		 * @param reductions The {@link DamageReduction}s to remove.
+		 * @return {@code this}
+		 */
 		public BlockingBuilder removeReductions(List<DamageReduction> reductions) {
 			damageReductions.removeAll(reductions);
 			return this;
 		}
 
+		/**
+		 * Set the block delay seconds.
+		 * @param blockDelaySeconds The delay.
+		 * @return {@code this}
+		 */
 		public BlockingBuilder blockDelaySeconds(float blockDelaySeconds) {
 			this.blockDelaySeconds = blockDelaySeconds;
 			return this;
 		}
 
+		/**
+		 * Set the disable cooldown scale
+		 * @param disableCooldownScale The scale.
+		 * @return {@code this}
+		 */
 		public BlockingBuilder disableCooldownScale(float disableCooldownScale) {
 			this.disableCooldownScale = disableCooldownScale;
 			return this;
 		}
 
+		/**
+		 * Set the {@link ItemDamageFunction}.
+		 * @param damageFunction The {@link ItemDamageFunction} to set.
+		 * @return {@code this}
+		 */
 		public BlockingBuilder itemDamage(ItemDamageFunction damageFunction) {
 			this.damageFunction = damageFunction;
 			return this;
 		}
 
+		/**
+		 * Set the damage type to bypass.
+		 * @param bypassedBy The damage type to set.
+		 * @return {@code this}
+		 */
 		public BlockingBuilder bypassedBy(@Nullable TagKey<DamageType> bypassedBy) {
 			this.bypassedBy = bypassedBy;
 			return this;
 		}
 
+		/**
+		 * Set the block sound.
+		 * @param blockSound The block sound to set.
+		 * @return {@code this}
+		 */
 		public BlockingBuilder blockSound(@Nullable Key blockSound) {
 			this.blockSound = blockSound;
 			return this;
 		}
 
+		/**
+		 * Set the disable sound.
+		 * @param disableSound The disable sound to set.
+		 * @return {@code this}
+		 */
 		public BlockingBuilder disableSound(@Nullable Key disableSound) {
 			this.disableSound = disableSound;
 			return this;
--- src/main/java/org/skriptlang/skript/bukkit/itemcomponents/blocking/DamageReductionWrapper.java
+++ src/main/java/org/skriptlang/skript/bukkit/itemcomponents/blocking/DamageReductionWrapper.java
@@ -1,5 +1,6 @@
 package org.skriptlang.skript.bukkit.itemcomponents.blocking;
 
+import io.papermc.paper.datacomponent.item.BlocksAttacks;
 import io.papermc.paper.datacomponent.item.blocksattacks.DamageReduction;
 import io.papermc.paper.registry.RegistryKey;
 import io.papermc.paper.registry.set.RegistryKeySet;
@@ -11,29 +12,57 @@ import org.skriptlang.skript.bukkit.itemcomponents.ComponentUtils;
 import java.util.List;
 import java.util.function.Consumer;
 
+/**
+ * Wrapper for {@link DamageReduction}.
+ */
 @SuppressWarnings("UnstableApiUsage")
 public class DamageReductionWrapper {
 
 	private BlockingWrapper wrapper;
 	private DamageReduction damageReduction;
 
+	/**
+	 * Construct a new {@link DamageReductionWrapper} with the parent {@link BlockingWrapper} and the
+	 * {@link DamageReduction} it belongs to.
+	 * @param wrapper The {@link BlockingWrapper}.
+	 * @param damageReduction The {@link DamageReduction}.
+	 */
 	public DamageReductionWrapper(BlockingWrapper wrapper, DamageReduction damageReduction) {
 		this.wrapper = wrapper;
 		this.damageReduction = damageReduction;
 	}
 
+	/**
+	 * Construct a new {@link DamageReductionWrapper} with the {@link DamageReduction}.
+	 * @param damageReduction The {@link DamageReduction}.
+	 */
 	public DamageReductionWrapper(DamageReduction damageReduction) {
 		this.damageReduction = damageReduction;
 	}
 
+	/**
+	 * Construct a new {@link DamageReductionWrapper} with a blank {@link DamageReduction}.
+	 */
 	public DamageReductionWrapper() {
 		this.damageReduction = new Builder().buildDamageReduction();
 	}
 
+	/**
+	 * @return The {@link DamageReduction} this {@link DamageReductionWrapper} is wrapping.
+	 */
 	public DamageReduction getDamageReduction() {
 		return damageReduction;
 	}
 
+	/**
+	 * Modify the current {@link DamageReduction} via {@link Builder}.
+	 * <p>
+	 *     If {@link #wrapper} is not null, and the {@link BlocksAttacks#damageReductions()} contains the {@link DamageReduction}
+	 *     before the changes, the {@link DamageReduction} in {@link #wrapper} will be updated.
+	 *     If it does not equal, {@link #wrapper} will be set to {@code null}.
+	 * </p>
+	 * @param consumer The consumer to change data of the {@link Builder}.
+	 */
 	public void modify(Consumer<Builder> consumer) {
 		Builder builder = toBuilder();
 		consumer.accept(builder);
@@ -51,10 +80,16 @@ public class DamageReductionWrapper {
 		damageReduction = newDamageReduction;
 	}
 
+	/**
+	 * @return The {@link Builder} with the data from {@link #damageReduction}.
+	 */
 	public Builder toBuilder() {
 		return new Builder(damageReduction);
 	}
 
+	/**
+	 * Custom builder class for {@link DamageReduction}.
+	 */
 	public static class Builder {
 
 		private float base = 0f;
@@ -62,8 +97,15 @@ public class DamageReductionWrapper {
 		private float horizontalBlockingAngle = 90f;
 		private @Nullable RegistryKeySet<DamageType> types = null;
 
+		/**
+		 * Construct a new {@link Builder}.
+		 */
 		public Builder() {}
 
+		/**
+		 * Construct a new {@link Builder} and copying the data from {@code damageReduction}.
+		 * @param damageReduction The {@link DamageReduction} to copy data from.
+		 */
 		public Builder(DamageReduction damageReduction) {
 			base = damageReduction.base();
 			factor = damageReduction.factor();
@@ -71,21 +113,41 @@ public class DamageReductionWrapper {
 			types = damageReduction.type();
 		}
 
+		/**
+		 * Set the base.
+		 * @param base The base amount to set.
+		 * @return {@code this}
+		 */
 		public Builder base(float base) {
 			this.base = base;
 			return this;
 		}
 
+		/**
+		 * Set the factor.
+		 * @param factor The factor amount to set.
+		 * @return {@code this}
+		 */
 		public Builder factor(float factor) {
 			this.factor = factor;
 			return this;
 		}
 
+		/**
+		 * Set the damage types.
+		 * @param types The damage types to set.
+		 * @return {@code this}
+		 */
 		public Builder types(@Nullable RegistryKeySet<DamageType> types) {
 			this.types = types;
 			return this;
 		}
 
+		/**
+		 * Set the damage types.
+		 * @param types The damage types to set.
+		 * @return {@code this}
+		 */
 		public Builder types(@Nullable List<DamageType> types) {
 			if (types == null || types.isEmpty()) {
 				this.types = null;
@@ -95,11 +157,19 @@ public class DamageReductionWrapper {
 			return this;
 		}
 
+		/**
+		 * Set the horizontal blocking angle.
+		 * @param horizontalBlockingAngle The horizontal blocking aingle to set.
+		 * @return {@code this}
+		 */
 		public Builder horizontalBlockingAngle(@Positive float horizontalBlockingAngle) {
 			this.horizontalBlockingAngle = horizontalBlockingAngle;
 			return this;
 		}
 
+		/**
+		 * @return The finalized {@link DamageReduction}.
+		 */
 		public DamageReduction buildDamageReduction() {
 			return DamageReduction.damageReduction()
 				.base(base)
@@ -109,6 +179,9 @@ public class DamageReductionWrapper {
 				.build();
 		}
 
+		/**
+		 * @return New {@link DamageReductionWrapper} with the finalized {@link DamageReduction}.
+		 */
 		public DamageReductionWrapper build() {
 			return new DamageReductionWrapper(buildDamageReduction());
 		}
