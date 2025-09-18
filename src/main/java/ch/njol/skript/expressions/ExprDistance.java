c836fede3a1d4b1d4ab042f9cdd51baa7638bab8
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2025-09-18 23:20:01 +0000

    Update ExprDistance.java

--- src/main/java/ch/njol/skript/expressions/ExprDistance.java
+++ src/main/java/ch/njol/skript/expressions/ExprDistance.java
@@ -10,6 +10,7 @@ import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.Literal;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.skript.registrations.Classes;
 import ch.njol.util.Kleenean;
 import org.bukkit.Location;
 import org.bukkit.event.Event;
