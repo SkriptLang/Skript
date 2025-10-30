5e8648668bd1898fcdd16765ef53510e2e458535
Mads Volder <madsvolder@gmail.com>
2025-10-30 13:41:03 +0100

    Added a couple of tests as well as an up to date test coverage list.

---
+++ src/test/java/org/skriptlang/skript/test/tests/syntaxes/effects/EffMakeEggHatchTest.java
@@ -0,0 +1,31 @@
+package org.skriptlang.skript.test.tests.syntaxes.effects;
+
+import ch.njol.skript.test.runner.SkriptJUnitTest;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Egg;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.player.PlayerEggThrowEvent;
+import org.easymock.EasyMock;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EffMakeEggHatchTest extends SkriptJUnitTest {
+
+	static {
+		setShutdownDelay(1);
+	}
+
+	@Test
+	public void test() {
+		Player player = EasyMock.niceMock(Player.class);
+		Egg egg = EasyMock.niceMock(Egg.class);
+		PlayerEggThrowEvent event = new PlayerEggThrowEvent(player, egg, false, (byte) 0, EntityType.CHICKEN);
+		Bukkit.getPluginManager().callEvent(event);
+
+		if(!event.isHatching())
+			Assert.fail("Egg should be hatching");
+	}
+
+}
