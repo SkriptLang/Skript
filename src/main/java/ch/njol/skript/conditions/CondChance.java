9dddb4c71a0727bcc0eb1e4ff5792c002f2027a9
Fusezion <fusezionstream@gmail.com>
2025-12-15 14:46:09 -0500

    Remove debug import

--- src/main/java/ch/njol/skript/conditions/CondChance.java
+++ src/main/java/ch/njol/skript/conditions/CondChance.java
@@ -9,7 +9,6 @@ import ch.njol.skript.lang.Condition;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.Kleenean;
-import org.bukkit.Bukkit;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 
