3bf8a27a2f29bdd35c713f36a379a740ea0d6f71
Patrick Miller <apickledwalrus@icloud.com>
2026-01-06 16:22:46 -0500

    Implement priority estimation

--- src/main/java/org/skriptlang/skript/registration/SyntaxInfoImpl.java
+++ src/main/java/org/skriptlang/skript/registration/SyntaxInfoImpl.java
@@ -19,6 +19,23 @@ import java.util.function.Supplier;
 
 class SyntaxInfoImpl<T extends SyntaxElement> implements SyntaxInfo<T> {
 
+	private static Priority estimatePriority(Collection<String> patterns) {
+		Priority priority = SyntaxInfo.SIMPLE;
+		for (String pattern : patterns) {
+			char[] chars = pattern.toCharArray();
+			for (int i = 0; i < chars.length; i++) {
+				if (chars[i] == '%') {
+					// weigh "%thing% %thing%" or "%thing% [%thing%]" much heavier
+					if ((i - 2 >= 0 && chars[i - 2] == '%') || (i - 3 >= 0 && chars[i - 3] == '%')) {
+						return SyntaxInfo.PATTERN_MATCHES_EVERYTHING;
+					}
+					priority = SyntaxInfo.COMBINED;
+				}
+			}
+		}
+		return priority;
+	}
+
 	private final Origin origin;
 	private final Class<T> type;
 	private final @Nullable Supplier<T> supplier;
@@ -27,7 +44,7 @@ class SyntaxInfoImpl<T extends SyntaxElement> implements SyntaxInfo<T> {
 
 	protected SyntaxInfoImpl(
 		Origin origin, Class<T> type, @Nullable Supplier<T> supplier,
-		Collection<String> patterns, Priority priority
+		Collection<String> patterns, @Nullable Priority priority
 	) {
 		Preconditions.checkArgument(supplier != null || ClassUtils.isNormalClass(type),
 				"Failed to register a syntax info for '" + type.getName() + "'."
@@ -39,6 +56,9 @@ class SyntaxInfoImpl<T extends SyntaxElement> implements SyntaxInfo<T> {
 		this.type = type;
 		this.supplier = supplier;
 		this.patterns = ImmutableList.copyOf(patterns);
+		if (priority == null) {
+			priority = estimatePriority(patterns);
+		}
 		this.priority = priority;
 	}
 
@@ -123,7 +143,7 @@ class SyntaxInfoImpl<T extends SyntaxElement> implements SyntaxInfo<T> {
 		Origin origin = Origin.UNKNOWN;
 		@Nullable Supplier<E> supplier;
 		final List<String> patterns = new ArrayList<>();
-		Priority priority = SyntaxInfo.COMBINED;
+		@Nullable Priority priority;
 
 		BuilderImpl(Class<E> type) {
 			this.type = type;
@@ -178,7 +198,9 @@ class SyntaxInfoImpl<T extends SyntaxElement> implements SyntaxInfo<T> {
 				builder.supplier((Supplier) supplier);
 			}
 			builder.addPatterns(patterns);
-			builder.priority(priority);
+			if (priority != null) {
+				builder.priority(priority);
+			}
 		}
 
 	}
