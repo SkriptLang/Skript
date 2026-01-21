7420ab443770672d183ad5059bce81306897fc4e
Patrick Miller <apickledwalrus@icloud.com>
2026-01-20 20:08:29 -0500

    Update chat event usage

--- src/main/java/org/skriptlang/skript/bukkit/entity/player/elements/ExprChatMessage.java
+++ src/main/java/org/skriptlang/skript/bukkit/entity/player/elements/ExprChatMessage.java
@@ -1,4 +1,4 @@
-package ch.njol.skript.expressions;
+package org.skriptlang.skript.bukkit.entity.player.elements;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.classes.Changer.ChangeMode;
@@ -9,14 +9,16 @@ import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.EventRestrictedSyntax;
 import ch.njol.skript.lang.Expression;
-import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
 import ch.njol.util.coll.CollectionUtils;
+import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.text.Component;
 import org.bukkit.event.Event;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.jetbrains.annotations.Nullable;
+import org.skriptlang.skript.registration.SyntaxInfo;
+import org.skriptlang.skript.registration.SyntaxRegistry;
 
 @Name("Chat Message")
 @Description("The chat message in a chat event.")
@@ -25,12 +27,15 @@ import org.jetbrains.annotations.Nullable;
 		player has permission "admin"
 		set the message to "<light red>%message%"
 	""")
-@Since("1.4.6")
+@Since("1.4.6, INSERT VERSION (support for reset)")
 @Events("chat")
-public class ExprChatMessage extends SimpleExpression<String> implements EventRestrictedSyntax {
+public class ExprChatMessage extends SimpleExpression<Component> implements EventRestrictedSyntax {
 
-	static {
-		Skript.registerExpression(ExprChatMessage.class, String.class, ExpressionType.SIMPLE, "[the] [chat( |-)]message");
+	public static void register(SyntaxRegistry syntaxRegistry) {
+		syntaxRegistry.register(SyntaxRegistry.EXPRESSION, SyntaxInfo.Expression.builder(ExprChatMessage.class, Component.class)
+			.supplier(ExprChatMessage::new)
+			.addPattern("[the] [chat( |-)]message")
+			.build());
 	}
 
 	@Override
@@ -39,11 +44,11 @@ public class ExprChatMessage extends SimpleExpression<String> implements EventRe
 	}
 
 	@Override
-	protected String @Nullable [] get(Event event) {
-		if (event instanceof AsyncPlayerChatEvent chatEvent) {
-			return new String[]{chatEvent.getMessage()};
+	protected Component[] get(Event event) {
+		if (event instanceof AsyncChatEvent asyncChatEvent) {
+			return new Component[]{asyncChatEvent.message()};
 		}
-		return new String[0];
+		return new Component[0];
 	}
 
 	@Override
@@ -53,16 +58,26 @@ public class ExprChatMessage extends SimpleExpression<String> implements EventRe
 			return null;
 		}
 		return switch (mode) {
-			case SET, DELETE -> CollectionUtils.array(String.class);
+			case SET, DELETE, RESET -> CollectionUtils.array(Component.class);
 			default -> null;
 		};
 	}
 
 	@Override
 	public void change(Event event, Object @Nullable [] delta, ChangeMode mode) {
-		if (event instanceof AsyncPlayerChatEvent chatEvent) {
-			chatEvent.setMessage(delta == null ? "" : (String) delta[0]);
+		if (!(event instanceof AsyncChatEvent asyncChatEvent)) {
+			return;
 		}
+
+		asyncChatEvent.message(switch (mode) {
+			case SET -> {
+				assert delta != null;
+				yield (Component) delta[0];
+			}
+			case DELETE -> Component.empty();
+			case RESET -> asyncChatEvent.originalMessage();
+			default -> throw new IllegalStateException("Unexpected change mode: " + mode);
+		});
 	}
 
 	@Override
@@ -71,8 +86,8 @@ public class ExprChatMessage extends SimpleExpression<String> implements EventRe
 	}
 
 	@Override
-	public Class<? extends String> getReturnType() {
-		return String.class;
+	public Class<? extends Component> getReturnType() {
+		return Component.class;
 	}
 
 	@Override
@@ -83,7 +98,7 @@ public class ExprChatMessage extends SimpleExpression<String> implements EventRe
 	@Override
 	public Class<? extends Event>[] supportedEvents() {
 		//noinspection unchecked
-		return new Class[]{AsyncPlayerChatEvent.class};
+		return new Class[]{AsyncChatEvent.class};
 	}
 
 }
