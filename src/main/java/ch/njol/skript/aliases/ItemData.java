ce2d8d04893fffe1a7e430a971ec1f69c015fede
sovdee <10354869+sovdeeth@users.noreply.github.com>
2026-01-24 18:22:58 -0800

    hascustomname is 1.21.3+

--- src/main/java/ch/njol/skript/aliases/ItemData.java
+++ src/main/java/ch/njol/skript/aliases/ItemData.java
@@ -654,6 +654,9 @@ public class ItemData implements Cloneable, YggdrasilExtendedSerializable {
 		return data;
 	}
 
+	// Can remove after 1.21.3 is the minimum supported version
+	private static final boolean HAS_CUSTOM_NAME = Skript.methodExists(PotionMeta.class, "hasCustomPotionName");
+
 	private static void copyPotionInfo(PotionMeta potionMeta, ItemData data) {
 		PotionMeta newMeta = (PotionMeta) itemFactory.getItemMeta(data.type);
 		if (!newMeta.equals(potionMeta)) {
@@ -667,7 +670,7 @@ public class ItemData implements Cloneable, YggdrasilExtendedSerializable {
 			}
 			if (potionMeta.hasColor())
 				newMeta.setColor(potionMeta.getColor());
-			if (potionMeta.hasCustomPotionName())
+			if (HAS_CUSTOM_NAME && potionMeta.hasCustomPotionName())
 				newMeta.setCustomPotionName(potionMeta.getCustomPotionName());
 			data.itemFlags = ItemFlags.CHANGED_TAGS;
 			data.setItemMeta(newMeta);
