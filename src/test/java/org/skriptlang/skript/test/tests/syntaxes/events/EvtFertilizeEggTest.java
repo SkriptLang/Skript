ad29c231df9d576746acd2ab1127c9e4b8ba5c1e
CJH3139 <imnotnotcjh@gmail.com>
2026-04-17 09:34:42 -0700

    Add player, itemstack, and experience to make sure event values work in the skript file

--- src/test/java/org/skriptlang/skript/test/tests/syntaxes/events/EvtFertilizeEggTest.java
+++ src/test/java/org/skriptlang/skript/test/tests/syntaxes/events/EvtFertilizeEggTest.java
@@ -3,8 +3,12 @@ package org.skriptlang.skript.test.tests.syntaxes.events;
 import ch.njol.skript.test.runner.SkriptJUnitTest;
 import io.papermc.paper.event.entity.EntityFertilizeEggEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Frog;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.easymock.EasyMock;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,17 +21,21 @@ public class EvtFertilizeEggTest extends SkriptJUnitTest {
 
 	private Frog mother;
 	private Frog father;
+	private Player player;
 
 	@Before
 	public void before() {
 		mother = spawnTestEntity(EntityType.FROG);
 		father = spawnTestEntity(EntityType.FROG);
+
+		player = EasyMock.niceMock(Player.class);
+		EasyMock.replay(player);
 	}
 
 	@Test
 	public void test() {
 		Bukkit.getPluginManager().callEvent(
-			new EntityFertilizeEggEvent(mother, father, null, null, 0));
+			new EntityFertilizeEggEvent(mother, father, player, new ItemStack(Material.SLIME_BALL), 5));
 	}
 
 	@After
