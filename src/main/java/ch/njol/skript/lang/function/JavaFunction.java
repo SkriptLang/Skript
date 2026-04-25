7ba35a53bdea8a98a827fae118221a0fde476336
Efnilite <35348263+Efnilite@users.noreply.github.com>
2026-04-25 14:14:15 +0200

    remove imports

--- src/main/java/ch/njol/skript/lang/function/JavaFunction.java
+++ src/main/java/ch/njol/skript/lang/function/JavaFunction.java
@@ -14,7 +14,6 @@ import org.skriptlang.skript.common.function.Parameters;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.SequencedCollection;
 
 /**
  * @deprecated Use {@link DefaultFunction} instead.
