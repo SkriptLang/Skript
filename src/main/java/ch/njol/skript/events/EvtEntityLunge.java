fdc75a72b6c4ba3b7100f461873f90d36749f98d
vuxeim <35169070+vuxeim@users.noreply.github.com>
2026-05-17 21:24:14 +0200

    feat: add lunge event feat: add lunge power expression

---
+++ src/main/java/ch/njol/skript/events/EvtEntityLunge.java
@@ -0,0 +1,81 @@
+package ch.njol.skript.events;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.entity.EntityType;
+import ch.njol.skript.lang.Literal;
+import ch.njol.skript.lang.SkriptEvent;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.registrations.EventConverter;
+import ch.njol.skript.registrations.EventValues;
+import io.papermc.paper.event.entity.EntityLungeEvent;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
+
+@SuppressWarnings("unchecked")
+public class EvtEntityLunge extends SkriptEvent {
+
+	@Nullable
+	private Literal<EntityType> entityTypes;
+
+	static {
+		// Since paper 26.1.2
+		if (Skript.classExists("io.papermc.paper.event.entity.EntityLungeEvent")) {
+			Skript.registerEvent("Entity Lunge", EvtEntityLunge.class, EntityLungeEvent.class, "[%-entitytypes%] lunge")
+				.description("Called when an entity lunges.",
+					"Either by using a spear enchanted with the lunge enchantment (e.g. players, skeletons)",
+					"or because of a mob's natural lunge attack (e.g. ravagers).",
+					"Lunge attack propels entity forward horizontally.")
+				.examples("on lunge:",
+						      "\tset lunge power to 4",
+						  "on ravager lunge:",
+							  "\tcancel event"
+				)
+				.since("2.16");
+		}
+
+		EventValues.registerEventValue(EntityLungeEvent.class, Integer.class, new EventConverter<>() {
+			@Override
+			public void set(EntityLungeEvent event, Integer value) {
+				event.setLungePower(value);
+			}
+
+			@Override
+			public Integer convert(EntityLungeEvent event) {
+				return event.getLungePower();
+			}
+		});
+	}
+
+	@Override
+	public boolean init(Literal<?>[] args, int matchedPattern, SkriptParser.ParseResult parseResult) {
+		entityTypes = (Literal<EntityType>) args[0];
+		return true;
+	}
+
+	@Override
+	public boolean check(Event event) {
+
+		if (!(event instanceof EntityLungeEvent lungeEvent)) {
+			return false;
+		}
+
+		if (entityTypes == null) {
+			return false;
+		}
+
+		boolean matches = false;
+		for (EntityType et : entityTypes.getAll()) {
+			if (et.isInstance(lungeEvent.getEntity())) {
+				matches = true;
+				break;
+			}
+		}
+		return matches;
+	}
+
+	@Override
+	public String toString(@Nullable Event e, boolean debug) {
+		return (entityTypes != null ? entityTypes.toString(e, debug) : "") + " lunge";
+	}
+
+}
---
+++ src/main/java/ch/njol/skript/expressions/ExprLungePower.java
@@ -0,0 +1,72 @@
+package ch.njol.skript.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.*;
+import ch.njol.skript.expressions.base.EventValueExpression;
+import ch.njol.skript.expressions.base.WrapperExpression;
+import ch.njol.skript.lang.*;
+import ch.njol.util.Kleenean;
+import ch.njol.util.coll.CollectionUtils;
+import io.papermc.paper.event.entity.EntityLungeEvent;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
+
+@Name("Lunge Power")
+@Description({"The power of lunge attack.",
+			"Can be set to modify the distance of the lunge attack.",
+			"Initially, the lunge power is determined by the enchantment level of the lunge enchantment" +
+				"of the weapon used to perform the lunge attack (e.g. a spear).",
+			"But some mobs (e.g. ravagers) can also perform lunge attacks without a weapon."})
+@Example("""
+on skeleton lunge:
+	if the lunge power is 1, 2 or 3:
+		broadcast "Normal lunge power"
+	else if the lunge power is greater than 3:
+		broadcast "Overpowered lunge power"
+""")
+@Example("""
+on lunge:
+	set event-lunge power to 5
+""")
+@Since("2.16")
+public class ExprLungePower extends WrapperExpression<Integer> implements EventRestrictedSyntax {
+
+	static {
+		// Since paper 26.1.2
+		if (Skript.classExists("io.papermc.paper.event.entity.EntityLungeEvent")) {
+			Skript.registerExpression(ExprLungePower.class, Integer.class, ExpressionType.SIMPLE, "[the] [event-]lunge power");
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+		if (exprs.length != 0) {
+			return false;
+		}
+
+		setExpr(new EventValueExpression<>(Integer.class));
+		return ((EventValueExpression<Integer>) getExpr()).init();
+	}
+
+	@Override
+	public Class<? extends Event>[] supportedEvents() {
+		return CollectionUtils.array(EntityLungeEvent.class);
+	}
+
+	@Override
+	public boolean isSingle() {
+		return true;
+	}
+
+	@Override
+	public Class<Integer> getReturnType() {
+		return Integer.class;
+	}
+
+	@Override
+	public String toString(@Nullable Event event, boolean debug) {
+		return "lunge power";
+	}
+
+}
