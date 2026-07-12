95db3cdca9b5a706a17c620cf17f633b7d064a07
MrScopes <drewmanrules23@gmail.com>
2026-07-12 19:03:00 -0400

    switch to playermodule over simpleevents

--- src/main/java/ch/njol/skript/events/SimpleEvents.java
+++ src/main/java/ch/njol/skript/events/SimpleEvents.java
@@ -8,7 +8,6 @@ import com.destroystokyo.paper.event.entity.EntityJumpEvent;
 import com.destroystokyo.paper.event.entity.ProjectileCollideEvent;
 import com.destroystokyo.paper.event.player.PlayerElytraBoostEvent;
 import com.destroystokyo.paper.event.player.PlayerJumpEvent;
-import com.destroystokyo.paper.event.player.PlayerPostRespawnEvent;
 import com.destroystokyo.paper.event.player.PlayerReadyArrowEvent;
 import com.destroystokyo.paper.event.server.PaperServerListPingEvent;
 import io.papermc.paper.event.player.*;
@@ -205,20 +204,6 @@ public class SimpleEvents {
 				.examples("on quit:",
 						"on disconnect:")
 				.since("1.0 (simple disconnection)");
-		Skript.registerEvent("Respawn", SimpleEvent.class, PlayerRespawnEvent.class, "[player] respawn[ing]")
-				.description("Called when a player respawns via death or entering the end portal in the end. You should prefer this event over the <a href='#death'>death event</a> as the player is technically alive when this event is called.")
-				.examples("on respawn:")
-				.since("1.0");
-		Skript.registerEvent("Post Respawn", SimpleEvent.class, PlayerPostRespawnEvent.class, "after [player] respawn[ing]")
-				.description("Called after a player respawns via death or entering the end portal in the end. You should prefer this event over the <a href='#respawn'>respawn event</a> if you need to ensure your changes to the player stick after they respawn.")
-				.examples("after respawn:",
-					"	if respawn location is a bed:",
-					"		broadcast \"%player% respawned at their bed at %respawn location%\"",
-					"	else if respawn location is a respawn anchor:",
-					"		broadcast \"%player% respawned at their anchor at %respawn location%\"",
-					"	else:",
-					"		broadcast \"%player% respawned at %respawn location%\"")
-				.since("INSERT VERSION");
 		Skript.registerEvent("Sneak Toggle", SimpleEvent.class, PlayerToggleSneakEvent.class, "[player] toggl(e|ing) sneak", "[player] sneak toggl(e|ing)")
 				.description("Called when a player starts or stops sneaking. Use <a href='#CondIsSneaking'>is sneaking</a> to get whether the player was sneaking before the event was called.")
 				.examples("# make players that stop sneaking jump",
--- src/main/java/org/skriptlang/skript/bukkit/entity/player/PlayerModule.java
+++ src/main/java/org/skriptlang/skript/bukkit/entity/player/PlayerModule.java
@@ -3,6 +3,8 @@ package org.skriptlang.skript.bukkit.entity.player;
 import ch.njol.skript.Skript;
 import ch.njol.skript.lang.util.SimpleEvent;
 import io.papermc.paper.event.player.AsyncChatEvent;
+import com.destroystokyo.paper.event.player.PlayerPostRespawnEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.skriptlang.skript.addon.AddonModule;
 import org.skriptlang.skript.addon.HierarchicalAddonModule;
 import org.skriptlang.skript.addon.SkriptAddon;
@@ -67,6 +69,30 @@ public class PlayerModule extends HierarchicalAddonModule {
 			.addSince("1.4.1")
 			.addPattern("chat")
 			.addEvent(AsyncChatEvent.class)
+			.build());
+
+		syntaxRegistry.register(BukkitSyntaxInfos.Event.KEY, BukkitSyntaxInfos.Event.builder(SimpleEvent.class, "Player Respawn")
+			.addDescription("Called when a player respawns via death or entering the end portal in the end. You should prefer this event over the <a href='#death'>death event</a> as the player is technically alive when this event is called.")
+			.addExample("on respawn:")
+			.addSince("1.0")
+			.addPattern("[player] respawn[ing]")
+			.addEvent(PlayerRespawnEvent.class)
+			.build());
+
+		syntaxRegistry.register(BukkitSyntaxInfos.Event.KEY, BukkitSyntaxInfos.Event.builder(SimpleEvent.class, "After Player Respawn")
+			.addDescription("Called after a player respawns via death or entering the end portal in the end. You should prefer this event over the <a href='#respawn'>respawn event</a> if you need to ensure your changes to the player stick after they respawn.")
+			.addExample("""
+				after respawn:
+					if respawn location is a bed:
+						broadcast "%player% respawned at their bed at %respawn location%"
+					else if respawn location is a respawn anchor:
+						broadcast "%player% respawned at their anchor at %respawn location%"
+					else:
+						broadcast "%player% respawned at %respawn location%"
+				""")
+			.addSince("INSERT VERSION")
+			.addPattern("after [player] respawn[ing]")
+			.addEvent(PlayerPostRespawnEvent.class)
 			.build());
 	}
 
