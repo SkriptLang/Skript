e29406ec43097e098182c50cbc7c04f3b743ec5e
nova ⭐ <novaa0@outlook.com>
2026-07-16 18:22:35 +0100

    add ExprItemModel

---
+++ src/main/java/org/skriptlang/skript/bukkit/item/elements/ExprItemModel.java
@@ -0,0 +1,78 @@
+package org.skriptlang.skript.bukkit.item.elements;
+
+import ch.njol.skript.aliases.ItemType;
+import ch.njol.skript.bukkitutil.ItemUtils;
+import ch.njol.skript.classes.Changer.ChangeMode;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Example;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import ch.njol.util.coll.CollectionUtils;
+import org.bukkit.NamespacedKey;
+import org.bukkit.event.Event;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.Nullable;
+import org.skriptlang.skript.registration.SyntaxRegistry;
+
+import static ch.njol.skript.classes.Changer.ChangeMode.SET;
+
+@Name("Item Model")
+@Description("The item model of an item. Accepts a Namespaced Key (e.g. 'minecraft:emerald').")
+@Example("""
+	set the item model of player's held item to "diamond"
+	set the item model of {_item} to "minecraft:dirt"
+	""")
+@Since("INSERT VERSION")
+public class ExprItemModel extends SimplePropertyExpression<ItemType, String> {
+	public static void register(SyntaxRegistry registry) {
+		registry.register(
+			SyntaxRegistry.EXPRESSION,
+			infoBuilder(ExprItemModel.class, String.class, "item model", "itemtypes", true)
+				.supplier(ExprItemModel::new)
+				.build()
+		);
+	}
+
+	@Override
+	public @Nullable String convert(ItemType from) {
+		ItemStack itemStack = ItemUtils.asItemStack(from);
+		if (itemStack == null) return null;
+
+		NamespacedKey key = itemStack.getItemMeta().getItemModel();
+		if (key == null) return null;
+
+		return key.asString();
+	}
+
+	@Override
+	public void change(Event event, Object @Nullable [] delta, ChangeMode mode) {
+		//noinspection DataFlowIssue
+		NamespacedKey key = (mode == SET) ? NamespacedKey.fromString((String) delta[0]) : null;
+
+		for (ItemType item : getExpr().getArray(event)) {
+			ItemMeta meta = item.getItemMeta();
+			meta.setItemModel(key);
+			item.setItemMeta(meta);
+		}
+	}
+
+	@Override
+	public Class<?> @Nullable [] acceptChange(ChangeMode mode) {
+		return switch (mode) {
+			case SET, DELETE, RESET -> CollectionUtils.array(String.class);
+			default -> null;
+		};
+	}
+
+	@Override
+	public Class<? extends String> getReturnType() {
+		return String.class;
+	}
+
+	@Override
+	protected String getPropertyName() {
+		return "item model";
+	}
+}
