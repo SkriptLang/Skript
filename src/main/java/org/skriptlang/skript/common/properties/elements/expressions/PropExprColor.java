c808ee38243e190cae702b93b3b78c65606b5f67
ItsTheSky <livegogo.16334@gmail.com>
2026-08-25 22:29:08 +0200

    docs: use a text block for the color property description

--- src/main/java/org/skriptlang/skript/common/properties/elements/expressions/PropExprColor.java
+++ src/main/java/org/skriptlang/skript/common/properties/elements/expressions/PropExprColor.java
@@ -12,12 +12,12 @@ import org.skriptlang.skript.lang.properties.handlers.base.ExpressionPropertyHan
 import org.skriptlang.skript.registration.SyntaxRegistry;
 
 @Name("Color of")
-@Description({
-	"The <a href='#color'>color</a> of an item, entity, block, firework effect, or text display.",
-	"This can also be used to color chat messages with \"&lt;%color of ...%&gt;this text is colored!\".",
-	"Do note that firework effects support setting, adding, removing, resetting, and deleting; text displays support " +
-	"setting and resetting; and items, entities, and blocks only support setting, and only for very few items/blocks."
-})
+@Description("""
+	The <a href='#color'>color</a> of an item, entity, block, firework effect, or text display.
+	This can also be used to color chat messages with "&lt;%color of ...%&gt;this text is colored!".
+	Do note that firework effects support setting, adding, removing, resetting, and deleting; text displays support \
+	setting and resetting; and items, entities, and blocks only support setting, and only for very few items/blocks.
+	""")
 @Example("""
 	on click on wool:
 		if event-block is tagged with minecraft tag "wool":
