08c3a4f8d4a39a7270a55bc6589c9e6adf8db9dd
_tud <98935832+UnderscoreTud@users.noreply.github.com>
2026-08-30 19:05:52 +0300

    Add docs

--- src/main/java/ch/njol/skript/expressions/ExprAny.java
+++ src/main/java/ch/njol/skript/expressions/ExprAny.java
@@ -1,6 +1,10 @@
 package ch.njol.skript.expressions;
 
 import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Example;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
 import ch.njol.skript.expressions.base.WrapperExpression;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
@@ -9,10 +13,17 @@ import ch.njol.util.Kleenean;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 
+@Name("Any Element")
+@Description("Returns any element from a list and forces the 'or' list behavior on expressions.")
+@Example("""
+	set {_list::*} to 1, 4, 5
+	broadcast whether any {_list::*} is evenly divisible by 2 # true
+	""")
+@Since("INSERT VERSION")
 public class ExprAny extends WrapperExpression<Object> {
 
 	static {
-		Skript.registerExpression(ExprAny.class, Object.class, ExpressionType.SIMPLE, "any %~objects%");
+		Skript.registerExpression(ExprAny.class, Object.class, ExpressionType.PROPERTY, "any %~objects%");
 	}
 
 	@Override
